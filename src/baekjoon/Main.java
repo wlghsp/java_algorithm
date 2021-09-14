df46410286a82abfe9a11e5016b157cd86360557
wlghsp <wlghsp@gmail.com>
2021-09-15 00:00:00 +0900

    자바 백준 기초 진행중

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,19 +1,18 @@
 package baekjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
 
-        int a = Integer.parseInt(br.readLine());
-        int b = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(str, " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
-        System.out.println(a + b);
-
+        System.out.println(a - b);
     }
 
 }
--- src/baekjoon/boj_10951.java
+++ src/baekjoon/boj_10951.java
@@ -1,25 +1,28 @@
 package baekjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class boj_10951 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String str;
-        for (int i = 0; i < 5; i++) {
-            str = br.readLine();
-            StringTokenizer st = new StringTokenizer(str, " ");
+        StringTokenizer st;
+
+        while ((str=br.readLine()) != null) {
+            st = new StringTokenizer(str, " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            System.out.println(a + b);
+            bw.write(a+b + "\n");
         }
 
+        br.close();
+        bw.flush();
+        bw.close();
 
     }
 }
