4d37769a49c2dccdec8ddfc9e72f803ae1def775
wlghsp <wlghsp@gmail.com>
2021-10-25 07:12:12 +0900

    211024 파일 정리 등

--- src/baekjoon/silverⅤ/Boj_1018.java
+++ src/baekjoon/silverⅤ/Boj_1018.java
@@ -1,4 +1,4 @@
-package baekjoon;
+package baekjoon.silverⅤ;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -9,42 +9,38 @@ import java.util.StringTokenizer;
 
 public class Boj_1018 {
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(st.nextToken());        
+        int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        
 
         String[] board = new String[50];
         for (int i = 0; i < n; i++) {
             board[i] = br.readLine();
         }
 
-        
         int ret = 64;
-        for (int i = 0; i <= n-8; i++) {
-            for (int j = 0; j <= m-8; j++) {
+        for (int i = 0; i <= n - 8; i++) {
+            for (int j = 0; j <= m - 8; j++) {
                 int cnt = 0;
                 for (int r = 0; r < 8; r++) {
                     for (int c = 0; c < 8; c++) {
-                        if ((r%2 == c % 2 ? 'W' : 'B') == board[i+r].charAt(j+c)) {
+                        if ((r % 2 == c % 2 ? 'W' : 'B') == board[i + r].charAt(j + c)) {
                             cnt++;
                         }
-                    
+
                     }
-                    
+
                 }
                 ret = Math.min(ret, cnt);
-                ret = Math.min(ret, 64-cnt);
+                ret = Math.min(ret, 64 - cnt);
             }
 
         }
-        bw.write(ret +"\n");
+        bw.write(ret + "\n");
         bw.flush();
         bw.close();
     }
 }
-
-
--- study_group/bruteForce/Boj2846_오르막길.java
+++ study_group/bruteForce/Boj2846_오르막길.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
-
 /*
 
 5
@@ -26,35 +25,33 @@ import java.util.StringTokenizer;
 
 */
 
-
-public class Boj_2846 {
+public class Boj2846_오르막길 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
 
         int[] nums = new int[N];
-        
-        StringTokenizer st = new StringTokenizer(br.readLine(), " "); 
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
         }
-        
+
         int ans = 0;
         int temp = 0;
-        /* 
-         for문을 돌리면서 이전 값보다 현재 값이 크면 오르막길
-         임시변수에 그 차이를 더해줌
-
-         크지 않다면 임시 변수 다시 0으로 만듬
-
-         크기 비교가 끝나면 최대값을 가지는 변수를 담아주기. 
-        */
+        /*
+         * for문을 돌리면서 이전 값보다 현재 값이 크면 오르막길 임시변수에 그 차이를 더해줌
+         * 
+         * 크지 않다면 임시 변수 다시 0으로 만듬
+         * 
+         * 크기 비교가 끝나면 최대값을 가지는 변수를 담아주기.
+         */
 
         for (int i = 1; i < N; i++) {
-            if (nums[i-1] < nums[i]) {
-                temp += nums[i] - nums[i-1];
+            if (nums[i - 1] < nums[i]) {
+                temp += nums[i] - nums[i - 1];
             } else {
                 temp = 0;
             }
