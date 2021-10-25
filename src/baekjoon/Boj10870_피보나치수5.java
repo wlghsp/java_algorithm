9e3a74775eeb46bb66a7918c6e22ed033af6f76e
wlghsp <wlghsp@gmail.com>
2021-10-25 17:55:06 +0900

    26일 문제 추가

---
+++ src/baekjoon/Boj10870_피보나치수5.java
@@ -0,0 +1,8 @@
+package baekjoon;
+
+
+public class Boj10870_피보나치수5 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/baekjoon/Boj15650_N과M2.java
@@ -0,0 +1,7 @@
+package baekjoon;
+
+public class Boj15650_N과M2 {
+    public static void main(String[] args) {
+        
+    }
+}
