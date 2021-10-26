1cd748e1f70bd3d715582ee2c18ebb60cceb70cd
wlghsp <wlghsp@gmail.com>
2021-10-26 17:51:18 +0900

    211026 거리의합, 아름다운수

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,39 +1,26 @@
 package baekjoon;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, M;
-    static int[] arr;
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr = new int[M];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
 
-        dfs(1, 0);
-
-    }
-
-
-    static void dfs(int start, int loc) {
-
-        //m 갯수에 맞게 배열을 모두 채웠으면 출력
-        if (loc == M) {
-            for (int val : arr) {
-                System.out.print(val +" ");
+        for (int i = 0; i < T; i++) {
+            long X = Long.parseLong(br.readLine());
+            boolean[] arr = new boolean[10];
+            while (X != 0) {
+                arr[(int) (X % 10L)] = true;
+                X /= 10;
             }
-            System.out.println();
-            return;
-        } 
-
-        //시작값을 정해줌으로써 visited처리를 확인하지 않아도 가능!
-        for (int i = start; i <= N; i++) {
-            arr[loc] = i;
-            dfs(i + 1, loc + 1);
+            int cnt = 0;
+            for (int j = 0; j < 10; j++) {
+                if (arr[j]) cnt++;
+            }
+            System.out.println(cnt);
         }
+
     }
 }
