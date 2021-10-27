791718d512cb63c774498133d8f8f167ff5bea8b
wlghsp <wlghsp@gmail.com>
2021-10-27 16:33:03 +0900

    211027 분해합, 하노이탑 이동순서

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -4,23 +4,26 @@ import java.io.*;
 
 public class Main {
 
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
-        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        bw.write((((int)Math.pow(2, n))-1) + "\n");
+        hanoi(n,1,2,3);
+        bw.write(sb + "\n");
+        bw.flush();
+        bw.close();
 
-        for (int i = 0; i < T; i++) {
-            long X = Long.parseLong(br.readLine());
-            boolean[] arr = new boolean[10];
-            while (X != 0) {
-                arr[(int) (X % 10L)] = true;
-                X /= 10;
-            }
-            int cnt = 0;
-            for (int j = 0; j < 10; j++) {
-                if (arr[j]) cnt++;
-            }
-            System.out.println(cnt);
+    }
+
+    static void hanoi(int n, int from, int aux, int to) {
+        if (n == 1) {
+            sb.append(from + " " + to).append("\n");
+            return;           
         }
-
+        hanoi(n-1, from, to, aux);
+        sb.append(from + " " + to).append("\n");
+        hanoi(n-1, aux, from, to);
     }
 }
--- src/baekjoon/bronzeⅡ/Boj2399_거리의합.java
+++ src/baekjoon/bronzeⅡ/Boj2399_거리의합.java
@@ -3,7 +3,6 @@ package baekjoon.bronzeⅡ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /*
