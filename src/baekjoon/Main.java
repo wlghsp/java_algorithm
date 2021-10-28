eb22c55a81f3dea05562f87c01e7d515ad2044b0
wlghsp <wlghsp@gmail.com>
2021-10-28 09:47:55 +0900

    211028 영화감독숌

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -4,26 +4,21 @@ import java.io.*;
 
 public class Main {
 
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        bw.write((((int)Math.pow(2, n))-1) + "\n");
-        hanoi(n,1,2,3);
-        bw.write(sb + "\n");
-        bw.flush();
-        bw.close();
-
-    }
-
-    static void hanoi(int n, int from, int aux, int to) {
-        if (n == 1) {
-            sb.append(from + " " + to).append("\n");
-            return;           
+        int N = Integer.parseInt(br.readLine());
+        int number = 666;
+        int cnt = 0;
+        while (true) {
+            if (String.valueOf(number).contains("666")) {
+                cnt +=1;
+                if (cnt == N) {
+                    System.out.println(number);
+                    break;
+                }
+            }
+            number +=1;
         }
-        hanoi(n-1, from, to, aux);
-        sb.append(from + " " + to).append("\n");
-        hanoi(n-1, aux, from, to);
+
     }
 }
