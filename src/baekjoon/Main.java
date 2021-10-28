24d0d261f174bac2909e0726f2aec2f480a9d9d3
wlghsp <wlghsp@gmail.com>
2021-10-28 18:03:15 +0900

    211028 부분수열의합 학습중

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,24 +1,36 @@
 package baekjoon;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int number = 666;
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
         int cnt = 0;
-        while (true) {
-            if (String.valueOf(number).contains("666")) {
-                cnt +=1;
-                if (cnt == N) {
-                    System.out.println(number);
-                    break;
+        for (int i = 0; i < N; i++) {
+            for (int j = i+1; j < N; j++) {
+                int sum = 0;
+                int[] temp = new int[j-i];
+                if (temp.length > 0) {
+                    for (int k = i; k <= j; k++) {
+                        sum += arr[k];
+                    }
+                    if (sum == S) {
+                        cnt++;
+                    }
                 }
             }
-            number +=1;
         }
-
+        System.out.println(cnt);
     }
 }
