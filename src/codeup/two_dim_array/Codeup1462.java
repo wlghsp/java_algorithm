a08057071ebe942c50a8336f4bb465bb6da9c438
wlghsp <wlghsp@gmail.com>
2021-11-10 21:28:09 +0900

    cc

--- src/codeup/two_dim_array/Codeup1462.java
+++ src/codeup/two_dim_array/Codeup1462.java
@@ -2,22 +2,23 @@ package codeup.two_dim_array;
 
 /*
 다음과 같은 n*n 배열 구조를 출력해보자.
+
 입력이 3인 경우 다음과 같이 출력한다.
-3 2 1
-6 5 4
-9 8 7
+1 4 7
+2 5 8
+3 6 9
 
 입력이 5인 경우는 다음과 같이 출력한다.
-5 4 3 2 1
-10 9 8 7 6
-15 14 13 12 11
-20 19 18 17 16
-25 24 23 22 21
+1 6 11 16 21
+2 7 12 17 22
+3 8 13 18 23
+4 9 14 19 24
+5 10 15 20 25
 
 입력이 n인 경우의 2차원 배열을 출력해보자.
 
 입력
- 첫 번째 줄에 배열의 크기 n이 입력된다.
+첫 번째 줄에 배열의 크기 n이 입력된다.
 [입력값의 정의역]
 1<= n <= 100
 
@@ -28,10 +29,10 @@ n*n 크기의 배열을 설명과 같이 채워 출력한다.
 4
 
 출력 예시
-4 3 2 1 
-8 7 6 5 
-12 11 10 9 
-16 15 14 13 
+1 5 9 13 
+2 6 10 14 
+3 7 11 15 
+4 8 12 16 
 
 */
 
@@ -41,18 +42,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Codeup1461 {
+public class Codeup1462 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         int[][] arr = new int[101][101];
-        int val = 1;
         for (int i = 1; i <= n; i++) {
-            for (int j = n; j >= 1; j--) {
+            for (int j = 1; j <= n; j++) {
+                int val = j;
                 arr[i][j] = val;
-                val++;
+                val += n;
             }
         }
         for (int i = 1; i <= n; i++) {
