661fc1512f7fd0d390521d57952115bc3b3137ba
wlghsp <wlghsp@gmail.com>
2021-11-12 17:55:52 +0900

    일단 내 풀이 완료. 다른 사람 풀이 학습 예정

---
+++ src/programmers/level1/자릿수더하기.java
@@ -0,0 +1,40 @@
+package programmers.level1;
+
+/*
+자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
+제한사항
+
+    N의 범위 : 100,000,000 이하의 자연수
+
+입출력 예
+N 	answer
+123 	6
+987 	24
+입출력 예 설명
+
+입출력 예 #1
+문제의 예시와 같습니다.
+
+입출력 예 #2
+9 + 8 + 7 = 24이므로 24를 return 하면 됩니다.
+
+
+*/
+
+public class 자릿수더하기 {
+
+    // 내 풀이
+    public static int solution(int n) {
+      String str = String.valueOf(n);
+      int result = 0;
+      for (int i = 0; i < str.length(); i++) result += (str.charAt(i) -'0');
+      return result;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(solution(123));
+        System.out.println(solution(987));
+    }
+}
