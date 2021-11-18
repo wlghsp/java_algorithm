6922238fe1f749aa6afc41969fccf437598315ef
wlghsp <wlghsp@gmail.com>
2021-11-18 18:01:05 +0900

    이상한문자만들기 완료

--- src/programmers/level1/이상한문자만들기.java
+++ src/programmers/level1/이상한문자만들기.java
@@ -27,6 +27,22 @@ public class 이상한문자만들기 {
 
     static String solution(String s) {
         String answer = "";
+        String[] arr = s.split("");
+        int idx = 0; // 인덱스
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(" ")) {
+                idx = 0;
+            }
+            else if (idx % 2 == 0) {
+                arr[i] = arr[i].toUpperCase();
+                idx++;
+            }
+            else if (idx % 2 == 1) {
+                arr[i] = arr[i].toLowerCase();
+                idx++;
+            }
+            answer += arr[i];
+        }
         return answer;
     }
 
