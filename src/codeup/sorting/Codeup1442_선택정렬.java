1a50a1ae608918a66c70d4d34bcbae3633d37407
wlghsp <wlghsp@gmail.com>
2021-11-25 15:32:57 +0900

    코드업1442 선택정렬

--- src/codeup/sorting/Codeup1442_선택정렬.java
+++ src/codeup/sorting/Codeup1442_선택정렬.java
@@ -2,11 +2,11 @@ package codeup.sorting;
 
 /*
 5
-1
 3
 2
-5
 4
+5
+1
 
 
 오름차순 선택 정렬은 '가장 작은 원소를 찾아 첫번째 위치로 옮기고, 남은 원소를 또 탐색하여 그 다음 작은 원소를 찾아 두번째 위치로 옮기고, ... 이런식으로 정렬하는 방식'이다.
@@ -18,7 +18,7 @@ import java.io.*;
 public class Codeup1442_선택정렬 {
 
     static int[] a = new int[10001];
-    static int n, temp, min;
+    static int n, min;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -28,15 +28,15 @@ public class Codeup1442_선택정렬 {
             a[i] = Integer.parseInt(br.readLine());
         }
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i < n; i++) {
             min = i;
-            for (int j = i + 1; j < n; j++) {
-                if (a[j] > a[j+1])
+            for (int j = i + 1; j <= n; j++) {
+                if (a[j] < a[min])
                 {
-                    temp = a[j];
-                    a[j] = a[j+1];
-                    a[j+1] = temp;
+                    min = j; // 최솟값의 인덱스 찾는 과정
                 }
+            // 최솟값 인덱스와 swap하기
+            swap(min, i);
             }
         }
 
@@ -53,4 +53,11 @@ public class Codeup1442_선택정렬 {
 
 
     }
+
+    public static void swap(int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
 }
