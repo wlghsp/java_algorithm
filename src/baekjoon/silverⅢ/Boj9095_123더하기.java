cad4827b90e74841001bd28db946d520136093f5
wlghsp <wlghsp@gmail.com>
2021-12-05 22:34:03 +0900

    백준

--- src/baekjoon/silverⅢ/Boj9095_123더하기.java
+++ src/baekjoon/silverⅢ/Boj9095_123더하기.java
@@ -21,10 +21,9 @@ public class Boj9095_123더하기 {
         }
         int t = Integer.parseInt(br.readLine());
 
-        while (t>0) {
+        while (t-- >0) {
             int n = Integer.parseInt(br.readLine());
             System.out.println(d[n]);
-            t--;
         }
     }
 }
--- src/codeplus/Boj4375_1.java
+++ src/codeplus/Boj4375_1.java
@@ -1,2 +1,24 @@
-package codeplus;public class Boj4375_1 {
+package codeplus;
+
+import java.util.Scanner;
+
+public class Boj4375_1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextInt()) {  // 값이 들어 올때마다 진행
+            int n = sc.nextInt();
+            int num = 0;
+            for (int i = 1; ; i++) {
+                num = num * 10 + 1;  // num은 전단계의 나머지
+                num %= n; //  n으로 나눈 나머지
+                if (num == 0) { // 나머지가 0이라면 이를 출력하고 탈출
+                    System.out.println(i);
+                    break;
+                }
+            }
+        }
+
+    }
 }
