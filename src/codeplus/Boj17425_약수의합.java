5cf97ab8b9903f46748ee0411f15b292b101afbb
wlghsp <wlghsp@gmail.com>
2021-12-08 20:37:44 +0900

    약수의합

--- src/codeplus/Boj17425_약수의합.java
+++ src/codeplus/Boj17425_약수의합.java
@@ -1,20 +1,39 @@
 package codeplus;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 
 // https://bbangson.tistory.com/53
 public class Boj17425_약수의합 {
-    public static final int MAX = 1000000;
+    static final int MAX = 1000000;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        boolean[] check = new boolean[MAX + 1];
-        ArrayList<Integer> prime = new ArrayList<>();
-        check[0] = check[1] = true;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        long[] d = new long[MAX+1];
+        for (int i = 1; i <= MAX; i++) {
+            d[i] = 1;
+        }
+
+        for (int i = 2; i <= MAX; i++) {
+            for (int j = 1; i*j <= MAX; j++) {
+                d[i*j] += i;
+            }
+        }
+
+        long[] s= new long[MAX+1];
+        for (int i = 1; i <= MAX; i++) {
+            s[i] = s[i-1]+d[i];
+        }
         int t = Integer.parseInt(br.readLine());
+        while (t-- >0) {
+            int n = Integer.parseInt(br.readLine());
+            bw.write(s[n] + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
 
 
 
