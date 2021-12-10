3e8cb6380d5e600493d3467e49ea5c69a36c01ba
wlghsp <wlghsp@gmail.com>
2021-12-10 11:32:18 +0900

    일곱난쟁이 | 사탕게임

---
+++ src/codeplus/bruteForce/Boj3085_사탕게임.java
@@ -0,0 +1,14 @@
+package codeplus.bruteForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj3085_사탕게임 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
