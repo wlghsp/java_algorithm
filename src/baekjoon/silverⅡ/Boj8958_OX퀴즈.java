9e8adcc1b568351b41be5774b156f3e36fb159fe
CHOIJIHO <wlghsp@gmail.com>
2022-02-10 21:52:44 +0900

    ox 퀴즈 다른 풀이 참고

--- src/baekjoon/silverⅡ/Boj8958_OX퀴즈.java
+++ src/baekjoon/silverⅡ/Boj8958_OX퀴즈.java
@@ -1,5 +1,6 @@
 package baekjoon.silverⅡ;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,21 +9,44 @@ public class Boj8958_OX퀴즈 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+//
+//        for (int i = 0; i < N; i++) {
+//            String s = br.readLine();
+//            int sum = 0, num = 0;
+//            for (int j = 0; j < s.length(); j++) {
+//                char c = s.charAt(j);
+//                if (c == 'X') {
+//                    num = 0;
+//                    continue;
+//                } else {
+//                    num++;
+//                    sum += num;
+//                }
+//            }
+//            System.out.println(sum);
+//        }
+
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < N; i++) {
-            String s = br.readLine();
-            int sum = 0, num = 0;
-            for (int j = 0; j < s.length(); j++) {
-                char c = s.charAt(j);
-                if (c == 'X') {
-                    num = 0;
-                    continue;
-                } else {
-                    num++;
-                    sum += num;
+
+            int cnt = 0; // 연속횟수
+            int sum = 0; // 누적합산
+
+            for (byte value:br.readLine().getBytes()
+                 ) {
+                if (value =='O') {
+                    cnt ++;
+                    sum+= cnt;
+                }
+                else{
+                    cnt = 0;
                 }
             }
-            System.out.println(sum);
+            sb.append(sum+"\n");
         }
+
+        System.out.println(sb);
+
     }
 }
