6f049127b8ae0c7ea0adeb82f958b08d54a810d9
wlghsp <wlghsp@gmail.com>
2022-02-10 21:19:47 +0900

    단어의개수, 알파벳찾기

---
+++ src/baekjoon/bronze/bronzeⅡ/Boj10809_알파벳찾기.java
@@ -0,0 +1,21 @@
+package baekjoon.bronze.bronzeⅡ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Boj10809_알파벳찾기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        int[] alphabets = new int[26];
+        Arrays.fill(alphabets, -1); // -1로 초기화
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+        }
+    }
+}
