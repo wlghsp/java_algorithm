1acd0e8cab9cffeaa756d71fee4c6a2f839ae915
wlghsp <wlghsp@gmail.com>
2022-03-02 18:31:00 +0900

    백준 문제 풀이 3월 2일

--- src/oneday_baekjoon/Boj10951_AplusB4.java
+++ src/oneday_baekjoon/Boj10951_AplusB4.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Boj10951 {
+public class Boj10951_AplusB4 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
---
+++ src/oneday_baekjoon/Boj1110_더하기사이클.java
@@ -0,0 +1,23 @@
+package oneday_baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Boj1110_더하기사이클 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        /**
+         * 0<= n <= 99
+         *  if n <= 0:
+         *   '0'+n
+         *   n
+         *  else: 10의자리 + 1의 자리
+         *  sum의 오른쪽 자리 수 + n 오른쪽 자리 수 = newNumber
+         *
+         *  if newNumber == original: break
+         *
+         *  print(cycle)
+         */
+    }
+}
