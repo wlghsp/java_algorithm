63adf49e2afa82f6f084dd04eff91e740affd34a
CHOIJIHO <wlghsp@gmail.com>
2022-03-08 08:26:19 +0900

    인프런 4. 단어뒤집기

---
+++ src/inflearn/q5_특정문자뒤집기/Main.java
@@ -0,0 +1,26 @@
+package inflearn.q5_특정문자뒤집기;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public String solution(String str) {
+        String answer = "";
+
+        char[] s = str.toCharArray();
+        for (char c : s) {
+            
+        }
+
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(T.solution(str));
+    }
+}
