db06c6707d493c37adf85d552ee9fe195fd5e6da
CHOIJIHO <wlghsp@gmail.com>
2022-03-07 23:55:36 +0900

    코드 정리

--- src/inflearn/q1_문자찾기/Main.java
+++ src/inflearn/q1_문자찾기/Main.java
@@ -4,16 +4,12 @@ package inflearn.q1_문자찾기;
 import java.util.Scanner;
 
 public class Main {
-
     public int solution(String str, char c) {
         int answer = 0;
         // Idea *** 전부 대문자 or 소문자로 변환 ****
-
         str = str.toUpperCase();
         c = Character.toUpperCase(c);
-        /*for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == c) answer++;
-        }*/
+
         for (char x : str.toCharArray()) {
             if (x == c) answer++;
         }
---
+++ src/inflearn/q4_단어뒤집기/Main.java
@@ -0,0 +1,19 @@
+package inflearn.q4_단어뒤집기;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public void solution(int n, String[] arr) {
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Main T = new Main();
+
+        int n = sc.nextInt();
+
+    }
+}
