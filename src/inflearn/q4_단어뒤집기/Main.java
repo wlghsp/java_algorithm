007459df121bc65a5b48fddd526e2e4d42e89bb6
CHOIJIHO <wlghsp@gmail.com>
2022-03-08 08:26:34 +0900

    인프런 5. 특정 문자 뒤집기 풀이중

--- src/inflearn/q4_단어뒤집기/Main.java
+++ src/inflearn/q4_단어뒤집기/Main.java
@@ -4,16 +4,22 @@ import java.util.Scanner;
 
 public class Main {
 
-    public void solution(int n, String[] arr) {
-
+    public void solution(String[] arr) {
+        for (String s : arr) {
+            System.out.println(new StringBuilder(s).reverse());
+        }
 
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Main T = new Main();
-
         int n = sc.nextInt();
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.next();
+        }
+        T.solution(arr);
 
     }
 }
