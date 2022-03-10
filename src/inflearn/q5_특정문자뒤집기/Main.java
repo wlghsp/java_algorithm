2d3a40e5212f9a139f9b652c789dbd814903d8c3
CHOIJIHO <wlghsp@gmail.com>
2022-03-10 12:50:33 +0900

    인프런 5. 특정 문자 뒤집기 완료

--- src/inflearn/q5_특정문자뒤집기/Main.java
+++ src/inflearn/q5_특정문자뒤집기/Main.java
@@ -5,17 +5,32 @@ import java.util.Scanner;
 public class Main {
 
     public String solution(String str) {
-        String answer = "";
-
+        String answer= "";
+        // 특정문자만 뒤집을 때,
         char[] s = str.toCharArray();
-        for (char c : s) {
-            
-        }
+        int lt = 0, rt = str.length()-1;
 
+        while (lt<rt) {
+            if(!Character.isAlphabetic(s[lt])) {
+                lt++;
+            } else if (!Character.isAlphabetic(s[rt])){
+                rt--;
+            } else {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+        }
+        /*for (int i = 0; i < s.length; i++) {
+            answer += s[i];
+        }*/
+        // String.valueOf로 char[]배열을 String 으로 변환할 수 있음
+        answer = String.valueOf(s);
 
         return answer;
     }
-
 
     public static void main(String[] args) {
         Main T = new Main();
