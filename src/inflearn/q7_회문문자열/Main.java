54a427c66a038e4fdb5c77c554d161d959c9c403
CHOIJIHO <wlghsp@gmail.com>
2022-03-10 14:44:41 +0900

    인프런 7.회문문자열

---
+++ src/inflearn/q7_회문문자열/Main.java
@@ -0,0 +1,26 @@
+package inflearn.q7_회문문자열;
+
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public void solution(String str) {
+
+        str = str.toLowerCase(Locale.ROOT);
+        String str1 = new StringBuilder(str).reverse().toString();
+
+        if (str.equals(str1)) {
+            System.out.println("YES");
+        } else System.out.println("NO");
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        Main T = new Main();
+        T.solution(s);
+    }
+}
