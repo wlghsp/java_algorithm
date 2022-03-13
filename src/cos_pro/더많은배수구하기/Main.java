2af1126501b93ef6c05ad29c77bf7fba35a17a5a
CHOIJIHO <wlghsp@gmail.com>
2022-03-13 22:38:34 +0900

    COS PRO2급 문제 더 많은 배수 구하기

---
+++ src/cos_pro/더많은배수구하기/Main.java
@@ -0,0 +1,17 @@
+package cos_pro.더많은배수구하기;
+
+public class Main {
+
+    public String solution(int[] arr){
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main sol = new Main();
+        int[] arr = {2, 3, 6, 9, 12, 15, 10, 20, 22, 25};
+        String ret = sol.solution(arr);
+
+        System.out.println("ret = " + ret);
+    }
+}
