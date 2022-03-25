b937354a0c262aa081d863e8b07c381f1134745e
CHOIJIHO <wlghsp@gmail.com>
2022-03-26 08:47:16 +0900

    인프런 12. 암호

---
+++ src/inflearn/q12_암호/Main.java
@@ -0,0 +1,24 @@
+package inflearn.q12_암호;
+
+import java.util.Scanner;
+
+public class Main {
+    private String solution(int n, String s) {
+        String answer = "";
+        for (int i = 0; i < n; i++) {
+            String tmp = s.substring(0, 7).replace('#', '1').replace('*', '0');
+            int num = Integer.parseInt(tmp, 2);
+            answer += (char) num;
+            s = s.substring(7);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = sc.next();
+        System.out.println(T.solution(n, s));
+    }
+}
