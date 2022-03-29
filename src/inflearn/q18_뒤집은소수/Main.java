6eac24fff9f729f742c034043b4c5341ba187faa
wlghsp <wlghsp@gmail.com>
2022-03-29 13:42:17 +0900

    인프런 18. 뒤집은 소수

--- src/inflearn/q18_뒤집은소수/Main.java
+++ src/inflearn/q18_뒤집은소수/Main.java
@@ -10,12 +10,13 @@ public class Main {
             StringBuilder temp = new StringBuilder(x);
             String reversed = temp.reverse().toString();
             int reversedInt = Integer.parseInt(reversed);
-            if (isPrimeNumber(reversedInt)&& reversedInt != 1) sb.append(reversedInt).append(" ");
+            if (isPrimeNumber(reversedInt)) sb.append(reversedInt).append(" ");
         }
         System.out.println(sb);
     }
 
     public boolean isPrimeNumber(int x) {
+        if (x == 1) return false;
         for (int i = 2; i <= (int) Math.sqrt(x); i++) {
             if (x % i == 0) return false;
         }
---
+++ src/inflearn/q18_뒤집은소수/Main2.java
@@ -0,0 +1,44 @@
+package inflearn.q18_뒤집은소수;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main2 {
+    private ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        for (int i = 0; i<n; i++) {
+            int tmp = arr[i];
+            int res = 0;
+            while(tmp > 0) {
+                int t = tmp%10; // 1의 자리 숫자 구하고
+                res = res*10 + t; // 1의 자리 숫자를 새로운 숫자에 넣어준다.
+                tmp = tmp/10; // 1의 자리를 제한 새로운 tmp로 위 과정을 반복한다.
+            }
+            if (isPrime(res)) answer.add(res);
+
+        }
+        return answer;
+    }
+
+    public boolean isPrime(int num) {
+        if (num ==1) return false;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Main2 T = new Main2();
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int x : T.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
