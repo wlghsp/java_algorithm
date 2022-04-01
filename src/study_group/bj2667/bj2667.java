f8a96ca0bb5f2f6282cdd097564d8284d5b32573
wlghsp <wlghsp@gmail.com>
2022-04-01 17:55:41 +0900

    백준 2667 단지번호 붙이기 풀이 -ing

---
+++ src/study_group/bj2667/bj2667.java
@@ -0,0 +1,21 @@
+package study_group.bj2667;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj2667 {
+
+    static int N;
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        map = new int[N + 1][N + 1];
+
+
+
+
+    }
+}
