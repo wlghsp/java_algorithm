0c1438a99039dec69c826d7ae58297a222af1cfb
CHOIJIHO <wlghsp@gmail.com>
2022-04-03 15:36:57 +0900

    인프런 23.임시반장 정하기 풀기 시작

---
+++ src/inflearn/q23_임시반장정하기/Main.java
@@ -0,0 +1,15 @@
+package inflearn.q23_임시반장정하기;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int numberOfStudents = sc.nextInt();
+
+
+
+    }
+}
