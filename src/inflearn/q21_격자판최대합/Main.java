9151ef242a0116bace4f2e9dd3356d73b0bbe9a3
CHOIJIHO <wlghsp@gmail.com>
2022-04-03 09:20:49 +0900

    인프런 21.격자판최대합

--- src/inflearn/q21_격자판최대합/Main.java
+++ src/inflearn/q21_격자판최대합/Main.java
@@ -4,8 +4,28 @@ import java.util.Scanner;
 
 public class Main {
 
-    private int solution(int[][] m) {
-        int answer = 0;
+    private int solution(int n, int[][] m) {
+        int answer = Integer.MIN_VALUE;
+        int sum1, sum2;
+        // 각 행의 합, 각 열의 합마다 최대값 비교
+        for (int i = 0; i < n; i++) {
+            sum1 = sum2 = 0;
+            for (int j = 0; j < n; j++) {
+                sum1 += m[i][j];
+                sum2 += m[j][i];
+            }
+            answer = Math.max(answer, sum1);
+            answer = Math.max(answer, sum2);
+        }
+        sum1 = sum2 = 0;
+
+        // 대각선의 합 구하고 비교하기
+        for (int i = 0; i < n; i++) {
+            sum1 += m[i][i];
+            sum2 += m[i][n - i - 1];
+        }
+        answer = Math.max(answer, sum1);
+        answer = Math.max(answer, sum2);
 
         return answer;
     }
@@ -21,6 +41,6 @@ public class Main {
                 map[i][j] = sc.nextInt();
             }
         }
-        System.out.println(T.solution(map));
+        System.out.println(T.solution(n,map));
     }
 }
