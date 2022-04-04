8fba239807bdc7a11f16f8d023d0ab5271c9e41a
CHOIJIHO <wlghsp@gmail.com>
2022-04-04 18:48:56 +0900

    백준 1260

--- src/study_group/graph_search/finished/bj1260_DFS와BFS/bj1260_AdjacencyList.java
+++ src/study_group/graph_search/finished/bj1260_DFS와BFS/bj1260_AdjacencyList.java
@@ -1,15 +1,14 @@
-package study_group.bj1260;
+package study_group.graph_search.finished.bj1260_DFS와BFS;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class bj1260_AdjacencyMatrix {
-
-    // 인접행렬 방식
+public class bj1260_AdjacencyList {
+    // 인접리스트 방식
     public static int n, m, v;
-    public static int[][] graph;
+    public static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
     public static boolean[] visited;
 
     // DFS 함수
@@ -19,10 +18,8 @@ public class bj1260_AdjacencyMatrix {
         System.out.print(x + " "); // 방문한 노드 출력
 
         // 현재 노드와 연결된 다른 노드를 재귀적으로 방문
-        for (int i = 1; i <= n; i++) {
-            if (graph[x][i] == 1 && !visited[i]){
-                dfs(i);
-            }
+        for(int value : graph.get(x)) {
+            if (!visited[value]) dfs(value);
         }
     }
 
@@ -39,10 +36,10 @@ public class bj1260_AdjacencyMatrix {
             int x = q.poll();
             System.out.print(x + " ");
             // 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
-            for(int i = 1; i<= n; i++) {
-                if (graph[x][i] == 1 &&!visited[i]) { //간선이 존재하고 방문하지 않았다면
-                    visited[i] = true;
-                    q.offer(i);
+            for(int e: graph.get(x)) {
+                if (!visited[e]) {
+                    visited[e] = true;
+                    q.offer(e);
                 }
             }
         }
@@ -57,19 +54,22 @@ public class bj1260_AdjacencyMatrix {
         v = Integer.parseInt(st.nextToken()); // 탐색 시작 정점
 
         visited = new boolean[n+1];
-        graph = new int[n + 1][n + 1];
 
+        // 그래프 초기화
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            // 무향(양방향) 그래프
-            graph[x][y] = 1; // 1은 간선이 존재, 나머지는 초기화로 인해 0이다
-            graph[y][x] = 1;
+            graph.get(x).add(y);
+            graph.get(y).add(x);
         }
-
-//        System.out.println(Arrays.deepToString(graph));
+        for (int i = 0; i <= n; i++) {
+            Collections.sort(graph.get(i));
+        }
         dfs(v);
         visited = new boolean[n+1];
         System.out.println();
--- src/study_group/graph_search/finished/bj1260_DFS와BFS/bj1260_AdjacencyMatrix.java
+++ src/study_group/graph_search/finished/bj1260_DFS와BFS/bj1260_AdjacencyMatrix.java
@@ -1,14 +1,15 @@
-package study_group.bj1260;
+package study_group.graph_search.finished.bj1260_DFS와BFS;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class bj1260_AdjacencyList {
-    // 인접리스트 방식
+public class bj1260_AdjacencyMatrix {
+
+    // 인접행렬 방식
     public static int n, m, v;
-    public static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    public static int[][] graph;
     public static boolean[] visited;
 
     // DFS 함수
@@ -18,8 +19,10 @@ public class bj1260_AdjacencyList {
         System.out.print(x + " "); // 방문한 노드 출력
 
         // 현재 노드와 연결된 다른 노드를 재귀적으로 방문
-        for(int value : graph.get(x)) {
-            if (!visited[value]) dfs(value);
+        for (int i = 1; i <= n; i++) {
+            if (graph[x][i] == 1 && !visited[i]){
+                dfs(i);
+            }
         }
     }
 
@@ -36,10 +39,10 @@ public class bj1260_AdjacencyList {
             int x = q.poll();
             System.out.print(x + " ");
             // 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
-            for(int e: graph.get(x)) {
-                if (!visited[e]) {
-                    visited[e] = true;
-                    q.offer(e);
+            for(int i = 1; i<= n; i++) {
+                if (graph[x][i] == 1 &&!visited[i]) { //간선이 존재하고 방문하지 않았다면
+                    visited[i] = true;
+                    q.offer(i);
                 }
             }
         }
@@ -54,22 +57,19 @@ public class bj1260_AdjacencyList {
         v = Integer.parseInt(st.nextToken()); // 탐색 시작 정점
 
         visited = new boolean[n+1];
+        graph = new int[n + 1][n + 1];
 
-        // 그래프 초기화
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
-        }
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            graph.get(x).add(y);
-            graph.get(y).add(x);
+            // 무향(양방향) 그래프
+            graph[x][y] = 1; // 1은 간선이 존재, 나머지는 초기화로 인해 0이다
+            graph[y][x] = 1;
         }
-        for (int i = 0; i <= n; i++) {
-            Collections.sort(graph.get(i));
-        }
+
+//        System.out.println(Arrays.deepToString(graph));
         dfs(v);
         visited = new boolean[n+1];
         System.out.println();
