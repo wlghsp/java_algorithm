af535280e880f923ab23375d88b2c44c72055003
wlghsp <wlghsp@gmail.com>
2022-04-05 18:03:59 +0900

    백준14502 연구소

---
+++ src/study_group/graph_search/bj14502_연구소/bj14502_연구소.java
@@ -0,0 +1,16 @@
+package study_group.graph_search.bj14502_연구소;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class bj14502_연구소 {
+
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+
+
+    }
+}
