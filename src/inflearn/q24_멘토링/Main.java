27729cd41efc78c50886530946aa9d980b29ed55
wlghsp <wlghsp@gmail.com>
2022-04-07 18:01:27 +0900

    인프런 멘토링 풀이 시작

---
+++ src/inflearn/q24_멘토링/Main.java
@@ -0,0 +1,27 @@
+package inflearn.q24_멘토링;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public int solution(int[][] arr) {
+        int answer = 0;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println(T.solution(arr));
+    }
+}
