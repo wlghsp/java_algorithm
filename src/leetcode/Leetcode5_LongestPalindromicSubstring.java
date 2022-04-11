e1c0b8610fbb55a712c18a15e6388c68f70e899b
wlghsp <wlghsp@gmail.com>
2022-04-11 17:06:03 +0900

    Leetcode5_LongestPalindromicSubstring 학습 중

---
+++ src/leetcode/Leetcode5_LongestPalindromicSubstring.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+public class Leetcode5_LongestPalindromicSubstring {
+
+    private int lo, maxLen;
+
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+
+        for (int i = 0; i < len - 1; i++) {
+            extendedPalindrome(s, i, i); //  assume odd length, try to extend Palindrome
+            extendedPalindrome(s, i, i+1); //  assume even length
+        }
+        return s.substring(lo, lo + maxLen);
+    }
+
+    private void extendedPalindrome(String s, int j, int k) {
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+        }
+
+        if (maxLen < k - j -1){
+            lo = j + 1;
+            maxLen = k - j - 1;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Leetcode5_LongestPalindromicSubstring T = new Leetcode5_LongestPalindromicSubstring();
+        String s1 = "babad";
+        String s2 = "cbbd";
+        System.out.println(T.longestPalindrome(s1));
+        System.out.println(T.longestPalindrome(s2));
+    }
+}
