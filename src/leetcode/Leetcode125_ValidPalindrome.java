ef256abba5502c449a039a3efba19b734a45b132
wlghsp <wlghsp@gmail.com>
2022-04-11 16:12:53 +0900

    Leetcode125_ValidPalindrome

--- src/leetcode/Leetcode125_ValidPalindrome.java
+++ src/leetcode/Leetcode125_ValidPalindrome.java
@@ -1,16 +1,20 @@
 package leetcode;
 
+import java.util.Locale;
+
 public class Leetcode125_ValidPalindrome {
 
     public boolean isPalindrome(String s) {
 
-        return true;
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        String reversed =  new StringBuilder(s).reverse().toString();
+        return s.equals(reversed);
     }
 
     public static void main(String[] args) {
         Leetcode125_ValidPalindrome T = new Leetcode125_ValidPalindrome();
 
-        String s1 = "A man, a plan, a canal: Panama";
+        String s1 = "0P";
         String s2 = "race a car";
         String s3 = " ";
 
