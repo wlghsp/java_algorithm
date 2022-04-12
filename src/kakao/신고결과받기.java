5a3520d9a0d9387c98aa66dad5d7ba4fb90ee28a
CHOIJIHO <wlghsp@gmail.com>
2022-04-13 08:33:41 +0900

    신고결과받기, 키패드누르기 시작

---
+++ src/kakao/신고결과받기.java
@@ -0,0 +1,25 @@
+package kakao;
+
+
+// https://youtu.be/ncCUNOJUn9Q
+//https://programmers.co.kr/learn/courses/30/lessons/92334
+public class 신고결과받기 {
+
+    private int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[0];
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        신고결과받기 T = new 신고결과받기();
+        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+        String[] report = {"muzi frodo", "apeach frodo", "frodo nexo", "muzi neo", "apeach muzi"};
+
+        int k = 2;
+
+        T.solution(id_list, report, k);
+    }
+
+
+}
---
+++ src/kakao/키패드누르기.java
@@ -0,0 +1,13 @@
+package kakao;
+
+// https://programmers.co.kr/learn/courses/30/lessons/67256
+// https://youtu.be/jFv2LRVCK6s
+public class 키패드누르기 {
+
+
+    public static void main(String[] args) {
+        키패드누르기 T = new 키패드누르기();
+
+
+    }
+}
