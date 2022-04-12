e5a912f459a88cd44800af97362eabd43c7bf4d7
wlghsp <wlghsp@gmail.com>
2022-04-12 15:39:45 +0900

    add : process of solution
    - Leetcode206_ReverseLinkedList
    - Leetcode326_PowerOfThree

--- src/leetcode/Leetcode326_PowerOfThree.java
+++ src/leetcode/Leetcode326_PowerOfThree.java
@@ -5,13 +5,18 @@ public class Leetcode326_PowerOfThree {
 
     /*
      3의 제곱수 판단 true 아니면 false
+     1. 3으로 나눌 수 있으면 계속 3으로 나누고 3으로 나눈 수를 할당한다.
+     2. 3의 제곱수라면 마지막에 n이 3이므로 n은 1로 반복문이 종료된다. 따라서 return 에서 n==1일지 확인한다.
+     3. n 이 0이라면 false를 반환한다.
      */
     public boolean isPowerOfThree(int n) {
         if (n == 0) return false;
         while (n % 3 == 0) {
+
             n /= 3;
         }
         return n == 1;
+
     }
 
 
