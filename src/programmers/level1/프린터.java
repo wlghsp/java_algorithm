dc8a361795e211df40205be0759cf157402136a4
CHOIJIHO <wlghsp@gmail.com>
2022-05-06 07:50:25 +0900

    프로그래머스 프린터 문제 파일 생성

---
+++ src/programmers/level1/프린터.java
@@ -0,0 +1,20 @@
+package programmers.level1;
+
+public class 프린터 {
+
+
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        프린터 T = new 프린터();
+
+
+
+    }
+}
