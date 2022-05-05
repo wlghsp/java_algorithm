6da2f31dcd9876976276a150bb5d7081b6f629b1
CHOIJIHO <wlghsp@gmail.com>
2022-05-05 16:05:35 +0900

    백준 3085 사탕게임 - 몇달전에 걍 답보고 옮겨 치기만 했는데 와 이게 이제야 이해가 가고 코드가 보인다. 놀랍다. - 일단 2차원배열에 각 문자를 담아 둔다. - 2중 for문을 돌려서 행, 열 둘 다 교환 → 체크 → 원복을 하며   사탕갯수의 최댓값을 찾는다. - 체크는 행, 열을 체크한다. 옆칸과 같다면 cnt를 증가시키고 이를 최댓값인지 체크한다. 다르다면 바로 cnt를 1로 초기화한다.

---
+++ src/cs_senpai/boj3085_사탕게임/Main.java
@@ -0,0 +1,91 @@
+package cs_senpai.boj3085_사탕게임;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int N;
+    static char[][] board;
+    static int ans;
+
+    public static void search() {
+        // 행 체크
+        // 시작점을 포함하므로 cnt는 1개부터 시작
+        for (int i = 0; i < N; i++) {
+            int cnt = 1;
+            for (int j = 1; j < N; j++) {
+                if (board[i][j-1] == board[i][j]) {
+                    cnt++;
+                    ans = Math.max(ans, cnt);
+                } else {
+                    cnt = 1;
+                }
+            }
+        }
+
+        // 열 체크
+        for (int j = 0; j < N; j++) {
+            int cnt = 1;
+            for (int i = 1; i < N; i++) {
+                if (board[i-1][j] == board[i][j]) {
+                    cnt++;
+                    ans = Math.max(ans, cnt);
+                } else {
+                    cnt = 1;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        board = new char[N][N];
+
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < N; j++) {
+                board[i][j] = s.charAt(j);
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (j + 1 < N) {
+                    char temp = board[i][j];
+                    board[i][j] = board[i][j+1];
+                    board[i][j+1] = temp;
+
+                    // →, ↓ 체크
+                    search();
+
+                    // 원상복구
+                    temp = board[i][j+1];
+                    board[i][j+1] = board[i][j];
+                    board[i][j] = temp;
+
+                }
+
+                if (i + 1 < N) { // 열 교환
+                    char temp = board[i][j];
+                    board[i][j] = board[i+1][j];
+                    board[i+1][j] = temp;
+
+                    // →, ↓ 체크
+                    search();
+
+                    // 원상복구
+                    temp = board[i+1][j];
+                    board[i+1][j] = board[i][j];
+                    board[i][j] = temp;
+                }
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
