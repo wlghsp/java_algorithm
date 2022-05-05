bada193be1a2f1c50e69a72f418c89b505063790
CHOIJIHO <wlghsp@gmail.com>
2022-05-06 07:56:04 +0900

    백준 1931 회의실 배정
    * 풀이
    - 2차원 배열로 시작시간, 종료시간을 입력하고 정렬조건에서 종료시간을 기준으로 정렬하고 종료시간이 같을 경우에 시작시간으로 정렬한단는 것을 Comparator를 람다로 작성만 하면 됐다.
    - 물론 코드야 짤 수 있지만 처음에 저런 발상을 생각하기도 어려웠다는게 내겐 좀 어려운 느낌이다.
    - 다음에 이런 문제 나오면 풀이 보지 않고 혼자 풀려고 노력하겠다.
    * 생각
    - 이 문제 접한지 몇년 전인데 그때는 전혀 이해도 못하겠고 해서 걍 포기했던ㅜㅜ
    - 그때는 그냥 알고리즘은 난 포기로 생각해서 걍 공부도 안했다.
    - 물론 이번에는 드디어 이해는 했지만 다음에도 이 비슷한 문제로 내가 풀어낼거라고 자신하기 어렵다.
    - 다음에는 꼭 내가 해보겠다.

---
+++ src/cs_senpai/boj1931_회의실배정/Main.java
@@ -0,0 +1,47 @@
+package cs_senpai.boj1931_회의실배정;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        /*
+		  time[][0] 은 시작시점을 의미
+		  time[][1] 은 종료시점을 의미
+		*/
+        int[][] time = new int[N][2];
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            time[i][0] = start;
+            time[i][1] = end;
+        }
+
+        Arrays.sort(time, ((o1, o2) -> {
+            if (o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            } else {
+                return o1[1] - o2[1];
+            }
+        }));
+
+        int ans = 0, t = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (t <= time[i][0]) {
+                ans++;
+                t = time[i][1];
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
