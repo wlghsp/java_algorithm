75ace9f26e37f074beb32391a9f2b32397fb4e7c
CHOIJIHO <wlghsp@gmail.com>
2022-05-08 20:47:43 +0900

    프로그래머스 Level2 타겟넘버(DFS)
    * 풀이
    - 개발자취업하기님 영상으로 공부하면서 풀이 작성
    - 생각보다 이해하기 쉽고 코드도 간단함(재귀라서 그렇기도 하겠지만)
    - 탈출조건과 수행동작을 작성하여준다. 탈출조건은 숫자 배열 길이에 도달하고 타겟넘버인지 확인하고
    맞으면 answer를 증가시킨다. 아니면 그냥 return으로 빠져나옴.
    - 수행동작은 +,- 두 가지이므로 인덱스를 증가시키면서 이를 재귀함수 호출하여준다.

--- src/programmers/kakao/타겟넘버.java
+++ src/programmers/kakao/타겟넘버.java
@@ -4,7 +4,36 @@ package programmers.kakao;
 //https://programmers.co.kr/learn/courses/30/lessons/43165
 public class 타겟넘버 {
 
-    public static void main(String[] args) {
+    int[] numbers;
 
+    int target;
+    int answer;
+
+    void dfs(int index, int sum) { // sum 누적합
+        // 1. 탈출 조건
+        if (index == numbers.length) {
+            if (sum == target) answer++; // 누적합이 타겟숫자와 같다면 카운트 증가
+            return;
+        }
+        // 2. 수행 동작
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
+    }
+
+    public int solution(int[] numbers, int target) {
+        answer = 0;
+        this.numbers = numbers;
+        this.target = target;
+
+        dfs(0, 0);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 1, 1, 1, 1};
+        int target = 3;
+        타겟넘버 T = new 타겟넘버();
+        System.out.println(T.solution(numbers, target)); // 5
     }
 }
