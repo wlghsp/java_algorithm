20be75bcb7315b25cbcc1ac57a7515027ca0822d
wlghsp <wlghsp@gmail.com>
2022-05-10 17:07:01 +0900

    인프런 q30 최대길이연속부분수열
    * 풀이
    - 투포인터로 풀이
    - 실제 값을 바꾸는게 아니고 그냥 0일때 cnt값을 증가 또는 감소하여 답을 구함.

--- src/inflearn/q30_최대길이연속부분수열/Main.java
+++ src/inflearn/q30_최대길이연속부분수열/Main.java
@@ -4,13 +4,34 @@ import java.util.Scanner;
 
 public class Main {
 
+    public int solution(int n, int k, int[] arr) {
+        int answer = 0, cnt =0, lt = 0;
+
+        for (int rt = 0; rt < arr.length; rt++) {
+            if (arr[rt] == 0) cnt++; // 0이면 바꿔야하므로 카운트
+            while (cnt > k) {  // 카운트된 횟수가 k를 넘을때는
+                if (arr[lt] == 0) cnt--; // lt인덱스에 0이라면 cnt를 감소
+                lt++; // 범위를 좁히기 위해 lt는 증가시켜준다.
+            }
+            answer = Math.max(answer, rt - lt + 1); // rt에서 lt의 거리= rt-lt+1, 둘을 비교해서 최댓값을 반환한다.
+        }
+
+        return answer;
+    }
 
 
     public static void main(String[] args) {
         Main T = new Main();
 
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
 
+        System.out.println(T.solution(n, k, arr));
 
     }
 }
