3e36d22c968a4d712e1e1fe57332cf406c408c74
wlghsp <wlghsp@gmail.com>
2022-05-10 17:57:54 +0900

    Boj5622_다이얼 풀이 중

---
+++ src/baekjoon/bronze/bronzeⅡ/Boj5622_다이얼/Main.java
@@ -0,0 +1,25 @@
+package baekjoon.bronze.bronzeⅡ.Boj5622_다이얼;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String word = br.readLine();
+
+
+        for (int i = 0; i < word.length(); i++) {
+
+            switch (word.charAt(i)) {
+
+//                case 'A' : case 'B': case ''
+            }
+        }
+
+    }
+}
