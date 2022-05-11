f1f8698850c9506bc51db3a3cabf7011f59e0ab3
wlghsp <wlghsp@gmail.com>
2022-05-11 09:11:15 +0900

    Boj5622_다이얼
    * 풀이
    - switch case문으로 각 숫자의 알파벳마다 작성을 해두고 다이얼 2는 +3초부터 시작한다.

--- src/baekjoon/bronze/bronzeⅡ/Boj5622_다이얼/Main.java
+++ src/baekjoon/bronze/bronzeⅡ/Boj5622_다이얼/Main.java
@@ -12,14 +12,37 @@ public class Main {
 
         String word = br.readLine();
 
-
+        int answer = 0;
         for (int i = 0; i < word.length(); i++) {
 
             switch (word.charAt(i)) {
-
-//                case 'A' : case 'B': case ''
+                case 'A' : case 'B': case 'C':
+                    answer += 3;
+                    break;
+                case 'D' : case 'E': case 'F':
+                    answer += 4;
+                    break;
+                case 'G' : case 'H': case 'I':
+                    answer += 5;
+                    break;
+                case 'J' : case 'K': case 'L':
+                    answer += 6;
+                    break;
+                case 'M' : case 'N': case 'O':
+                    answer += 7;
+                    break;
+                case 'P' : case 'Q': case 'R': case 'S':
+                    answer += 8;
+                    break;
+                case 'T' : case 'U': case 'V':
+                    answer += 9;
+                    break;
+                case 'W' : case 'X': case 'Y': case 'Z':
+                    answer += 10;
+                    break;
             }
         }
+        System.out.println(answer);
 
     }
 }
