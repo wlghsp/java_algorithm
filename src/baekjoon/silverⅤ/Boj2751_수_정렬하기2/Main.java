264574b1e35e1c2b9f118836c1ee8df95097b998
CHOIJIHO <wlghsp@gmail.com>
2022-05-13 08:41:37 +0900

    Boj2751_수 정렬하기 2
    * 풀이
    - 카운팅소트 사용 정렬 공부하는 좋은 기회

---
+++ src/baekjoon/silverⅤ/Boj2751_수_정렬하기2/Main.java
@@ -0,0 +1,29 @@
+package baekjoon.silverⅤ.Boj2751_수_정렬하기2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+
+        boolean[] arr = new boolean[2000001];
+
+        for (int i = 0; i < N; i++) {
+            arr[Integer.parseInt(br.readLine()) + 1000000] = true;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]) {
+                sb.append(i - 1000000).append('\n');
+            }
+        }
+
+        System.out.println(sb);
+
+    }
+}
