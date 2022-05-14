7812dc4c77c8b0624e68ec75eb5720cbf33dadd8
CHOIJIHO <wlghsp@gmail.com>
2022-05-14 21:12:30 +0900

    Boj1764 듣보잡
    * 1번 풀이
    - set 2개 만들고 교집합을 구하는 retainAll을 이용
    - 교집합이 구해진 set을 리스트로 변환 후 정렬을 한다.
    
    * 2번 풀이
    - set 하나로 한쪽은 받고, 다른 쪽은 저 set에 포함된 값만 list에 담고 정렬을 한다.
    - 이게 더 빠르고 메모리도 적게 씀.

--- src/baekjoon/silverⅣ/Boj1764_듣보잡/Main2.java
+++ src/baekjoon/silverⅣ/Boj1764_듣보잡/Main2.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
+public class Main2 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,20 +13,23 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        HashSet<String> s1 = new HashSet<>();
-        HashSet<String> s2 = new HashSet<>();
+        HashSet<String> set = new HashSet<>();
 
         for (int i = 0; i < N; i++) {
-            s1.add(br.readLine());
+            set.add(br.readLine());
         }
 
+        List<String> list = new ArrayList<>();
+        // set에 포함여부 확인하고 list에 담아준다.
         for (int i = 0; i < M; i++) {
-            s2.add(br.readLine());
+            String tmp = br.readLine();
+            if (set.contains(tmp)) {
+                list.add(tmp);
+            }
         }
-        
-        s1.retainAll(s2); // retainAll 교집합
-        List<String> list = new ArrayList<>(s1);
+
         Collections.sort(list);
+
         StringBuilder sb = new StringBuilder();
         sb.append(list.size()).append('\n');
         for (String s : list) {
