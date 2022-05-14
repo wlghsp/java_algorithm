dac796f74c789bdf2855ef1ce89b5560e9c38419
wlghsp <wlghsp@gmail.com>
2022-05-14 19:48:19 +0900

    인프런 매출액의 종류 (해쉬, 슬라이딩 윈도우)
    * 풀이
    - 해쉬, 투포인터, 슬라이딩윈도우를 활용하여 풀이한다.
    - 투포인터를 활용하는 코드가 자주 나오는데 다시 복습해야겠다.
    - 해쉬에 기록해두고 슬라이딩 윈도우를 이동시키면서 해쉬에 저장된 값을 증감시킨다.

---
+++ src/inflearn/q33_매출액의종류/Main.java
@@ -0,0 +1,48 @@
+package inflearn.q33_매출액의종류;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    private ArrayList<Integer> solution(int n, int k, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        // K만큼 미리 세팅
+        for (int i = 0; i < k - 1; i++) {
+            hm.put(arr[i], hm.getOrDefault(arr[i], 0) +1);
+        }
+        // 투포인터 알고리즘, 슬라이딩 윈도우
+        int lt = 0;
+        for (int rt = k-1; rt< n; rt++) {
+            hm.put(arr[rt], hm.getOrDefault(arr[rt], 0) + 1);
+            answer.add(hm.size());
+            hm.put(arr[lt], hm.get(arr[lt]) -1);
+            if (hm.get(arr[lt]) == 0) hm.remove(arr[lt]);
+            lt++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Main T = new Main();
+        for (Integer i : T.solution(N,K,arr)) {
+            System.out.print(i + " ");
+        }
+    }
+
+}
