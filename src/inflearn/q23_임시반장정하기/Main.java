adb2735bfe7d8b46cfbd2d9b1d0515115189bc02
CHOIJIHO <wlghsp@gmail.com>
2022-05-18 21:44:38 +0900

    인프런 임시반장정하기 수정

--- src/inflearn/q23_임시반장정하기/Main.java
+++ src/inflearn/q23_임시반장정하기/Main.java
@@ -1,51 +1,53 @@
 package inflearn.q23_임시반장정하기;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
+    // 3 중 포문 사용
+    // n = 5
+    // for i 학생 1~5,
+    //      for j 학생, 1~5
+    //          for k 학년, 무조건 1~5
+    //              if  a[i][k] == a[j][k]
+    //                      cnt++;
+    // i, j가 같으면 자기자신과의 비교이지만 어차피 가장 많이 겹치는 학생을 찾는 것이므로 다 같은 조건으로 i!=j 이렇게 거를 필요 없음.
+    // 학생을 카운팅할 때 한 번만 카운팅하도록 break를 걸어준다.
 
-    public void solution(int n, int[][] table) {
-        int grade = 0;
-        int num = 0;
 
-        for (int i = 0; i < n; i++) { // 학생
-            Set<Integer> set = new HashSet<>();
-            for (int j = 0; j < 5; j++) { // 학년
-                for (int k = 0; k < n; k++) {
-                    if (i==k) continue;
-                    if (table[i][j] == table[k][j]) {
-                        System.out.printf("[(%d) 번 학생과 (%d) 학생은 (%d) 학년일 때 (%d)과 (%d)반으로 같음]   ", i+1, k+1, j+1, table[i][j], table[k][j]);
-                        set.add(k);
+    public int solution(int n, int[][] arr) {
+        int stdNum = 0; // 학생 번호
+        int max = Integer.MIN_VALUE;
+
+
+        for (int i = 1; i <= n; i++) {
+            int cnt = 0;
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= 5; k++) {
+                    if (arr[i][k] == arr[j][k]) {
+                        cnt++;
+                        break;
                     }
                 }
             }
-            System.out.printf("%d 번 학생이 총 중복되는 학생은 %d 명임", i + 1, set.size());
-
-            if (num < set.size()) {
-                num = set.size();
-                grade = i + 1;
+            if (cnt > max) {
+                max = cnt;
+                stdNum = i;
             }
-            System.out.println(num);
         }
-        if (grade == 0 ) {
-            System.out.println(1);
-        } else {
-            System.out.println(grade);
-        }
-
+        return stdNum;
     }
+
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[][] table = new int[n][5];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 5; j++) {
-                table[i][j] = sc.nextInt();
+        int[][] arr = new int[n+1][6];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 5; j++) {
+                arr[i][j] = sc.nextInt();
             }
         }
-
-       T.solution(n, table);
+        System.out.println(T.solution(n, arr));
     }
 }
