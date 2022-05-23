66047726c57e5e66f48ab4d64c91c02aa26b192a
CHOIJIHO <wlghsp@gmail.com>
2022-05-23 16:34:06 +0900

    Boj10815_숫자카드 - Hashmap 이용 풀이
    * 풀이
    - Hashmap으로만 풀이한다. 빈도를 hashmap에 저장해 놓고 containskey로 포함하는지 확인하고 get으로 찾아온다.

---
+++ src/baekjoon/silverⅣ/Boj10815_숫자카드/Main.java
@@ -0,0 +1,47 @@
+package baekjoon.silverⅣ.Boj10815_숫자카드;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] cards = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N ; i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
+        }
+        int M = Integer.parseInt(br.readLine());
+        int[] candidate = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            candidate[i] = Integer.parseInt(st.nextToken());
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int c : cards) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int c : candidate) {
+            if(map.containsKey(c)) {
+                int result = map.get(c).intValue();
+                sb.append(result).append(" ");
+            } else {
+                sb.append(0).append(" ");
+            }
+        }
+
+        System.out.println(sb);
+
+    }
+}
