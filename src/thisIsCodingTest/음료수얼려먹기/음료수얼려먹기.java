f30a9cbcf1c663d710489b3bb0bc5ba52a512423
wlghsp <wlghsp@gmail.com>
2022-05-24 17:43:11 +0900

    이코테 음료수얼려먹기
    * 풀이
    - dfs로 상하좌우를 확인한다.

---
+++ src/thisIsCodingTest/음료수얼려먹기/음료수얼려먹기.java
@@ -0,0 +1,57 @@
+package thisIsCodingTest.음료수얼려먹기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 음료수얼려먹기 {
+
+    static int[][] graph;
+    static int n,m;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        graph = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            char[] arr = br.readLine().toCharArray();
+            for (int j = 0; j < m; j++) {
+                graph[i][j] = arr[j] - '0';
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (dfs(i, j) == true) {
+                    result++;
+                }
+            }
+        }
+
+        System.out.println(result);
+
+    }
+
+
+    public static boolean dfs(int x, int y) {
+        if (x <= -1 || x >= n || y <= -1 || y >= m) return false;
+
+        if (graph[x][y] == 0) {
+            graph[x][y] = 1;
+            dfs(x - 1, y);
+            dfs(x, y - 1);
+            dfs(x, y + 1);
+            dfs(x+1, y);
+            return true;
+        }
+
+        return false;
+    }
+}
