0f33d1dd5e5c0269a895c6f7729223ec25f58636
wlghsp <wlghsp@gmail.com>
2022-05-24 17:52:34 +0900

    이코테 상하좌우 풀이 진행 중

---
+++ src/thisIsCodingTest/상하좌우/상하좌우.java
@@ -0,0 +1,26 @@
+package thisIsCodingTest.상하좌우;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 상하좌우 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int x = 1, y = 1;
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {-1, 1, 0, 0};
+        char[] move_types = {'L', 'R', 'U', 'D'};
+
+
+
+    }
+}
