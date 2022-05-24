26da37c1e01d6eee4c601bf2f1f708570fd60d43
wlghsp <wlghsp@gmail.com>
2022-05-24 18:02:43 +0900

    이코테 상하좌우
    * 풀이
    - 파이썬 풀이 그대로 가져옴
    - 문자와 같은 인덱스 좌표이동을 하고 좌표가 지도 내에 있는지 확인하고 좌표를 변경한다.

--- src/thisIsCodingTest/상하좌우/상하좌우.java
+++ src/thisIsCodingTest/상하좌우/상하좌우.java
@@ -3,6 +3,8 @@ package thisIsCodingTest.상하좌우;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class 상하좌우 {
@@ -13,14 +15,31 @@ public class 상하좌우 {
         int x = 1, y = 1;
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-
+        List<Character> plans = new ArrayList<>();
+        while (st.hasMoreTokens()) {
+            plans.add(st.nextToken().charAt(0));
+        }
 
         int[] dx = {0, 0, -1, 1};
         int[] dy = {-1, 1, 0, 0};
         char[] move_types = {'L', 'R', 'U', 'D'};
-
-
+        int nx = 0, ny = 0;
+        for (Character plan : plans) {
+            for (int i = 0; i < move_types.length; i++) {
+                if (plan == move_types[i]) {
+                    nx = x + dx[i];
+                    ny = y + dy[i];
+                }
+            }
+            if (nx < 1 || ny < 1|| nx > N || ny > N) continue;
+            x = nx;
+            y = ny;
+        }
+        System.out.println(x + " " + y);
 
     }
 }
+/*
+5
+R R R U D D
+ */
