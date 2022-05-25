5783cf29a5280b9d9f202dcb0326de443ae103d2
CHOIJIHO <wlghsp@gmail.com>
2022-05-26 08:33:28 +0900

    인프런 괄호문자제거 오늘 풀어야지

---
+++ src/inflearn/q36_괄호문자제거/Main.java
@@ -0,0 +1,25 @@
+package inflearn.q36_괄호문자제거;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+
+    public String solution(String s) {
+        String answer = "";
+        Stack<Character> stk = new Stack<>();
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        System.out.println(T.solution(s));
+    }
+}
