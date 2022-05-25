e2ffac77de135d50f7f2a606e6ff4279d9d8c2a0
CHOIJIHO <wlghsp@gmail.com>
2022-05-26 08:02:03 +0900

    인프런 K번째 큰 수
    * 풀이
    - 중복을 제거하고 숫자를 정렬하려면 TreeSet을 사용
    - 기본 오름차순 정렬, 내림차순정렬은 Collections.reverseOrder() 넣기
    - 그리고 K번째 수만 출력하면 됨

--- src/inflearn/q35_K번째큰수/Main.java
+++ src/inflearn/q35_K번째큰수/Main.java
@@ -1,16 +1,35 @@
 package inflearn.q35_K번째큰수;
 
+import java.util.Collections;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 public class Main {
 
     public int solution(int N, int K, int[] arr) {
-        int answer = 0;
+        int answer = -1;
+        // TreeSet 중복없는 숫자의 정렬, Collections.reverseOrder()를 통해 내림차순 정렬
+        TreeSet<Integer> Tset = new TreeSet<>(Collections.reverseOrder());
 
+        // 3장을 뽑을 수 있는 모든 경우, 3중 포문
+        for (int i = 0; i < N; i++) {
+            for (int j = i+1; j < N; j++) {
+                for (int l = j+1; l < N; l++) {
+                    Tset.add(arr[i] + arr[j] + arr[l]);
+                }
+            }
+        }
+
+        int idx = 1;
+        for (Integer x : Tset) {
+            if (idx == K) {
+                return x;
+            }
+            idx++;
+        }
 
         return answer;
     }
-
 
     public static void main(String[] args) {
         Main T = new Main();
