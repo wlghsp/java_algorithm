ee4d348bb2cf0f43e63d2ba8afd8be0ffc914052
CHOIJIHO <wlghsp@gmail.com>
2022-05-27 08:33:28 +0900

    인프런 쇠막대기 풀이 예정

---
+++ src/inflearn/q39_쇠막대기/Main.java
@@ -0,0 +1,22 @@
+package inflearn.q39_쇠막대기;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        System.out.println(T.solution(s));
+
+    }
+
+
+    public int solution(String s) {
+        int answer = 0;
+        return answer;
+    }
+}
