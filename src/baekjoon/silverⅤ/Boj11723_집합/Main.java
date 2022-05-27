8121dbb0e00a92eb45ec89c2b4e14f8e7d0cd9eb
CHOIJIHO <wlghsp@gmail.com>
2022-05-28 08:30:44 +0900

    Boj11723 풀이 예정

---
+++ src/baekjoon/silverⅤ/Boj11723_집합/Main.java
@@ -0,0 +1,27 @@
+package baekjoon.silverⅤ.Boj11723_집합;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    private static Set<Integer> set = new HashSet<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int M = Integer.parseInt(br.readLine());
+
+        while (M-- > 0){
+            String[] commands = br.readLine().split(" ");
+
+            String command = commands[0];
+
+
+        }
+
+    }
+}
