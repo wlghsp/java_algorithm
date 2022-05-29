52677492a83d1f5486f31d38e16e8ff0e9c723df
CHOIJIHO <wlghsp@gmail.com>
2022-05-29 11:27:01 +0900

    Boj14888 연산자끼워넣기
    * 풀이
    - 완전탐색 문제 모든 경우의 수를 다 알아봄
    - 백트래킹문제라거나 백트래킹이라는 단어에 대해 헷갈림
    - 쫌 어렵다 문제를 잘 읽고 최대 최소 구할 생각도 해야한다
    - 이런 문제도 이런 풀이에 익숙해지면 쉽게 풀 거 같다
    - 프로그래머스 타겟넘버랑 비슷한데 최댓값,최솟값 구하고 연산자 증감이 있어서 더 어려웠다
    - 다음에 다시 풀자

---
+++ src/baekjoon/silverⅠ/Boj14888_연산자끼워넣기/Main.java
@@ -0,0 +1,70 @@
+package baekjoon.silverⅠ.Boj14888_연산자끼워넣기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N; // 정수의 개수
+    static int[] arr;
+    static int[] operators = new int[4]; // 0: 덧셈, 1:뺄셈, 2: 곱셈, 3: 나눗셈
+    static int MAX = Integer.MIN_VALUE, MIN = Integer.MAX_VALUE;
+
+    public static void dfs(int num, int idx) {
+        if (idx == N){
+            MAX = Math.max(MAX, num);
+            MIN = Math.min(MIN, num);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            // 연산자 개수가 1개 이상인 경우
+            if (operators[i] > 0) {
+                operators[i]--;
+
+                switch (i) {
+                    case 0:
+                        dfs(num + arr[idx], idx + 1);
+                        break;
+                    case 1:
+                        dfs(num - arr[idx], idx + 1);
+                        break;
+                    case 2:
+                        dfs(num * arr[idx], idx + 1);
+                        break;
+                    case 3:
+                        dfs(num / arr[idx], idx + 1);
+                        break;
+                }
+
+                // 재귀호출이 종료되면 다시 해당 연산자 개수를 복구한다.
+                operators[i]++;
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 4; i++) {
+            operators[i] = Integer.parseInt(st.nextToken());
+        }
+        dfs(arr[0], 1);
+        StringBuilder sb = new StringBuilder();
+        sb.append(MAX).append('\n');
+        sb.append(MIN);
+        System.out.println(sb);
+
+    }
+}
