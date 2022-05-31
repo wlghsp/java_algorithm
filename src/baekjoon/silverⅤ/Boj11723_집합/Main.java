2eb16cd000dff5e2237019568f2257a918d5c7ac
wlghsp <wlghsp@gmail.com>
2022-05-31 14:44:59 +0900

    Boj11723 집합
    * 풀이
    - 비트마스킹으로 집합 문제 풀이
    - 비트마스킹을 학습하는 좋은 문제이다 익숙해지면 유용하게 사용할 수 있을 것이다
    - 하나하나 소홀히하지 않고 파고들어서 학습하자
    - 비트마스킹 문제에 다음에도 도전!!

--- src/baekjoon/silverⅤ/Boj11723_집합/Main.java
+++ src/baekjoon/silverⅤ/Boj11723_집합/Main.java
@@ -3,11 +3,11 @@ package baekjoon.silverⅤ.Boj11723_집합;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Main {
-
+    /*
+     비트마스킹 문제
+     */
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,25 +20,25 @@ public class Main {
             int num  = 0;
 
             switch (command) {
-                case "add": // 원소 추가  a | (1 << p)
+                case "add": // 원소 추가  a | (1 << p) x룰 추가 이미 있는 경우에는 연산을 무시
                     num = Integer.parseInt(commands[1]);
                     // num - 1 인 이유는 20이 들어왔을 때
                     // 2^19 자리가 20번째이기 때문이다.
                     bitset |= (1 << (num - 1));
                     break;
-                case "remove": // 원소 삭제 a & ~b
+                case "remove": // 원소 삭제 a & ~b x가 있으면 제외하고 없으면 무시
                     num = Integer.parseInt(commands[1]);
                     bitset &= ~(1 << (num - 1));
                     break;
-                case "check":
+                case "check": // 값이 있으면 1을 없으면 0을 출력
                     num = Integer.parseInt(commands[1]);
                     sb.append((bitset & (1 << (num - 1))) != 0 ? "1\n" : "0\n");
                     break;
-                case "toggle":
+                case "toggle": // 값이 있으면 제거하고 없으면 그 값을 추가한다
                     num = Integer.parseInt(commands[1]);
                     bitset ^= (1 << (num - 1));
                     break;
-                case "all": //
+                case "all": // 1~20이 포함된 집합으로 바꾼다
                     bitset |= (~0);
                     break;
                 case "empty": // a & 0 을 하면 전부 0으로 만드므로 공집합이 된다
