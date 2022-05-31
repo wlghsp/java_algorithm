01f5760ad69680aff9f74455414797133cc29361
wlghsp <wlghsp@gmail.com>
2022-05-31 10:14:23 +0900

    Boj11723 집합
    * 풀이
    - 비트마스킹 첫 문제
    - 비트마스킹도 반복해서 공부하면 쉽게 이해 할 수 있따!
    - 이해 못한 부분은 다음에 이해하자

--- src/baekjoon/silverⅤ/Boj11723_집합/Main.java
+++ src/baekjoon/silverⅤ/Boj11723_집합/Main.java
@@ -8,20 +8,45 @@ import java.util.Set;
 
 public class Main {
 
-    private static Set<Integer> set = new HashSet<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringBuilder sb = new StringBuilder();
         int M = Integer.parseInt(br.readLine());
-
+        int bitset = 0;
         while (M-- > 0){
             String[] commands = br.readLine().split(" ");
-
             String command = commands[0];
+            int num  = 0;
 
+            switch (command) {
+                case "add": // 원소 추가  a | (1 << p)
+                    num = Integer.parseInt(commands[1]);
+                    // num - 1 인 이유는 20이 들어왔을 때
+                    // 2^19 자리가 20번째이기 때문이다.
+                    bitset |= (1 << (num - 1));
+                    break;
+                case "remove": // 원소 삭제 a & ~b
+                    num = Integer.parseInt(commands[1]);
+                    bitset &= ~(1 << (num - 1));
+                    break;
+                case "check":
+                    num = Integer.parseInt(commands[1]);
+                    sb.append((bitset & (1 << (num - 1))) != 0 ? "1\n" : "0\n");
+                    break;
+                case "toggle":
+                    num = Integer.parseInt(commands[1]);
+                    bitset ^= (1 << (num - 1));
+                    break;
+                case "all": //
+                    bitset |= (~0);
+                    break;
+                case "empty": // a & 0 을 하면 전부 0으로 만드므로 공집합이 된다
+                    bitset &= 0;
+                    break;
+            }
 
         }
-
+        System.out.println(sb);
     }
 }
