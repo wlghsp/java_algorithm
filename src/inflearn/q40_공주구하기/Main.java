9acfe0e92913974d118dea0cd385307e45ededbf
CHOIJIHO <wlghsp@gmail.com>
2022-06-05 09:53:36 +0900

    인프런 공주구하기
    * 풀이
    - 요세푸스문제랑 똑같다. 문제 스토리만 바꾼 문제라
    큐 자료구조만 사용하면 쉽게 풀 수 있다.
    - 놀라운 건 요세푸스 문제 처음 접할때는 어렵고 이걸 어떻게 이렇게 푸나 했는데 이번에는 그때 문제 기억하고 거의 스스로 풀었다.
    - 이런 것도 훈련인가 보다 익숙해지면 쉬워진다

---
+++ src/inflearn/q40_공주구하기/Main.java
@@ -0,0 +1,37 @@
+package inflearn.q40_공주구하기;
+
+import java.util.*;
+
+public class Main {
+
+    public int solution(int N, int K) {
+        int answer = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            queue.offer(i);
+        }
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < K - 1; i++) {
+                queue.offer(queue.poll());
+            }
+            queue.poll();
+            if (queue.size() == 1) {
+                answer = queue.poll();
+            }
+        }
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Main T = new Main();
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        System.out.println(T.solution(N, K));
+
+    }
+}
