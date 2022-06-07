54a2eb2f1e5bfcc543074a96efc22a4c79eff72d
CHOIJIHO <wlghsp@gmail.com>
2022-06-07 20:48:41 +0900

    LeetCode 200 Number of Islands
    * 풀이
    - dfs 풀이 dfs를 잘 통과하면 1이  리턴된다
    - dfs는 일단 따라 적기는 쉬운데 코드 이해하기가 재귀라서 그런지 어렵다. 딱히 뭔가 특별한 처리도 없는데 문제 끝 이런 느낌

---
+++ src/leetcode/LeetCode200_NumberofIslands/Solution.java
@@ -0,0 +1,44 @@
+package leetcode.LeetCode200_NumberofIslands;
+
+public class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int numOfIslands = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    numOfIslands += dfs(grid, i, j);
+                }
+            }
+        }
+        return numOfIslands;
+    }
+
+    public int dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || grid[i].length <= j || grid[i][j] == '0') {
+            return 0;
+        }
+
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        char[][] grid = {
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+        };
+
+        System.out.println(T.numIslands(grid));
+    }
+}
