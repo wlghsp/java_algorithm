073244eee0e20bd60a16ebbca5803e6d5be3cb4f
wlghsp <wlghsp@gmail.com>
2022-06-08 16:45:45 +0900

    Boj11655_ROT13
    * 풀이
    - 알파벳을 배열로 만들지 않고 아스키코드로 풀이하였다.
    - 이런 문제도 이렇게 풀고 많이 발전했다. 예전에는 아스키코드만 봐도 어려워서 어찌할지 몰랐다. 많이 익숙해졌구나

--- src/baekjoon/bronze/bronzeⅠ/Boj11655_ROT13/Main.java
+++ src/baekjoon/bronze/bronzeⅠ/Boj11655_ROT13/Main.java
@@ -8,7 +8,27 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        String sen = br.readLine();
 
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < sen.length(); i++) {
+            char c = sen.charAt(i);
+            if (Character.isAlphabetic(c)) {
+                if (Character.isUpperCase(c)) {
+                    int num = c;
+                    char ch = (char) ((((num - 65) + 13) % 26) + 65);
+                    sb.append(ch);
+                } else {
+                    int num = c;
+                    char ch = (char) ((((num - 97) + 13) % 26) + 97);
+                    sb.append(ch);
+                }
+            } else {
+                sb.append(c);
+            }
+        }
+
+        System.out.println(sb);
 
 
     }
