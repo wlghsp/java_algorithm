8dfd0af4c4d8e43819760f3718417b664db899a6
CHOIJIHO <wlghsp@gmail.com>
2022-06-14 08:43:51 +0900

    Boj2999_비밀이메일 풀이 예정

---
+++ src/baekjoon/bronze/bronzeⅠ/Boj2999_비밀이메일/Main.java
@@ -0,0 +1,14 @@
+package baekjoon.bronze.bronzeⅠ.Boj2999_비밀이메일;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
