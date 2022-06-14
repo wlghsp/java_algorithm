bf5562e48f784fd3649c47f81437e7d3bc48c163
wlghsp <wlghsp@gmail.com>
2022-06-14 18:02:58 +0900

    Boj2999_비밀이메일
    * 풀이
    - 파이썬 풀이 그대로 가져옴
    - 최적화된지는 아직 R, C 초기화 부분이 이해가 잘 안간다.

--- src/baekjoon/bronze/bronzeⅠ/Boj2999_비밀이메일/Main.java
+++ src/baekjoon/bronze/bronzeⅠ/Boj2999_비밀이메일/Main.java
@@ -6,9 +6,29 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        String message = br.readLine();
+        int length = message.length();
+        int R = 1, C = message.length();
+        char[] chars = message.toCharArray();
+        for (int i = 2; i < message.length(); i++) {
+            if (i * i > message.length()) break;
+            if (length % i == 0) {
+                R = i;
+                C = length / i;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                sb.append(chars[j * R + i]);
+            }
+        }
+        System.out.println(sb);
 
     }
+
 }
+
