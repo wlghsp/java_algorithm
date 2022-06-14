09154924a14cd74104a20908c7bd6e7c5ee3ec62
CHOIJIHO <wlghsp@gmail.com>
2022-06-14 22:26:18 +0900

    Boj2999_비밀이메일
    * 풀이
    - 파이썬에서 좋은 풀이를 가져옴
    - i * i > N 에서 break를 시키는 부분 좋다
    - 문자출력은 원래 문제 지시대로 구현했는데, 2중for문 하나로 해결 가능하다는 것이 신기하다. ㅇ
    - 행렬의 가로로 읽어야 하므로행의 배수로 인덱스를 이동시킨다.

--- src/baekjoon/bronze/bronzeⅠ/Boj2999_비밀이메일/Main.java
+++ src/baekjoon/bronze/bronzeⅠ/Boj2999_비밀이메일/Main.java
@@ -11,16 +11,21 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String message = br.readLine();
         int length = message.length();
-        int R = 1, C = message.length();
+        int R = 1, C = length;
         char[] chars = message.toCharArray();
-        for (int i = 2; i < message.length(); i++) {
-            if (i * i > message.length()) break;
+        for (int i = 2; i < length; i++) {
+            if (i * i > length) break;
             if (length % i == 0) {
                 R = i;
                 C = length / i;
             }
         }
         StringBuilder sb = new StringBuilder();
+        /*
+        행렬의 가로로 읽어야 하므로 행의 배수로 인덱스 이동
+        행의 인덱스가 바깥쪽, 열의 인덱스 안 쪽
+
+         */
         for (int i = 0; i < R; i++) {
             for (int j = 0; j < C; j++) {
                 sb.append(chars[j * R + i]);
