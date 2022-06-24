64e464287490bffc284496ef5fc33a8d4e0b6aab
wlghsp <wlghsp@gmail.com>
2022-06-24 18:05:59 +0900

    일단 반영

--- src/inflearn/q42_응급실/Main.java
+++ src/inflearn/q42_응급실/Main.java
@@ -1,13 +1,35 @@
 package inflearn.q42_응급실;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
 
+    static Queue<Person> queue;
 
     public int solution(int[] arr, int M){
         int answer = 0;
+        queue = new LinkedList<>();
+        for (int i = 0; i < arr.length; i++) {
+            queue.offer(new Person(i, arr[i]));
+        }
 
+        while (!queue.isEmpty()) {
+            Person tmp = queue.poll();
+            for (Person p : queue) {
+                if (p.priority > tmp.priority) {
+                    queue.offer(tmp);
+                    tmp = null;
+                    break;
+                }
+            }
+
+            if (tmp != null) {
+                answer++;
+                if (tmp.id == M) return answer;
+            }
+        }
 
         return answer;
     }
@@ -25,5 +47,15 @@ public class Main {
 
         System.out.println(T.solution(arr, M));
 
+    }
+}
+
+class Person {
+    int id;
+    int priority;
+
+    public Person(int id, int priority) {
+        this.id = id;
+        this.priority = priority;
     }
 }
