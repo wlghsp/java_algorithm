9722f26836793d5f72fcbf8ae6b8971c33d799be
CHOIJIHO <wlghsp@gmail.com>
2022-07-14 23:00:45 +0900

    진법변환 코드 수정

--- src/baekjoon/bronze/bronzeⅡ/Boj2745_진법변환/Boj2745_진법변환.java
+++ src/baekjoon/bronze/bronzeⅡ/Boj2745_진법변환/Boj2745_진법변환.java
@@ -20,7 +20,7 @@ public class Boj2745_진법변환 {
         long result = 0;
         int idx = 0; // 승 0, 1, 2, 3....
         int num = 0;  // 계산하기 위해 각 자리 숫자를 10진수로 바꿔준다.
-        for (int i = n.length()-1; i >= 0; i++) {
+        for (int i = n.length()-1; i >= 0; i--) {
             char c = n.charAt(i);
 
             if (c >= '0' && c <= '9') {
@@ -33,6 +33,4 @@ public class Boj2745_진법변환 {
         }
         System.out.println(result);
     }
-
-
 }
