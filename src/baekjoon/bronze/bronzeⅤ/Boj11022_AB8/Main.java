a73abc0d7fedaf05b2c77f78199a181aa27504d3
CHOIJIHO <wlghsp@gmail.com>
2022-07-14 23:01:20 +0900

    Boj11022_AB8 시간 없어서 쉬운 문제로 스트릭 유지

---
+++ src/baekjoon/bronze/bronzeⅤ/Boj11022_AB8/Main.java
@@ -0,0 +1,22 @@
+package baekjoon.bronze.bronzeⅤ.Boj11022_AB8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int i = 1;
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.printf("Case #%d: %d + %d = %d\n", i++, a, b, a + b);
+        }
+
+    }
+}
