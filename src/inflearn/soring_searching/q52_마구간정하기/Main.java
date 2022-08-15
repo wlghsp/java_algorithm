00884956235e5cc2dbcd58fce8f06f767079f76e
CHOIJIHO <wlghsp@gmail.com>
2022-08-16 00:16:18 +0900

    인프런 마구간 정하기
    * 풀이
    - 어렵다. 결정 알고리즘은 아직 이해가 안간다.

--- src/inflearn/soring_searching/q52_마구간정하기/Main.java
+++ src/inflearn/soring_searching/q52_마구간정하기/Main.java
@@ -1,14 +1,55 @@
 package inflearn.soring_searching.q52_마구간정하기;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
+    public int count(int[] arr, int dist) { //          
+        int cnt = 1;
+        int ep = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - ep >= dist) {
+                cnt++;
+                ep = arr[i];
+            }
+        }
+        return cnt;
+    }
+
+    private int solution(int n, int c, int[] arr) {
+        int answer = 0;
+        Arrays.sort(arr);
+        int lt = 1;
+        int rt = arr[n-1];
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (count(arr, mid) >= c) {
+                answer = mid;
+                lt = mid + 1;
+            } else {
+                rt = mid - 1;
+            }
+        }
+        return answer;
+
+    }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int C = sc.nextInt();
+
+        int[] arr = new int[N];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(T.solution(N, C, arr));
 
 
     }
+
+
 }
