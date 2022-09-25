5323322bdb9bcb6a9ee6bb28741b4656b89c6884
wlghsp <wlghsp@gmail.com>
2022-09-25 19:57:10 +0900

    boj 2805 나무 자르기 진행 중

---
+++ src/ryuhoseok/parametric_search/boj2805_나무자르기/Main.java
@@ -0,0 +1,27 @@
+package ryuhoseok.parametric_search.boj2805_나무자르기;
+
+/*
+     N 나무의 갯수
+     M 필요한 나무의 길이
+     1<= N <= 100만
+     1<= M <= 20억
+     0 <= 각 나무 높이 <= 10억
+
+
+      M미터를 가져갈 수 있는 높이의 최댓값
+     */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+    }
+}
