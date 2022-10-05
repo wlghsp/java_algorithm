b30bb1389ce2efe5dfd6f776e08fb0b88dfc71b0
CHOIJIHO <wlghsp@gmail.com>
2022-10-05 19:14:32 +0900

    백준 1924 2007년

---
+++ src/baekjoon/bronze/bronzeⅠ/boj1924_2007년/Main.java
@@ -0,0 +1,26 @@
+package baekjoon.bronze.bronzeⅠ.boj1924_2007년;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int[] months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] day = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+        int sum = y;
+        for (int i = 0; i < x - 1; i++) {
+            sum += months[i];
+        }
+        System.out.println(day[sum % 7]);
+
+    }
+}
