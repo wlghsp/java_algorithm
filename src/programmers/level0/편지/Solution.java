c7941a86d3a6f460da48b3d87a5e79a4001c5b60
CHOIJIHO <wlghsp@gmail.com>
2022-10-09 22:23:44 +0900

    프로그래머스 편지
    길이 * 2 하면 됨

---
+++ src/programmers/level0/편지/Solution.java
@@ -0,0 +1,14 @@
+package programmers.level0.편지;
+
+public class Solution {
+
+    public int solution(String message) {
+        return message.length() * 2;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution("happy birthday!")); // 30
+        System.out.println(s.solution("I love you~")); // 22
+    }
+}
