0f46fdb25a44cd8797b660eb55bc4b4b641dc92a
CHOIJIHO <wlghsp@gmail.com>
2022-10-11 08:25:15 +0900

    종이 자르기 문제 풀이 예정

---
+++ src/programmers/level0/종이자르기/Solution.java
@@ -0,0 +1,16 @@
+package programmers.level0.종이자르기;
+
+public class Solution {
+
+    public int solution(int M, int N) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(2, 2)); // 3
+        System.out.println(s.solution(2, 5)); // 9
+        System.out.println(s.solution(1, 1)); // 0
+    }
+}
