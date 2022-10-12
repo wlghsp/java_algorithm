09c83963a482272ca73c87dbd00c8a3ab1c00864
CHOIJIHO <wlghsp@gmail.com>
2022-10-13 08:34:42 +0900

    백준 2501 약수구하기 풀이 예정

---
+++ src/baekjoon/bronze/bronzeⅢ/boj2501_약수구하기/Main.java
@@ -0,0 +1,16 @@
+package baekjoon.bronze.bronzeⅢ.boj2501_약수구하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+    }
+}
