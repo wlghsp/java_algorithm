656dd574ac44643b0007345a11ef9fa8ce71ffef
CHOIJIHO <wlghsp@gmail.com>
2022-10-20 07:52:09 +0900

    프로그래머스 삼각형의 완성조건 (2) 풀이 예정

---
+++ src/programmers/level0/삼각형의완성조건_2/Solution.java
@@ -0,0 +1,15 @@
+package programmers.level0.삼각형의완성조건_2;
+
+public class Solution {
+
+    public int solution(int[] sides) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.solution(new int[]{1,2}));
+    }
+}
