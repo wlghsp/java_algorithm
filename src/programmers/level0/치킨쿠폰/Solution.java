e798370558a6d12875bbb12f4f78ccfcbff3603b
CHOIJIHO <wlghsp@gmail.com>
2022-10-20 23:14:56 +0900

    프로그래머스 치킨 쿠폰

---
+++ src/programmers/level0/치킨쿠폰/Solution.java
@@ -0,0 +1,16 @@
+package programmers.level0.치킨쿠폰;
+
+public class Solution {
+
+    public int solution(int chicken) {
+        int answer = -1;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(100)); // 11
+        System.out.println(s.solution(1081)); // 120
+
+    }
+}
