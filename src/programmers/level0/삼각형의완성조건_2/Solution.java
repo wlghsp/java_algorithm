5d56a85f14e7d3584ab0841db3f00ef35b25e4b7
CHOIJIHO <wlghsp@gmail.com>
2022-10-20 22:51:21 +0900

    프로그래머스 삼각형의 완성조건 (2)
    - 이건 머리로 정리는 안된 상태인데 밑에 풀이보고 경우의 수로 대충 풀었다. 그래도 풀었다 ㅎㅎ

--- src/programmers/level0/삼각형의완성조건_2/Solution.java
+++ src/programmers/level0/삼각형의완성조건_2/Solution.java
@@ -4,12 +4,29 @@ public class Solution {
 
     public int solution(int[] sides) {
         int answer = 0;
+        int sum = sides[0] + sides[1];
+        int bigger = Math.max(sides[0], sides[1]);
+        int smaller = Math.min(sides[0], sides[1]);
+        // 1. 가장 긴변이 두 수 중에 하나인 경우
+        for (int i = 0; i < bigger; i++) {
+            if (bigger < smaller + i){
+                answer++;
+            }
+        }
+        // 2. 나머지 한 변이 가장 긴 변인 경우
+        for (int i = bigger; i < sum; i++) {
+            if (i < sum) {
+                answer++;
+            }
+        }
         return answer;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
 
-        System.out.println(s.solution(new int[]{1,2}));
+        System.out.println(s.solution(new int[]{1,2})); // 1
+        System.out.println(s.solution(new int[]{3,6})); // 5
+        System.out.println(s.solution(new int[]{11,7})); // 13
     }
 }
