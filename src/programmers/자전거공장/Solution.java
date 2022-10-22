47ed487700b363138aa008cdd4e61e1d47210e21
CHOIJIHO <wlghsp@gmail.com>
2022-10-22 10:05:27 +0900

    프로그래머스 기지국 설치 등

---
+++ src/programmers/자전거공장/Solution.java
@@ -0,0 +1,19 @@
+package programmers.자전거공장;
+
+public class Solution {
+    public int solution(int[][] cost, int[][] order) {
+        int answer = 0;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(new int[][]{{0,10}, {50, 20}, {100, 30}, {200, 40}},
+                new int[][]{{3, 50}, {7, 200}, {8, 200}})); // 5000
+        System.out.println(s.solution(new int[][]{{0,10}, {50, 20}},
+                new int[][]{{3, 100}, {4, 200}}));  // 4000
+
+    }
+}
