b81cad4969c6ebb32cd20e859abb45fd68f4fcb5
CHOIJIHO <wlghsp@gmail.com>
2022-10-23 13:51:40 +0900

    백준 숫자카드 10815 풀이 예정

---
+++ src/baekjoon/silverⅤ/boj10815_숫자카드/Main.java
@@ -0,0 +1,16 @@
+package baekjoon.silverⅤ.boj10815_숫자카드;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+    }
+}
