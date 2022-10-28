797dc1d5c753016e972d8fa39771672877a8d3f1
wlghsp <wlghsp@gmail.com>
2022-10-28 11:59:08 +0900

    백준 19532 수학은 비대면강의입니다.

---
+++ src/baekjoon/bronze/bronzeⅡ/boj19532_수학은비대면강의입니다/Main.java
@@ -0,0 +1,28 @@
+package baekjoon.bronze.bronzeⅡ.boj19532_수학은비대면강의입니다;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+        loop:for (int i = -999; i < 1000; i++) {
+            for (int j = -999; j < 1000; j++) {
+                if (a*i+b*j == c && d*i + e*j == f) {
+                    System.out.println(i + " " + j);
+                    break loop;
+                }
+            }
+        }
+
+    }
+}
