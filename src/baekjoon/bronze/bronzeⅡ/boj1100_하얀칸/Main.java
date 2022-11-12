6bdf8d6b7b045ebd516a8bf19ab4175bce6be45d
CHOIJIHO <wlghsp@gmail.com>
2022-11-12 12:45:42 +0900

    boj1100_하얀칸 풀이 예정

---
+++ src/baekjoon/bronze/bronzeⅡ/boj1100_하얀칸/Main.java
@@ -0,0 +1,14 @@
+package baekjoon.bronze.bronzeⅡ.boj1100_하얀칸;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
