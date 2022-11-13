d1393fd70f5181f51251a13533c02c0890c678d2
CHOIJIHO <wlghsp@gmail.com>
2022-11-13 15:02:55 +0900

    boj1100_하얀칸

--- src/baekjoon/bronze/bronzeⅡ/boj1100_하얀칸/Main.java
+++ src/baekjoon/bronze/bronzeⅡ/boj1100_하얀칸/Main.java
@@ -8,7 +8,17 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-
+        int cnt = 0;
+        for (int i = 0; i < 8; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < 8; j++) {
+                char t = s.charAt(j);
+                if ('F' == t)
+                    if ((i + j) % 2 == 0) {
+                        cnt++;
+                    }
+            }
+        }
+        System.out.println(cnt);
     }
 }
