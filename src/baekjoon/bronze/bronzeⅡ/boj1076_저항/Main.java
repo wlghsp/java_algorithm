f85fa9534873183da3d10865937e0f3a71279d44
CHOIJIHO <wlghsp@gmail.com>
2022-11-14 00:46:20 +0900

    boj1076_저항

--- src/baekjoon/bronze/bronzeⅡ/boj1076_저항/Main.java
+++ src/baekjoon/bronze/bronzeⅡ/boj1076_저항/Main.java
@@ -5,9 +5,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
+    static String[] colors = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        String a = br.readLine();
+        String b = br.readLine();
+        String c = br.readLine();
+        int num = getIdx(a) * 10 + getIdx(b);
+        int idx = getIdx(c);
+        System.out.println((long) (num * Math.pow(10, idx)));
     }
+
+    private static int getIdx(String a) {
+        int result = 0;
+        for (int i = 0; i < colors.length; i++) {
+            if (colors[i].equals(a)) result = i;
+        }
+        return result;
+    }
+
 }
