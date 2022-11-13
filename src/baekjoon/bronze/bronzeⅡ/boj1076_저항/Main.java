d093214e515b430e518b4bb55f434aa8f9fba16c
CHOIJIHO <wlghsp@gmail.com>
2022-11-13 15:03:08 +0900

    boj1076_저항 풀이 예정

---
+++ src/baekjoon/bronze/bronzeⅡ/boj1076_저항/Main.java
@@ -0,0 +1,13 @@
+package baekjoon.bronze.bronzeⅡ.boj1076_저항;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
