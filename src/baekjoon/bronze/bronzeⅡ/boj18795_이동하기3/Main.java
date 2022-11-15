354318420bd25c81f7bdaacecc93389af825f25a
CHOIJIHO <wlghsp@gmail.com>
2022-11-16 08:35:21 +0900

    boj18795_이동하기3 풀이 예정

---
+++ src/baekjoon/bronze/bronzeⅡ/boj18795_이동하기3/Main.java
@@ -0,0 +1,15 @@
+package baekjoon.bronze.bronzeⅡ.boj18795_이동하기3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+    }
+}
