46a34c33ce73f6c787cc2aeaa45d0f1eede607fb
wlghsp <wlghsp@gmail.com>
2022-11-17 13:34:30 +0900

    boj16561_3의배수
    완전탐색으로 2개의 수를 구하는 식으로 풀 수 있다.

--- src/baekjoon/bronze/bronzeⅡ/boj16561_3의배수/Main1.java
+++ src/baekjoon/bronze/bronzeⅡ/boj16561_3의배수/Main1.java
@@ -4,20 +4,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Main1 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // 두 개를 뽑으면 나머지 하나는 결정된다.
-        int cnt = 0;
         int N = Integer.parseInt(br.readLine());
-        for (int i = 1; i <= N/3; i++) {
-            for (int j = 1; j <= N/3; j++) {
-                int sum = 0;
-                sum = i * 3 + j * 3;
-                if ((N-sum) % 3 == 0 && sum < N) cnt++;
-            }
+        int sum = 1;
+        int count = 2;
+        for (int i = 9; i < N; i+=3) {
+            sum += count;
+            count += 1;
         }
-        System.out.println(cnt);
-
+        System.out.println(sum);
     }
 }
