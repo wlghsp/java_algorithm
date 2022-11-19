b6078033e6d241c6a4afc9643a29e5ddf140e63a
CHOIJIHO <wlghsp@gmail.com>
2022-11-19 12:53:37 +0900

    boj2566_최댓값

--- src/baekjoon/bronze/bronzeⅢ/boj2566_최댓값/Main.java
+++ src/baekjoon/bronze/bronzeⅢ/boj2566_최댓값/Main.java
@@ -3,13 +3,11 @@ package baekjoon.bronze.bronzeⅢ.boj2566_최댓값;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         int maxVal = Integer.MIN_VALUE;
         int row = 1;
         int maxRow = 0;
