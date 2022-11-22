5067f97d77d1fb981e4fe374c0f9287223931878
CHOIJIHO <wlghsp@gmail.com>
2022-11-22 23:41:48 +0900

    구름레벨 난이도1 최장맨해튼거리

---
+++ src/goorm_level/난이도1/최장맨해튼거리/Main.java
@@ -0,0 +1,22 @@
+package goorm_level.난이도1.최장맨해튼거리;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x1 = Integer.parseInt(st.nextToken());
+        int y1 = Integer.parseInt(st.nextToken());
+        int x2 = Integer.parseInt(st.nextToken());
+        int y2 = Integer.parseInt(st.nextToken());
+
+
+        int result = Math.abs(x1 - x2) + Math.abs(y1 - y2);
+        System.out.println(result);
+    }
+}
