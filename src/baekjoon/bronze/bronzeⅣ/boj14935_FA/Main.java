302e3346ad9b7dd74fdbf249fe4917f90091d7f1
CHOIJIHO <wlghsp@gmail.com>
2022-11-22 23:40:47 +0900

    boj14935_FA

---
+++ src/baekjoon/bronze/bronzeⅣ/boj14935_FA/Main.java
@@ -0,0 +1,10 @@
+package baekjoon.bronze.bronzeⅣ.boj14935_FA;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("FA");
+    }
+}
