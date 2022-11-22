9bb83838e35337970b2771a7dd3c7f7ad8bc1a80
CHOIJIHO <wlghsp@gmail.com>
2022-11-22 23:40:57 +0900

    boj19698_헛간청약

---
+++ src/baekjoon/bronze/bronzeⅣ/boj19698_헛간청약/Main.java
@@ -0,0 +1,23 @@
+package baekjoon.bronze.bronzeⅣ.boj19698_헛간청약;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        int maxCnt = (W / L) * (H / L);
+        if (maxCnt >= N) System.out.println(N);
+        else System.out.println(maxCnt);
+
+    }
+}
