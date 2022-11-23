97b2c39cd4acad9774e17236b85ca6c62c207dc8
CHOIJIHO <wlghsp@gmail.com>
2022-11-24 08:32:18 +0900

    풀이 코드개선

--- src/real_tests/satrec/Main3.java
+++ src/real_tests/satrec/Main3.java
@@ -11,18 +11,23 @@ public class Main3 {
         int result = 0;
         int oddSum = 0;
         for (int i = 0; i < idNumber.length(); i++) {
+            char c = idNumber.charAt(i);
             if (i % 2 == 0) {
-                char c = idNumber.charAt(i);
                 result += c - 48;
             } else {
-                char c = idNumber.charAt(i);
                 oddSum += c - 48;
             }
         }
-        result = result * 3;
+        result *= 3;
         result += oddSum;
-        String tmp = String.valueOf(result);
-        result = tmp.charAt(tmp.length() - 1) - 48;
+
+        // 일의 자릿수 구하기
+//        String tmp = String.valueOf(result);
+//        result = tmp.charAt(tmp.length() - 1) - 48;
+
+        // 이게 더 효율적
+        result = result % 10;
+
         if (result != 0) {
             result = 10 - result;
         }
