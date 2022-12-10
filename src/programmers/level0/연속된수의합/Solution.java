416345f51efdcd062035b64402cf45633bb6f397
CHOIJIHO <wlghsp@gmail.com>
2022-12-10 19:57:49 +0900

    프로그래머스 level0 연속된 수의 합 재 풀이를 위해 초기화

--- src/programmers/level0/연속된수의합/Solution.java
+++ src/programmers/level0/연속된수의합/Solution.java
@@ -7,34 +7,8 @@ import java.util.Queue;
 public class Solution {
 
     public int[] solution(int num, int total) {
-        int m = -((total / 2) + 1);
-
-        int sum = 0;
-        int cnt = 0;
-        Queue<Integer> q = new LinkedList<>();
-        while (q.size() < num) {
-            q.offer(m);
-            sum += m;
-            m++;
-        }
-        while (true) {
-            if (num == q.size() && sum == total) break;
-            else if (num == q.size() && sum < total) {
-                int poll = q.poll();
-                sum = poll < 0 ? sum - poll : sum + poll;
-                m++;
-                q.offer(m);
-                sum = m < 0 ? sum + m : sum - m;
-                if (num == q.size() && sum == total) break;
-            }
-        }
-        int[] answer = new int[q.size()];
-        int i = 0;
-        for (Integer integer : q) {
-            answer[i++] = integer;
-        }
-
-        return new int[num];
+        int[] answer = {};
+        return answer;
     }
 
 
