3681f9b885ac10e04fcc964aadb1a632f6a63093
wlghsp <wlghsp@gmail.com>
2022-12-13 17:08:12 +0900

    boj2997_네번째수

---
+++ src/baekjoon/bronze/bronzeⅢ/boj2997_네번째수/Main.java
@@ -0,0 +1,30 @@
+package baekjoon.bronze.bronzeⅢ.boj2997_네번째수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[3];
+        arr[0] = Integer.parseInt(st.nextToken());
+        arr[1] = Integer.parseInt(st.nextToken());
+        arr[2] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        if ((arr[1] - arr[0]) == (arr[2] - arr[1])) {
+            System.out.println(arr[2] + (arr[1] - arr[0]));
+        } else if ((arr[1] - arr[0]) * 2 == (arr[2] - arr[1])) {
+            System.out.println(arr[1] + (arr[1] - arr[0]));
+        } else if ((arr[2] - arr[1]) * 2 == (arr[1] - arr[0])) {
+            System.out.println(arr[0] + (arr[2] - arr[1]));
+        }
+    }
+}
