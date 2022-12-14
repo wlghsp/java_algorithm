750ef321ff189029ea386dc13f6b2e9b992daaa9
CHOIJIHO <wlghsp@gmail.com>
2022-12-14 21:58:36 +0900

    boj5612_터널의입구와출구 완료
    - 첫 터널에 있는 갯수 m이 최댓값이 될 수 있는데 이 점을 고려해서 최댓값 변수에 m을 초기에 대입해야 한다.

--- src/baekjoon/bronze/bronzeⅢ/boj5612_터널의입구와출구/Main.java
+++ src/baekjoon/bronze/bronzeⅢ/boj5612_터널의입구와출구/Main.java
@@ -10,20 +10,15 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
-        int maxCars = Integer.MIN_VALUE;
-        boolean zero = false;
+        int maxVal = m;
         for (int i = 1; i <= n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int in = Integer.parseInt(st.nextToken());
             int out = Integer.parseInt(st.nextToken());
-            m += in;
-            if (m > maxCars) {
-                maxCars = m;
-            }
-            m -= out;
-            if (m < 0) zero = true;
-
+            m = m + in - out;
+            if (m < 0) { maxVal = 0;break;}
+            if (m > maxVal) maxVal = m;
         }
-        System.out.println(zero ? 0 : maxCars);
+        System.out.println(maxVal);
     }
 }
