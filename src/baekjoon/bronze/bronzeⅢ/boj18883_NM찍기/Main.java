51804f3b87ec13fa3ffa979573ccaed7453b6f7d
CHOIJIHO <wlghsp@gmail.com>
2022-12-19 22:51:13 +0900

    boj18883_NM찍기

---
+++ src/baekjoon/bronze/bronzeⅢ/boj18883_NM찍기/Main.java
@@ -0,0 +1,26 @@
+package baekjoon.bronze.bronzeⅢ.boj18883_NM찍기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int num = 1;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                sb.append(num++);
+                if (j != M - 1) sb.append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
