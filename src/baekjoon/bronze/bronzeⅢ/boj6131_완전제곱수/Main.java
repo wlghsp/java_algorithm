398e70173e734b9555309c7333bee2e2e9aa572c
CHOIJIHO <wlghsp@gmail.com>
2022-12-19 22:51:02 +0900

    boj6131_완전제곱수

--- src/baekjoon/bronze/bronzeⅢ/boj6131_완전제곱수/Main.java
+++ src/baekjoon/bronze/bronzeⅢ/boj6131_완전제곱수/Main.java
@@ -9,7 +9,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
-
+        int cnt = 0;
+        for (int b = 1; b <= 500; b++) {
+            for (int a = b; a <= 500; a++) {
+                if (a * a == (b * b + N)) cnt++;
+            }
+        }
+        System.out.println(cnt);
     }
 }
