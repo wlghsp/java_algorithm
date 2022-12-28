a0fb6585b98227cd7eb362c619ef7524a070adaa
CHOIJIHO <wlghsp@gmail.com>
2022-12-28 21:43:31 +0900

    boj18411_試驗_Exam

---
+++ src/baekjoon/bronze/bronzeⅣ/boj18411_試驗_Exam/Main.java
@@ -0,0 +1,21 @@
+package baekjoon.bronze.bronzeⅣ.boj18411_試驗_Exam;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[3];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[2] + arr[1]);
+    }
+}
