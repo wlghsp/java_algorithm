cec0bf25376ab0fe64147058716e3220bce5999a
CHOIJIHO <wlghsp@gmail.com>
2022-12-30 16:41:41 +0900

    boj10953_APlusB_6

---
+++ src/baekjoon/bronze/bronzeⅡ/boj10953_APlusB_6/Main.java
@@ -0,0 +1,22 @@
+package baekjoon.bronze.bronzeⅡ.boj10953_APlusB_6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), ",");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a + b).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
