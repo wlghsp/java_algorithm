b252040bc18e0c10c9601fc6ee0fbcda8d1872e9
CHOIJIHO <wlghsp@gmail.com>
2023-01-10 16:49:34 +0900

    파일 이동

--- src/baekjoon/silverⅤ/boj10815_숫자카드/Main_HashMap.java
+++ src/baekjoon/silverⅤ/boj10815_숫자카드/Main_HashMap.java
@@ -1,28 +1,23 @@
-package baekjoon.silverⅣ.Boj10815_숫자카드;
+package baekjoon.silverⅤ.boj10815_숫자카드;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
-public class Main_이진탐색 {
-
-    static int[] cards;
+public class Main_HashMap {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
 
-        cards = new int[N];
+        int[] cards = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N ; i++) {
             cards[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(cards);
-
         int M = Integer.parseInt(br.readLine());
         int[] candidate = new int[M];
         st = new StringTokenizer(br.readLine());
@@ -30,31 +25,23 @@ public class Main_이진탐색 {
             candidate[i] = Integer.parseInt(st.nextToken());
         }
 
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int c : cards) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
         StringBuilder sb = new StringBuilder();
-        for (int i : candidate) {
-            if (bsearch(i) >= 0) {
-                sb.append(1).append(' ');
+        for (int c : candidate) {
+            if(map.containsKey(c)) {
+                int result = map.get(c).intValue();
+                sb.append(result).append(" ");
             } else {
-                sb.append(0).append(' ');
+                sb.append(0).append(" ");
             }
         }
+
         System.out.println(sb);
-    }
 
-    private static int bsearch(int num){
-        int lo = 0;
-        int high = cards.length - 1;
-
-        while (lo <= high) {
-            int mid = (lo+high) / 2;
-            if (cards[mid] > num) {
-                high = mid -1;
-            } else if (cards[mid] < num ) {
-                lo = mid + 1;
-            } else {
-                return mid;
-            }
-        }
-        return -1;
     }
 }
--- src/baekjoon/silverⅤ/boj10815_숫자카드/Main_이진탐색.java
+++ src/baekjoon/silverⅤ/boj10815_숫자카드/Main_이진탐색.java
@@ -1,23 +1,28 @@
-package baekjoon.silverⅣ.Boj10815_숫자카드;
+package baekjoon.silverⅤ.boj10815_숫자카드;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main_이진탐색 {
+
+    static int[] cards;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
 
-        int[] cards = new int[N];
+        cards = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N ; i++) {
             cards[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(cards);
+
         int M = Integer.parseInt(br.readLine());
         int[] candidate = new int[M];
         st = new StringTokenizer(br.readLine());
@@ -25,23 +30,31 @@ public class Main {
             candidate[i] = Integer.parseInt(st.nextToken());
         }
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int c : cards) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
-
         StringBuilder sb = new StringBuilder();
-        for (int c : candidate) {
-            if(map.containsKey(c)) {
-                int result = map.get(c).intValue();
-                sb.append(result).append(" ");
+        for (int i : candidate) {
+            if (bsearch(i) >= 0) {
+                sb.append(1).append(' ');
             } else {
-                sb.append(0).append(" ");
+                sb.append(0).append(' ');
             }
         }
-
         System.out.println(sb);
+    }
 
+    private static int bsearch(int num){
+        int lo = 0;
+        int high = cards.length - 1;
+
+        while (lo <= high) {
+            int mid = (lo+high) / 2;
+            if (cards[mid] > num) {
+                high = mid -1;
+            } else if (cards[mid] < num ) {
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
     }
 }
