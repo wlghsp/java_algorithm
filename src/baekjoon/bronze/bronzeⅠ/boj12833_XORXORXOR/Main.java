09782cc04c77213c41bac527c57c1702c5ffb9db
CHOIJIHO <wlghsp@gmail.com>
2023-01-12 09:49:54 +0900

    boj12833_XORXORXOR

---
+++ src/baekjoon/bronze/bronzeⅠ/boj12833_XORXORXOR/Main.java
@@ -0,0 +1,21 @@
+package baekjoon.bronze.bronzeⅠ.boj12833_XORXORXOR;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < c; i++) {
+            a ^= b;
+        }
+        System.out.println(a);
+    }
+}
