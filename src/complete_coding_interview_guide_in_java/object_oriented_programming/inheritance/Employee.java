6cd50a69de718411fc00fcbf3be3fa19f5b71c0e
CHOIJIHO <wlghsp@gmail.com>
2023-01-13 16:24:26 +0900

    객체지향 프로그램 - 상속
    - 상속을 통해 다른 객체를 기반으로 객체를 만들 수 있다.
    - 상속은 객체가 다른 객체의 코드를 재사용할 수 있도록 허용하여 코드의 재사용성을 유지한다. 또한 각 객체만의 로직도 추가 할 수 있다.
    - 상속은 IS-A 관계라고도 하며 부모-자녀 관계라고도 한다.
    - 상속된 객체는 슈퍼클래스 또는 부모클래스라고 하고 슈퍼클래스를 상속받은 객체는 서브클래스 또는 자식 클래스라고 한다.
    - 자바에서는 여러 개의 클래스를 상속할 수 없다.

---
+++ src/complete_coding_interview_guide_in_java/object_oriented_programming/inheritance/Employee.java
@@ -0,0 +1,18 @@
+package complete_coding_interview_guide_in_java.object_oriented_programming.inheritance;
+
+public class Employee {
+
+    private String name;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
