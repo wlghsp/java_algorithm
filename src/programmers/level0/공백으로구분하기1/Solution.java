a932c60285486d01de213f6014ee3456894414a0
CHOIJIHO <wlghsp@gmail.com>
2023-05-21 23:07:41 +0900

    프로그래머스 공백으로구분하기1

---
+++ src/programmers/level0/공백으로구분하기1/Solution.java
@@ -0,0 +1,19 @@
+package programmers.level0.공백으로구분하기1;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public String[] solution(String my_string) {
+        return  my_string.split(" ");
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String my_string1 = "i love you";
+        String my_string2 = "programmers";
+        System.out.println(Arrays.toString(s.solution(my_string1)));
+        System.out.println(Arrays.toString(s.solution(my_string2)));
+
+    }
+}
