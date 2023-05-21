f863e19f157e37152ca138018d361e4e25d6824c
CHOIJIHO <wlghsp@gmail.com>
2023-05-21 23:07:54 +0900

    프로그래머스 flag에 따라 다른 값 반환하기

---
+++ src/programmers/level0/flag에따라다른값반환하기/Solution.java
@@ -0,0 +1,16 @@
+package programmers.level0.flag에따라다른값반환하기;
+
+public class Solution {
+
+    public int solution(int a, int b, boolean flag) {
+        return flag ? a + b : a - b;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int a = -4;
+        int b = 7;
+        System.out.println(s.solution(a, b, true));
+        System.out.println(s.solution(a, b, false));
+    }
+}
