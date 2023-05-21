39c1b5f30f29d16b094e3f504bac8b58da553221
jihochoi <wlghsp@gmail.com>
2023-05-22 08:58:02 +0900

    프로그래머스 부분문자열인지확인하기

---
+++ src/programmers/코딩기초트레이닝/부분문자열인지확인하기/Solution.java
@@ -0,0 +1,7 @@
+package programmers.코딩기초트레이닝.부분문자열인지확인하기;
+
+public class Solution {
+    public int solution(String my_string, String target) {
+        return my_string.contains(target) ? 1: 0;
+    }
+}
