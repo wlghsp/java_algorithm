232a9348c3e40659ff553582d0a2be4a25d6e933
JIHO CHOI <wlghsp@gmail.com>
2023-08-13 14:47:02 +0900

    프로그래머스 배열 만들기 5

--- src/programmers/코딩기초트레이닝/배열만들기5/Solution.java
+++ src/programmers/코딩기초트레이닝/배열만들기5/Solution.java
@@ -1,11 +1,24 @@
 package programmers.코딩기초트레이닝.배열만들기5;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Solution {
     public int[] solution(String[] intStrs, int k, int s, int l) {
-        int[] answer = {};
-        return answer;
+        return Stream.of(intStrs).mapToInt(str -> Integer.parseInt(str.substring(s, s + l))).filter(i -> i > k).toArray();
+    }
+
+
+
+    public int[] solution1(String[] intStrs, int k, int s, int l) {
+        List<Integer> list = new ArrayList<>();
+        for (String intStr : intStrs) {
+            int intSubStr = Integer.parseInt(intStr.substring(s, s + l));
+            if (intSubStr > k) list.add(intSubStr);
+        }
+        return list.stream().mapToInt(i -> i).toArray();
     }
 
     public static void main(String[] args) {
