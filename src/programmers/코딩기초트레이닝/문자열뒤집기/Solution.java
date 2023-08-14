6a13199972c93ae8c8ff3139e1320d73cf7bb499
JIHO CHOI <wlghsp@gmail.com>
2023-08-14 21:28:57 +0900

    프로그래머스 코딩 기초 트레이닝 문자열 뒤집기

--- src/programmers/코딩기초트레이닝/문자열뒤집기/Solution.java
+++ src/programmers/코딩기초트레이닝/문자열뒤집기/Solution.java
@@ -1,15 +1,25 @@
 package programmers.코딩기초트레이닝.문자열뒤집기;
 
 public class Solution {
-    public String solution(String my_string, int s, int e) {
-        String answer = "";
 
-        return answer;
+    public String solution(String my_string, int s, int e) {
+        String subStr = new StringBuilder(my_string.substring(s, e + 1)).reverse().toString();
+        return my_string.substring(0, s) + subStr + my_string.substring(e + 1);
+    }
+    public String solution1(String my_string, int s, int e) {
+        char[] charArray = my_string.toCharArray();
+        String subStr = new StringBuilder(my_string.substring(s, e + 1)).reverse().toString();
+        int idx = 0;
+        for (int i = s; i <= e; i++) {
+            charArray[i] = subStr.charAt(idx++);
+        }
+        return new String(charArray);
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
         System.out.println(s.solution("Progra21Sremm3", 6, 12)); // "ProgrammerS123"
+        System.out.println(s.solution("Stanley1yelnatS", 4, 10)); // "Stanley1yelnatS"
 
     }
 }
