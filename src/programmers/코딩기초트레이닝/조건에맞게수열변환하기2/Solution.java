e83b46fc7203394451ceff1f792d0c1432ab35a3
jihochoi <wlghsp@gmail.com>
2023-08-18 16:54:17 +0900

    프로그래머스 코딩 기초 트레이닝 조건에 맞게 수열 변환하기 2 풀이 예정

---
+++ src/programmers/코딩기초트레이닝/조건에맞게수열변환하기2/Solution.java
@@ -0,0 +1,14 @@
+package programmers.코딩기초트레이닝.조건에맞게수열변환하기2;
+
+public class Solution {
+    public int solution(int[] arr) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(new int[]{1, 2, 3, 100, 99, 98})); // 5
+    }
+}
