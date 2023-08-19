415f2eb62a16234fa948993eff9643a591b7ddde
JIHO CHOI <wlghsp@gmail.com>
2023-08-19 12:56:08 +0900

    프로그래머스 코딩 기초 트레이닝 조건에 맞게 수열 변환하기 2

--- src/programmers/코딩기초트레이닝/조건에맞게수열변환하기2/Solution.java
+++ src/programmers/코딩기초트레이닝/조건에맞게수열변환하기2/Solution.java
@@ -3,8 +3,22 @@ package programmers.코딩기초트레이닝.조건에맞게수열변환하기2;
 public class Solution {
     public int solution(int[] arr) {
         int answer = 0;
+        while (true) {
+            boolean changed = false;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] > 50 && arr[i] % 2 == 0) {
+                    arr[i] = arr[i] / 2;
+                    changed = true;
+                } else if (arr[i] < 50 && arr[i] % 2 != 0) {
+                    arr[i] = arr[i] * 2 + 1;
+                    changed = true;
+                }
+            }
+            answer++;
+            if (!changed) break;
+        }
 
-        return answer;
+        return answer - 1;
     }
 
     public static void main(String[] args) {
