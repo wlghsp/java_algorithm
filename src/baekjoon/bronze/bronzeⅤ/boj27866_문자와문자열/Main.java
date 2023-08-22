8e2bcc93ef0b01fdda72fe16e23834ec6e475c50
JIHO CHOI <wlghsp@gmail.com>
2023-08-22 23:34:32 +0900

    boj27866_문자와문자열

---
+++ src/baekjoon/bronze/bronzeⅤ/boj27866_문자와문자열/Main.java
@@ -0,0 +1,15 @@
+package baekjoon.bronze.bronzeⅤ.boj27866_문자와문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int i = Integer.parseInt(br.readLine());
+        System.out.println(s.charAt(i - 1));
+    }
+}
