1b3041429bcf78ad97526b0e419047f7b3533dda
JIHO CHOI <wlghsp@gmail.com>
2023-09-09 10:40:59 +0900

    코딩테스트 입문 연속된 수의 합

--- src/programmers/level0/연속된수의합/Solution.java
+++ src/programmers/level0/연속된수의합/Solution.java
@@ -7,7 +7,16 @@ import java.util.Queue;
 public class Solution {
 
     public int[] solution(int num, int total) {
-        int[] answer = {};
+        int[] answer = new int[num];
+
+        int middleNumber = total / num;
+        int subtractNumber = total % num == 0 ? num / 2 : num / 2 - 1;
+
+        int startNumber = middleNumber - subtractNumber;
+
+        for (int i = 0; i < num; i++) {
+            answer[i] = startNumber + i;
+        }
         return answer;
     }
 
