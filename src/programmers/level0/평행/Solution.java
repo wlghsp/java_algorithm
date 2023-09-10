dd51213059b2b3973c8bca2ef2a17c9ed43c3082
JIHO CHOI <wlghsp@gmail.com>
2023-09-10 19:25:58 +0900

    프로그래머스 평행 완료

--- src/programmers/level0/평행/Solution.java
+++ src/programmers/level0/평행/Solution.java
@@ -4,31 +4,25 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Solution {
-
+    int answer;
     public int solution(int[][] dots) {
-        // 기울기를 저장할 집합
-        Set<Double> slopes = new HashSet<>();
-
-        // 모든 두 점 쌍에 대한 기울기 계산
-        int cnt = 0;
-        for (int i = 0; i < dots.length; i++) {
-            for (int j = i + 1; j < dots.length; j++) {
-                int[] point1 = dots[i];
-                int[] point2 = dots[j];
-                double slope;
-
-                // 두 점이 같은 경우, 기울기를 무한대로 설정
-                slope = (double) (point2[1] - point1[1]) / (point2[0] - point1[0]);
-
-                // 기울기를 집합에 추가
-                slopes.add(slope);
-                cnt++;
-            }
-        }
-
-        return cnt == slopes.size() ? 0 : 1;
+        answer = 0;
+        // 1-2, 3-4가 이어졌을 때
+        calculation(dots[0], dots[1], dots[2], dots[3]);
+        // 1-3, 2-4가 이어졌을 때
+        calculation(dots[0], dots[2], dots[1], dots[3]);
+        // 1-4, 2-3가 이어졌을 때
+        calculation(dots[0], dots[3], dots[1], dots[2]);
+        return answer > 0 ? 1 : 0;
     }
 
+    private void calculation(int[] a, int[] b, int[] c, int[] d) {
+        double abDiff = (double) (b[1] - a[1]) / (b[0] - a[0]);
+        double cdDiff = (double) (d[1] - c[1]) / (d[0] - c[0]);
+        if (abDiff == cdDiff) {
+            answer++;
+        }
+    }
 
 
 }
--- test/programmers/level0/평행/평행Test.java
+++ test/programmers/level0/평행/평행Test.java
@@ -11,7 +11,9 @@ class 평행Test {
         Solution s = new Solution();
         int result1 = s.solution(new int[][]{{1, 4}, {9, 2}, {3, 8}, {11, 6}});
         int result2 = s.solution(new int[][]{{3, 5}, {4, 1}, {2, 4}, {5, 10}});
+        int result3 = s.solution(new int[][]{{1, 1}, {2, 2}, {3, 3}, {50, 100}});
         assertEquals(1, result1);
         assertEquals(0, result2);
+        assertEquals(0, result3);
     }
 }
