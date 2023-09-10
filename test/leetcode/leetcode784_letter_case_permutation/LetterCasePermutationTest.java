6131b262ac5b335a6e6d7c3a46e7ae104e92b3d5
JIHO CHOI <wlghsp@gmail.com>
2023-09-10 11:48:47 +0900

    leetcode784_letter_case_permutation

---
+++ test/leetcode/leetcode784_letter_case_permutation/LetterCasePermutationTest.java
@@ -0,0 +1,22 @@
+package leetcode.leetcode784_letter_case_permutation;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LetterCasePermutationTest {
+    @Test
+    void testLetterCasePermutationTest() {
+        Solution s = new Solution();
+        String str = "a1b2";
+        List<String> result = s.letterCasePermutation(str);
+        Collections.sort(result);
+        String[] expected = {"a1b2","a1B2","A1b2","A1B2"};
+        Arrays.sort(expected);
+        assertArrayEquals(expected, result.toArray(new String[0]));
+    }
+}
