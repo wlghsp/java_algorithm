0ed75c1fc68e76b5125b7bca3f16b3d62b54f7d2
jihochoi <wlghsp@gmail.com>
2023-09-10 17:12:34 +0900

    프로그래머스 코딩테스트 입문 평행 풀이 예정

---
+++ src/programmers/level0/평행/Solution.java
@@ -0,0 +1,12 @@
+package programmers.level0.평행;
+
+public class Solution {
+
+    public int solution(int[][] dots) {
+        int answer = 0;
+        return answer;
+    }
+
+
+
+}
---
+++ test/programmers/level0/평행/평행Test.java
@@ -0,0 +1,17 @@
+package programmers.level0.평행;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class 평행Test {
+
+    @Test
+    void testSolution() {
+        Solution s = new Solution();
+        int result1 = s.solution(new int[][]{{1, 4}, {9, 2}, {3, 8}, {11, 6}});
+        int result2 = s.solution(new int[][]{{3, 5}, {4, 1}, {2, 4}, {5, 10}});
+        assertEquals(1, result1);
+        assertEquals(0, result2);
+    }
+}
