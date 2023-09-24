3edd5080acb0c837432ba01601dcca1dc88c46de
JIHO CHOI <wlghsp@gmail.com>
2023-09-24 19:29:38 +0900

    leetcode875 koko eating bananas

---
+++ test/leetcode/leetcode875_koko_eating_bananas/SolutionTest.java
@@ -0,0 +1,20 @@
+package leetcode.leetcode875_koko_eating_bananas;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+    @Test
+    void testKoko() {
+        Solution s = new Solution();
+        int[] piles1 = {3, 6, 7, 11};
+        int[] piles2 = {30,11, 23, 4, 20};
+        int h1 = 8;
+        int h2 = 5;
+        int result1 = s.minEatingSpeed(piles1, h1);
+        int result2 = s.minEatingSpeed(piles2, h2);
+        assertEquals(4, result1);
+        assertEquals(30, result2);
+    }
+}
