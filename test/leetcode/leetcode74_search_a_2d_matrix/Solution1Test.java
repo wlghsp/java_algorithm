14d790907043642f144bc68cd44d67a032dab554
JIHO CHOI <wlghsp@gmail.com>
2023-09-24 19:49:24 +0900

    leetcode74_search_a_2d_matrix 재 풀이
    - 1차원으로 만들지 않고 2차원에서 행마다 이진탐색하여 target 존재여부 확인

---
+++ test/leetcode/leetcode74_search_a_2d_matrix/Solution1Test.java
@@ -0,0 +1,22 @@
+package leetcode.leetcode74_search_a_2d_matrix;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution1Test {
+
+    @Test
+    void testSolution1() {
+        Solution1 s1 = new Solution1();
+        boolean result1 = s1.searchMatrix(new int[][]{
+                {1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}
+        }, 3);
+
+        boolean result2 = s1.searchMatrix(new int[][]{
+                {1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}
+        }, 13);
+        assertTrue(result1);
+        assertFalse(result2);
+    }
+}
