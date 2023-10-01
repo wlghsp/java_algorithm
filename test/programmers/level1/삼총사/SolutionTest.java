134ab2244f4e7d933795d4b8475a0c7dafc28977
JIHO CHOI <wlghsp@gmail.com>
2023-10-01 20:14:11 +0900

    프로그래머스 level1 삼총사

--- test/programmers/level1/삼총사/SolutionTest.java
+++ test/programmers/level1/삼총사/SolutionTest.java
@@ -2,11 +2,19 @@ package programmers.level1.삼총사;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class SolutionTest {
 
 
     @Test
     void test삼총사() {
-
+        Solution s = new Solution();
+        int result1 = s.solution(new int[]{-2, 3, 0, 2, -5});
+        int result2 = s.solution(new int[]{-3, -2, -1, 0, 1, 2, 3});
+        int result3 = s.solution(new int[]{-1, 1, -1, 1});
+        assertEquals(2, result1);
+        assertEquals(5, result2);
+        assertEquals(0, result3);
     }
 }
