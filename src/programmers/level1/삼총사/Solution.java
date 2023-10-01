86644450a3bf609a63a0802c1cf49d5a22e797cf
CHOIJIHO <wlghsp@gmail.com>
2023-10-01 18:25:42 +0900

    프로그래머스 level1 삼총사 풀이 예정

---
+++ src/programmers/level1/삼총사/Solution.java
@@ -0,0 +1,9 @@
+package programmers.level1.삼총사;
+
+public class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+
+        return answer;
+    }
+}
---
+++ test/programmers/level1/삼총사/SolutionTest.java
@@ -0,0 +1,12 @@
+package programmers.level1.삼총사;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+
+    @Test
+    void test삼총사() {
+
+    }
+}
