a7bc211005d6fb5266504faf54866846b81664a4
JIHO CHOI <wlghsp@gmail.com>
2023-10-10 22:15:43 +0900

    프로그래머스 level1 비밀지도 풀이 예정

---
+++ src/programmers/level1/비밀지도/Solution.java
@@ -0,0 +1,9 @@
+package programmers.level1.비밀지도;
+
+public class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
+
+        return answer;
+    }
+}
---
+++ test/programmers/level1/비밀지도/SolutionTest.java
@@ -0,0 +1,18 @@
+package programmers.level1.비밀지도;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+
+    @Test
+    void test() {
+        Solution s = new Solution();
+        String[] result1 = s.solution(5, new int[]{9, 20, 28, 18, 11}, new int[]{30, 1, 21, 17, 28});
+        String[] result2 = s.solution(6, new int[]{46, 33, 33 ,22, 31, 50}, new int[]{27 ,56, 19, 14, 14, 10});
+        assertArrayEquals(new String[]{"#####","# # #", "### #", "# ##", "#####"}, result1);
+        assertArrayEquals(new String[]{"######", "### #", "## ##", " #### ", " #####", "### # "}, result2);
+    }
+}
