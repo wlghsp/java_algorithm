65dbd61121a9a4e42232a07e3d4803bb4bb8f2e4
JIHO CHOI <wlghsp@gmail.com>
2023-10-10 21:05:40 +0900

    프로그래머스 level1 최소 직사각형 풀이

--- src/programmers/level1/최소직사각형/Solution.java
+++ src/programmers/level1/최소직사각형/Solution.java
@@ -2,7 +2,26 @@ package programmers.level1.최소직사각형;
 
 public class Solution {
     public int solution(int[][] sizes) {
-        int answer = 0;
-        return answer;
+        int maxW = 0;
+        int maxH = 0;
+
+        for (int i = 0; i < sizes.length; i++) {
+            int w = sizes[i][0];
+            int h = sizes[i][1];
+            if(w < h) {
+                swap(sizes, i);
+            }
+            maxW = Math.max(sizes[i][0], maxW);
+            maxH = Math.max(sizes[i][1], maxH);
+        }
+
+        return maxW * maxH;
+    }
+
+    private void swap(int[][] sizes, int i) {
+        int[] size = sizes[i];
+        int tmp = size[0];
+        size[0] = size[1];
+        size[1] = tmp;
     }
 }
