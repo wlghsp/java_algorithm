1419d8bf8a682af17449ca23d1a4b12074b65419
JIHO CHOI <wlghsp@gmail.com>
2023-10-11 18:28:07 +0900

    프로그래머스 level1 비밀지도 풀이 완료

--- test/programmers/level1/비밀지도/SolutionTest.java
+++ test/programmers/level1/비밀지도/SolutionTest.java
@@ -12,15 +12,9 @@ class SolutionTest {
         Solution s = new Solution();
         String[] result1 = s.solution(5, new int[]{9, 20, 28, 18, 11}, new int[]{30, 1, 21, 17, 28});
         String[] result2 = s.solution(6, new int[]{46, 33, 33 ,22, 31, 50}, new int[]{27 ,56, 19, 14, 14, 10});
-        assertArrayEquals(new String[]{"#####","# # #", "### #", "# ##", "#####"}, result1);
-        assertArrayEquals(new String[]{"######", "### #", "## ##", " #### ", " #####", "### # "}, result2);
+        assertArrayEquals(new String[]{"#####","# # #", "### #", "#  ##", "#####"}, result1);
+        assertArrayEquals(new String[]{"######", "###  #", "##  ##", " #### ", " #####", "### # "}, result2);
     }
 
 
-    @Test
-    void decimalToBinaryTest() {
-        Solution s = new Solution();
-        String result1 = s.decimalToBinary(5, 9);
-        assertEquals("01001", result1);
-    }
 }
