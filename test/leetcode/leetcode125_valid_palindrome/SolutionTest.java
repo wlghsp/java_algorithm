dbe7e492f2fca291f967ae98260ff67f9f8812be
JIHO CHOI <wlghsp@gmail.com>
2023-10-14 13:02:36 +0900

    leetcode125_valid_palindrome easy 문제.

---
+++ test/leetcode/leetcode125_valid_palindrome/SolutionTest.java
@@ -0,0 +1,22 @@
+package leetcode.leetcode125_valid_palindrome;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+
+    @Test
+    void test() {
+        Solution s = new Solution();
+        boolean result1 = s.isPalindrome("A man, a plan, a canal: Panama");
+        boolean result2 = s.isPalindrome("race a car");
+        boolean result3 = s.isPalindrome(" ");
+        boolean result4 = s.isPalindrome("0P");
+        assertEquals(true, result1);
+        assertEquals(false, result2);
+        assertEquals(true, result3);
+        assertEquals(false, result4);
+    }
+}
