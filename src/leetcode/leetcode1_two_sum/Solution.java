c09801d6d0686c3765b8933c274892aa2e03fdf4
JIHO CHOI <wlghsp@gmail.com>
2023-10-14 19:15:57 +0900

    leetcode1_two_sum 투포인터 풀이 추가

--- src/leetcode/leetcode1_two_sum/Solution.java
+++ src/leetcode/leetcode1_two_sum/Solution.java
@@ -1,10 +1,36 @@
 package leetcode.leetcode1_two_sum;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
+
     public int[] twoSum(int[] nums, int target) {
+        int[][] numWithIndex = new int[nums.length][2];
+        for (int i = 0; i < nums.length; i++) {
+            numWithIndex[i][0] = nums[i];
+            numWithIndex[i][1] = i;
+        }
+
+        Arrays.sort(numWithIndex, Comparator.comparingInt(arr -> arr[0]));
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int sum = numWithIndex[left][0] + numWithIndex[right][0];
+            if (sum == target) {
+                return new int[]{numWithIndex[left][1], numWithIndex[right][1]};
+            } else if (sum < right) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[]{};
+    }
+
+    public int[] twoSum3(int[] nums, int target) {
         Map<Integer, Integer> numToIndex = new HashMap<>();
 
         // First Pass: Populate the HashMap
@@ -20,7 +46,7 @@ public class Solution {
             }
         }
 
-        return new int[0];
+        return new int[]{};
     }
 
 
