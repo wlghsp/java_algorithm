4348486981bcaae1734706c2c566a1a9a80ab1c4
JIHO CHOI <wlghsp@gmail.com>
2023-10-14 21:25:54 +0900

    leetcode15_3sum

---
+++ test/leetcode/leetcode15_3sum/SolutionTest.java
@@ -0,0 +1,25 @@
+package leetcode.leetcode15_3sum;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void test3Sum() {
+        Solution s = new Solution();
+        List<List<Integer>> result1 = s.threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+
+
+        List<List<Integer>> expected = new ArrayList<>();
+        expected.add(Arrays.asList(-1, -1, 2));
+        expected.add(Arrays.asList(-1, 0, 1));
+
+        assertEquals(expected, result1);
+    }
+}
