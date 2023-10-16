86ae1a9478a1bd8fda4d2ad9525c5c60c5037987
JIHO CHOI <wlghsp@gmail.com>
2023-10-17 00:20:50 +0900

    boj1337_올바른배열
    - 각 원소별로 5개의 연속된 숫자가 배열에 포함되어 있는지를 확인하고 포함이 안된 건수가 가장 작은 값을 출력
    - 포함여부 판단을 위해 Set을 써서 contains로 O(1)의 시간복잡도로 확인함

--- src/baekjoon/silverⅣ/boj1337_올바른배열/Main.java
+++ src/baekjoon/silverⅣ/boj1337_올바른배열/Main.java
@@ -3,36 +3,31 @@ package baekjoon.silverⅣ.boj1337_올바른배열;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+        Integer[] arr = new Integer[N];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(arr);
-        int maxCnt = Integer.MIN_VALUE;
-        int cnt = 1;
-
-        int priorDiff = arr[1] - arr[0];
-        for (int rt = 2; rt < N; rt++) {
-            int diff = arr[rt] - arr[rt -1];
-            if (diff == priorDiff) {
-               cnt++;
-            } else {
-                cnt = 1;
-                priorDiff = diff;
+        Set<Integer> set = new HashSet<>(Arrays.asList(arr));
+        int minCnt = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            int cnt = 0;
+            for (int j = arr[i]; j < arr[i] + 5; j++) {
+                if (!set.contains(j)) {
+                    cnt++;
+                }
             }
-            maxCnt = Math.max(maxCnt, cnt);
+            minCnt = Math.min(minCnt, cnt);
         }
 
-        System.out.println(5 - maxCnt);
-
+        System.out.println(minCnt);
     }
 }
