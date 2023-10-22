620c9a7bae35741afbf2342aa74787c3d9118657
JIHO CHOI <wlghsp@gmail.com>
2023-10-22 09:58:47 +0900

    leetcode11_container_with_most_water

---
+++ test/leetcode/leetcode11_container_with_most_water/SolutionTest.java
@@ -0,0 +1,20 @@
+package leetcode.leetcode11_container_with_most_water;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+
+    @Test
+    void containerWithMostWaterTest() {
+        Solution s = new Solution();
+        int result1 = s.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7});
+        int result2 = s.maxArea(new int[]{1, 1});
+
+        assertEquals(49, result1);
+        assertEquals(1, result2);
+
+    }
+}
