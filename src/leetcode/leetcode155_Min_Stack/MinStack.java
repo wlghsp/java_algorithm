cacb3f128a2e7fcf618b1cd25531e1adb29e308d
JIHO CHOI <wlghsp@gmail.com>
2023-11-01 21:56:56 +0900

    leetcode155_Min_Stack

---
+++ src/leetcode/leetcode155_Min_Stack/MinStack.java
@@ -0,0 +1,41 @@
+package leetcode.leetcode155_Min_Stack;
+
+import java.util.Stack;
+
+public class MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        if (!stack.isEmpty()) {
+            int top = stack.pop();
+
+            if (!minStack.isEmpty() && top == minStack.peek()) {
+                minStack.pop();
+            }
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+}
