060453ec745dfa89c92cd4e55ed247104fc289b9
JIHO CHOI <wlghsp@gmail.com>
2023-11-05 04:23:00 +0900

    leetcode217_contains_duplicate: add more efficient solution

--- src/leetcode/leetcode217_contains_duplicate/Solution.java
+++ src/leetcode/leetcode217_contains_duplicate/Solution.java
@@ -1,10 +1,29 @@
 package leetcode.leetcode217_contains_duplicate;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Solution {
 
+    /**
+     * Time : O(n)
+     * Space: O(n)
+     */
     public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) return true;
+            set.add(num);
+        }
+        return false;
+    }
+
+    /**
+     * Time : O(n log n)
+     * Space: O(1)
+     */
+    public boolean containsDuplicate1(int[] nums) {
         Arrays.sort(nums);
         for (int i = 1; i < nums.length; i++) {
             if (nums[i - 1] == nums[i]) return true;
