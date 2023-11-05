edffd850d0687986394f29ada1f948fa69864069
JIHO CHOI <wlghsp@gmail.com>
2023-11-05 17:34:41 +0900

    leetcode22_generate_parentheses

---
+++ src/leetcode/leetcode22_generate_parentheses/Solution.java
@@ -0,0 +1,34 @@
+package leetcode.leetcode22_generate_parentheses;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution {
+
+    List<String> answer = new ArrayList<>();
+    Stack<String> stk = new Stack<>();
+
+
+    public List<String> generateParenthesis(int n) {
+        backTrack(0, 0, n);
+        return answer;
+    }
+
+    private void backTrack(int openCnt, int closeCnt, int n) {
+        if (openCnt == closeCnt && closeCnt == n) {
+            answer.add(String.join("", stk));
+        }
+        if (openCnt < n) {
+            stk.push("(");
+            backTrack(openCnt + 1, closeCnt, n);
+            stk.pop();
+        }
+        if (closeCnt < openCnt) {
+            stk.push(")");
+            backTrack(openCnt, closeCnt + 1, n);
+            stk.pop();
+        }
+    }
+
+}
---
+++ test/leetcode/leetcode22_generate_parentheses/SolutionTest.java
@@ -0,0 +1,22 @@
+package leetcode.leetcode22_generate_parentheses;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void testGenerateParentheses() {
+        Solution s1 = new Solution();
+        Solution s2 = new Solution();
+        List<String> result1 = s1.generateParenthesis(3);
+        List<String> result2 = s2.generateParenthesis(1);
+        String[] expected1 = {"((()))","(()())","(())()","()(())","()()()"};
+        String[] expected2 = {"()"};
+        assertArrayEquals(expected1, result1.toArray(new String[0]));
+        assertArrayEquals(expected2, result2.toArray(new String[0]));
+    }
+}
