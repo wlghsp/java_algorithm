bd825e38fa7a26f773404de559f9cb09a0d6cd8f
JIHO CHOI <wlghsp@gmail.com>
2023-11-05 22:38:39 +0900

    leetcode3_longest_substring_without_repeating_characters

---
+++ test/leetcode/leetcode3_longest_substring_without_repeating_characters/SolutionTest.java
@@ -0,0 +1,19 @@
+package leetcode.leetcode3_longest_substring_without_repeating_characters;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void testLongestSubstringWithoutRepeatingCharacters() {
+        Solution s = new Solution();
+        int result1 = s.lengthOfLongestSubstring("abcabcbb");
+        int result2 = s.lengthOfLongestSubstring("bbbbb");
+        int result3 = s.lengthOfLongestSubstring("pwwkew");
+        assertEquals(3, result1);
+        assertEquals(1, result2);
+        assertEquals(3, result3);
+    }
+}
