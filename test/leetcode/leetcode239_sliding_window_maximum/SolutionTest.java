cc741d452f3ef95d87ab9d289a62c2069d4d3068
JIHO CHOI <wlghsp@gmail.com>
2023-11-19 19:43:49 +0900

    leetcode239_sliding_window_maximum

---
+++ test/leetcode/leetcode239_sliding_window_maximum/SolutionTest.java
@@ -0,0 +1,18 @@
+package leetcode.leetcode239_sliding_window_maximum;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void test() {
+        Solution s = new Solution();
+        int[] result1 = s.maxSlidingWindow(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3);
+        int[] result2 = s.maxSlidingWindow(new int[]{1}, 1);
+
+        assertArrayEquals(new int[]{3, 3, 5, 5, 6, 7}, result1);
+        assertArrayEquals(new int[]{1}, result2);
+    }
+}
