c9eacbe4734b2171669c94b1f10aa653eb0ff179
JIHO CHOI <wlghsp@gmail.com>
2023-11-25 18:44:58 +0900

    leetcode347_Top_K_Frequent_Elements 풀이 추가
    - 버켓소트 적용

--- src/leetcode/leetcode347_Top_K_Frequent_Elements/Solution.java
+++ src/leetcode/leetcode347_Top_K_Frequent_Elements/Solution.java
@@ -1,12 +1,43 @@
 package leetcode.leetcode347_Top_K_Frequent_Elements;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Solution {
 
+    /**
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     */
     public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        List<Integer>[] bucket = new ArrayList[nums.length + 1];
+        for (int num : nums) {
+            count.merge(num, 1, Integer::sum);
+        }
+        for (Integer key : count.keySet()) {
+            int freq = count.get(key);
+            if (bucket[freq] == null) bucket[freq] = new ArrayList<>();
+            bucket[freq].add(key);
+        }
+        int index = 0;
+        int[] answer = new int[k];
+        for (int i = nums.length; i >= 0; i--) {
+            if (bucket[i] != null) {
+                for (int val : bucket[i]) {
+                    answer[index++] = val;
+                    if (index == k) {
+                        return answer;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+
+    public int[] topKFrequent1(int[] nums, int k) {
         int[] answer = new int[k];
         Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
