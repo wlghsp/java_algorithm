242277fee1c57d20baa555d8cf5ee936ce8940a8
JIHO CHOI <wlghsp@gmail.com>
2023-11-25 09:44:51 +0900

    leetcode392_is_subsequence 풀이 최적화

--- src/leetcode/leetcode392_is_subsequence/Solution.java
+++ src/leetcode/leetcode392_is_subsequence/Solution.java
@@ -3,6 +3,17 @@ package leetcode.leetcode392_is_subsequence;
 public class Solution {
 
     public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        return i == s.length();
+    }
+
+    public boolean isSubsequence1(String s, String t) {
         if (s.isEmpty()) {
             return true;
         }
