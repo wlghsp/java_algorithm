83b0741b1a8bb1c046713c8ef6e5ca92f5045670
JIHO CHOI <wlghsp@gmail.com>
2023-11-25 11:29:54 +0900

    leetcode289_word_pattern

---
+++ src/leetcode/leetcode289_word_pattern/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.leetcode289_word_pattern;
+
+import java.util.*;
+
+public class Solution {
+
+    public boolean wordPattern(String pattern, String s) {
+        String[] strs = s.split(" ");
+
+        if (strs.length != pattern.length()) return false;
+
+        Map<Character, String> charToWord = new HashMap<>();
+        Map<String, Character> wordToChar = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            String word = strs[i];
+            char c = pattern.charAt(i);
+            if (charToWord.containsKey(c) && !charToWord.get(c).equals(word)) return false;
+            if (wordToChar.containsKey(word) && wordToChar.get(word) != c) return false;
+            charToWord.put(c, word);
+            wordToChar.put(word, c);
+        }
+
+        return true;
+    }
+}
---
+++ test/leetcode/leetcode289_word_pattern/SolutionTest.java
@@ -0,0 +1,21 @@
+package leetcode.leetcode289_word_pattern;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+
+    @Test
+    void test() {
+        Solution s = new Solution();
+        boolean result1 = s.wordPattern("abba", "dog cat cat dog");
+        boolean result2 = s.wordPattern("abba", "dog cat cat fish");
+        boolean result3 = s.wordPattern("aaaa", "dog cat cat dog");
+
+        assertTrue(result1);
+        assertFalse(result2);
+        assertFalse(result3);
+    }
+}
