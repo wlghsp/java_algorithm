f95537fe4f56c0c48e7e70f4a1573ba7a486b073
JIHO CHOI <wlghsp@gmail.com>
2023-11-26 12:51:16 +0900

    프로그래머스 level 1 푸드 파이트 대회

--- src/programmers/level1/푸드파이트대회/Solution.java
+++ src/programmers/level1/푸드파이트대회/Solution.java
@@ -3,7 +3,16 @@ package programmers.level1.푸드파이트대회;
 public class Solution {
 
     public String solution(int[] food) {
-        String answer = "";
-        return answer;
+        StringBuilder front = new StringBuilder();
+        StringBuilder back = new StringBuilder();
+        for (int i = 1; i < food.length; i++) {
+            while (food[i] >= 2) {
+                front.append(i);
+                back.insert(0, i);
+                food[i] -= 2;
+            }
+        }
+
+        return front + "0" + back;
     }
 }
