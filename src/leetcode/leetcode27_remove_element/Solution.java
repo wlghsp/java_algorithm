e474d074b9bcaae533514a3fdb9035ec816bf107
JIHO CHOI <wlghsp@gmail.com>
2023-11-28 17:56:05 +0900

    leetcode27_remove_element 주석 추가

--- src/leetcode/leetcode27_remove_element/Solution.java
+++ src/leetcode/leetcode27_remove_element/Solution.java
@@ -4,15 +4,15 @@ public class Solution {
 
     public int removeElement(int[] nums, int val) {
         int l = 0, r = nums.length - 1;
-        while (l <= r) {
-            if (nums[l] == val) {
-                swap(nums, l, r);
-                r--;
-            } else {
-                l++;
+        while (l <= r) { // 반복은 l과 r이 같아 질 때까지
+            if (nums[l] == val) { // val과 같은 수를 발견하면
+                swap(nums, l, r); // l과 r의 인덱스의 수를 교환
+                r--; // r의 위치를 채워 넣었으니 r을 감소시킴
+            } else { // val과 다른 수라면
+                l++; // val 과 같은 수가 아니므로 l을 증가시킨다.
             }
         }
-        return l;
+        return l; // val과 같지 않은 숫자의 갯수를 반환
     }
 
     private void swap(int[] nums, int i , int j) {
