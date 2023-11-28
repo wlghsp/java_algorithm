9ffbef50a61fbb8edafebb97fa7297d0b6e373ff
jihochoi <wlghsp@gmail.com>
2023-11-28 10:31:07 +0900

    leetcode206_reverse_linked_list

---
+++ src/leetcode/leetcode206_reverse_linked_list/Solution.java
@@ -0,0 +1,17 @@
+package leetcode.leetcode206_reverse_linked_list;
+
+public class Solution {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+}
