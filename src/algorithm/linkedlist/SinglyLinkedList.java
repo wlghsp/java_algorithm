9c47bac943839367089119950adeb2d3c17744ef
JIHO CHOI <wlghsp@gmail.com>
2023-11-30 00:01:26 +0900

    단방향 Linked List 구현 - 유튜브 엔지니어 대한민국

---
+++ src/algorithm/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,16 @@
+package algorithm.linkedlist;
+
+public class SinglyLinkedList {
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.append(2);
+        head.append(3);
+        head.append(4);
+        head.retrieve();
+        head.delete(2);
+        head.delete(3);
+        head.retrieve();
+
+    }
+}
