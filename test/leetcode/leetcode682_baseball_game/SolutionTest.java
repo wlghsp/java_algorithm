1f2a3f4ac5d15f0be88cfb5965792eefcb3d29fb
JIHO CHOI <wlghsp@gmail.com>
2023-12-02 10:56:13 +0900

    leetcode 682. Baseball Game

---
+++ test/leetcode/leetcode682_baseball_game/SolutionTest.java
@@ -0,0 +1,18 @@
+package leetcode.leetcode682_baseball_game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void test() {
+        Solution s = new Solution();
+        int result1 = s.calPoints(new String[]{"5", "2", "C", "D", "+"});
+        int result2 = s.calPoints(new String[]{"5","-2","4","C","D","9","+","+"});
+
+        assertEquals(30, result1);
+        assertEquals(27, result2);
+    }
+}
