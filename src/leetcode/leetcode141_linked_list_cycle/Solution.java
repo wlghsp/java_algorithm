7ae1f813979027b919d34ed0f92c2610d9761b40
JIHO CHOI <wlghsp@gmail.com>
2023-12-03 19:32:06 +0900

    leetcode 141 Linked List Cycle

---
+++ src/leetcode/leetcode141_linked_list_cycle/Solution.java
@@ -0,0 +1,15 @@
+package leetcode.leetcode141_linked_list_cycle;
+
+public class Solution {
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
+}
