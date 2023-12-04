b6e36c78989b51359dcdffad334cb9fd723ae65d
JIHO CHOI <wlghsp@gmail.com>
2023-12-04 21:24:20 +0900

    프로그래머스 level1 둘만의 암호

--- src/programmers/level1/둘만의암호/Solution.java
+++ src/programmers/level1/둘만의암호/Solution.java
@@ -3,7 +3,16 @@ package programmers.level1.둘만의암호;
 public class Solution {
 
     public String solution(String s, String skip, int index) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        for (char c : skip.toCharArray()) {
+            alphabet = alphabet.replace(Character.toString(c), "");
+        }
+        for (char c : s.toCharArray()) {
+            int num = (alphabet.indexOf(c) + index) % alphabet.length() ;
+            sb.append(alphabet.charAt(num));
+        }
+
+        return sb.toString();
     }
 }
