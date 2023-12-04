6a109c91edfe4adb9e21225f9288533c3fe1540d
jihochoi <wlghsp@gmail.com>
2023-12-04 15:38:57 +0900

    프로그래머스 level1 체육복 Set, 배열 풀이 추가

--- test/programmers/level1/체육복/Solution.java
+++ test/programmers/level1/체육복/Solution.java
@@ -4,7 +4,71 @@ import java.util.*;
 
 public class Solution {
 
+
+    // 배열 풀이
     public int solution(int n, int[] lost, int[] reserve) {
+        int[] student = new int[n + 2];
+
+        for (int i : lost) {
+            student[i]--;
+        }
+        for (int i : reserve) {
+            student[i]++;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            if (student[i] == 1) {
+                int front = i - 1;
+                int back = i + 1;
+                if (student[front] == -1) {
+                    student[front]++;
+                    student[i]--;
+                } else if (student[back] == -1) {
+                    student[back]++;
+                    student[i]--;
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 1; i < n + 1; i++) {
+            if (student[i] >= 0) ans++;
+        }
+        return ans;
+    }
+
+    // Set 풀이
+    public int solution2(int n, int[] lost, int[] reserve) {
+        Set<Integer> lostSet = new HashSet<>();
+        Set<Integer> resSet = new HashSet<>();
+
+        for (int i : reserve) {
+            resSet.add(i);
+        }
+
+        for (int i : lost) {
+            if (resSet.contains(i)) {
+                resSet.remove(i);
+            } else {
+                lostSet.add(i);
+            }
+        }
+
+        for (Integer i : resSet) {
+            if (lostSet.contains(i - 1)) {
+                lostSet.remove(i - 1);
+            } else if (lostSet.contains(i + 1)) {
+                lostSet.remove(i + 1);
+            }
+        }
+
+
+        return n - lostSet.size();
+
+    }
+
+    // hashMap 풀이
+    public int solution1(int n, int[] lost, int[] reserve) {
         Map<Integer, Integer> map = new HashMap<>();
 
         Arrays.sort(lost);
