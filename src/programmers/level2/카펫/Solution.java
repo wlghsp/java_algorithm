433ae209f9330112b99240e95c4755971c444b8d
JIHO CHOI <wlghsp@gmail.com>
2023-12-10 23:34:52 +0900

    프로그래머스 레벨 2 카펫 책 보고 풀이 학습 완료

--- src/programmers/level2/카펫/Solution.java
+++ src/programmers/level2/카펫/Solution.java
@@ -1,8 +1,26 @@
 package programmers.level2.카펫;
 
 public class Solution {
+    /**
+     * brown = boundary, yellow = center
+     * center = total - boundary
+     * boundary = width * 2 + (width - 2) * 2
+     * = (width + height - 2) * 2
+     * 완전탐색하여 center와 boundary가 yello, brown에 일치하면
+     * 그 width, height 를 반환한다.
+     */
     public int[] solution(int brown, int yellow) {
-        int[] answer = {};
-        return answer;
+        for (int width = 0; width <= 5000; width++) {
+            for (int height = 0; height < width; height++) {
+                int boundary = (width + height - 2) * 2;
+                int center = width * height - boundary;
+
+                if (brown == boundary && yellow == center) {
+                    return new int[]{width, height};
+                }
+            }
+        }
+
+        return null;
     }
 }
