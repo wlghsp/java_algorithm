3e0da717c892c4e6e021735bf0a408fc4856213b
JIHO CHOI <wlghsp@gmail.com>
2023-12-10 11:52:13 +0900

    leetcode 23. Merge k Sorted Lists

---
+++ src/leetcode/leetcode23_merge_k_sorted_lists/Solution.java
@@ -0,0 +1,75 @@
+package leetcode.leetcode23_merge_k_sorted_lists;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Solution {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        int size = lists.length;
+        int interval = 1;
+
+        while (interval < size) {
+            for (int i = 0; i < size - interval; i += 2 * interval) {
+                lists[i] = merge(lists[i], lists[i + interval]);
+            }
+
+            interval *= 2;
+        }
+
+        return size > 0 ? lists[0] : null;
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+
+            curr = curr.next;
+        }
+
+        curr.next = l1 != null ? l1 : l2;
+
+        return dummy.next;
+    }
+
+
+
+    public ListNode mergeKLists1(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+
+        PriorityQueue<ListNode> queue = new PriorityQueue<>(Comparator.comparingInt(a -> a.val));
+
+        for (ListNode node : lists) {
+            if (node != null) {
+                queue.offer(node);
+            }
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (!queue.isEmpty()) {
+            ListNode node = queue.poll();
+            current.next = node;
+            current = current.next;
+
+            if (node.next != null) {
+                queue.offer(node.next);
+            }
+        }
+
+        return dummy.next;
+
+    }
+}
