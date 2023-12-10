4a2139e981009ef272a3c4b35854e05c6c31d2ee
JIHO CHOI <wlghsp@gmail.com>
2023-12-10 11:02:52 +0900

    Leetcode 2. Add Two Numbers

---
+++ src/leetcode/leetcode2_add_two_numbers/ListNode.java
@@ -0,0 +1,10 @@
+package leetcode.leetcode2_add_two_numbers;
+
+public class ListNode {
+
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
---
+++ src/leetcode/leetcode2_add_two_numbers/Solution.java
@@ -0,0 +1,29 @@
+package leetcode.leetcode2_add_two_numbers;
+
+public class Solution {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode();
+        ListNode cur = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int digit1 = (l1 != null) ? l1.val : 0;
+            int digit2 = (l2 != null) ? l2.val : 0;
+
+            // new digit
+            int sum = digit1 + digit2 + carry;
+            int digit = sum % 10;
+            carry = sum / 10;
+            ListNode newNode = new ListNode(digit);
+            cur.next = newNode;
+
+            // update ptrs
+            cur = cur.next;
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+        }
+
+        return dummyHead.next;
+    }
+}
