39aa3db8a1ad6eeea5c42deca2a4f62ffb214c01
CHOIJIHO <wlghsp@gmail.com>
2023-12-10 16:05:53 +0900

    프로그래머스 level 2 이진변환 반복하기 책에 있는 풀이 학습

--- src/programmers/level2/이진변환반복하기/Solution.java
+++ src/programmers/level2/이진변환반복하기/Solution.java
@@ -4,7 +4,33 @@ import java.util.Arrays;
 
 public class Solution {
 
+    private int countZeros(String s) {
+        int zeros = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '0') zeros++;
+        }
+        return zeros;
+    }
     public int[] solution(String s) {
+        int loop = 0;
+        int removed = 0;
+
+        // s가 "1"이 될 때까지 반복하며 loop, removed 누적
+        while (!s.equals("1")) {
+            // s 변환하며 loop, removed 누적
+            int zeros = countZeros(s);
+            removed += zeros;
+            loop++;
+
+
+            int ones = s.length() - zeros;
+            s = Integer.toString(ones, 2);
+        }
+
+        return new int[]{loop, removed};
+    }
+
+    public int[] solution1(String s) {
         int zeroCnt = 0;
         int cnt = 0;
         while (!s.equals("1")) {
