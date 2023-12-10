5b6b78430765eb8e3ae4c6dde4646199ab4bdf88
CHOIJIHO <wlghsp@gmail.com>
2023-12-10 18:59:59 +0900

    프로그래머스 level 1 K번째 수

---
+++ test/programmers/level1/k번째수/SolutionTest.java
@@ -0,0 +1,16 @@
+package programmers.level1.k번째수;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void test() {
+        Solution s = new Solution();
+        int[] result1 = s.solution(new int[]{1, 5, 2, 6, 3, 7, 4}, new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}});
+
+        assertArrayEquals(new int[]{5, 6, 3}, result1);
+    }
+}
