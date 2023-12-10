28f0a1b827c08d01b561d08a85a18c384de0c9fb
JIHO CHOI <wlghsp@gmail.com>
2023-12-10 11:52:04 +0900

    Leetcode 138. Copy List with Random Pointer

---
+++ src/leetcode/leetcode138_copy_list_with_random_pointer/Node.java
@@ -0,0 +1,14 @@
+package leetcode.leetcode138_copy_list_with_random_pointer;
+
+public class Node {
+
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
