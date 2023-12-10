3192bd5338c4d83ee2d1b83faf5b1bc3951f6c88
CHOIJIHO <wlghsp@gmail.com>
2023-12-10 14:09:18 +0900

    프로그래머스 level 2 피보나치 수 : 책에 있는 풀이 학습

--- src/programmers/level2/피보나치수/Solution.java
+++ src/programmers/level2/피보나치수/Solution.java
@@ -1,11 +1,28 @@
 package programmers.level2.피보나치수;
 
+import java.util.Arrays;
+
 public class Solution {
+    private final int[] mem = new int[100001];
+
+    private int fibonacci(int n) {
+        if (mem[n] != -1) return mem[n];
+        if (n == 0 || n == 1) return n;
+
+        return mem[n] = (fibonacci(n - 1) + fibonacci(n -2)) % 1234567;
+    }
+    public int solution(int n) {
+        Arrays.fill(mem, -1);
+        for (int i = 0; i <= n; i++) {
+            fibonacci(n);
+        }
+        return fibonacci(n);
+    }
 
     /**
      * dp공간을 사용하지 않고 변수 2개로 구하는 방식
      */
-    public int solution(int n) {
+    public int solution2(int n) {
         int answer = 0;
         int f1 = 0, f2 = 1;
         for (int i = 2; i <= n; i++) {
