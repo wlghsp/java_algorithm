41c1d7051dabf02466947272c86cc0835073a4b2
JIHO CHOI <wlghsp@gmail.com>
2024-01-08 23:03:27 +0900

    프로그래머스 Level2 영어 끝말잇기

--- src/programmers/level2/영어끝말잇기/Solution.java
+++ src/programmers/level2/영어끝말잇기/Solution.java
@@ -1,8 +1,27 @@
 package programmers.level2.영어끝말잇기;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = {};
+        int[] answer = new int[]{0, 0};
+        Set<String> set = new HashSet<>();
+        String lastWord = "";
+        for (int i = 0; i < words.length; i++) {
+            if (set.contains(words[i])) {
+                return new int[]{i % n + 1, i / n + 1};
+            }
+
+            if (i > 0 && !set.isEmpty()) {
+                if (lastWord.charAt(lastWord.length() - 1) != words[i].charAt(0)) {
+                    return new int[]{i % n + 1, i / n + 1};
+                }
+            }
+            lastWord = words[i];
+            set.add(words[i]);
+        }
+
         return answer;
     }
 }
--- test/programmers/level2/영어끝말잇기/SolutionTest.java
+++ test/programmers/level2/영어끝말잇기/SolutionTest.java
@@ -11,7 +11,7 @@ class SolutionTest {
         Solution s = new Solution();
         int[] result1 = s.solution(3, new String[]{"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"});
         int[] result2 = s.solution(5, new String[]{"hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"});
-        int[] result3 = s.solution(2, new String[]{"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"});
+        int[] result3 = s.solution(2, new String[]{"hello", "one", "even", "never", "now", "world", "draw"});
         assertArrayEquals(new int[]{3, 3}, result1);
         assertArrayEquals(new int[]{0, 0}, result2);
         assertArrayEquals(new int[]{1, 3}, result3);
