1b8804ec620936311d9007a716815cf9b07a3095
JIHO CHOI <wlghsp@gmail.com>
2024-01-09 21:14:22 +0900

    leetcode226_invert_binary_tree

---
+++ src/leetcode/leetcode226_invert_binary_tree/Solution.java
@@ -0,0 +1,18 @@
+package leetcode.leetcode226_invert_binary_tree;
+
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+
+        // swap
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        // DFS
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
