f892df703cd95cff2cd3bcd1269497813ed5cef5
CHOIJIHO <wlghsp@gmail.com>
2024-01-28 16:07:34 +0900

    leetcode543_diameter_of_binary_tree

--- src/leetcode/trees/leetcode543_diameter_of_binary_tree/Solution.java
+++ src/leetcode/trees/leetcode543_diameter_of_binary_tree/Solution.java
@@ -1,7 +1,20 @@
 package leetcode.trees.leetcode543_diameter_of_binary_tree;
 
 public class Solution {
+    static int result = 0;
+    private int dfs(TreeNode root) {
+        if (root == null) return -1;
+
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        result = Math.max(result, 2 + left + right);
+
+        return 1 + Math.max(left, right);
+    }
+
     public int diameterOfBinaryTree(TreeNode root) {
-        return 0;
+        result = 0;
+        dfs(root);
+        return result;
     }
 }
