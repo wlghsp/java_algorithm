e869b6713d445b651ee555a18f579393dbc18052
CHOIJIHO <wlghsp@gmail.com>
2024-01-28 18:17:54 +0900

    leetcode124_binary_tree_maximum_path_sum 풀이 예정

---
+++ src/leetcode/trees/leetcode124_binary_tree_maximum_path_sum/Solution.java
@@ -0,0 +1,7 @@
+package leetcode.trees.leetcode124_binary_tree_maximum_path_sum;
+
+public class Solution {
+    public int maxPathSum(TreeNode root) {
+        return 0;
+    }
+}
---
+++ src/leetcode/trees/leetcode124_binary_tree_maximum_path_sum/TreeNode.java
@@ -0,0 +1,14 @@
+package leetcode.trees.leetcode124_binary_tree_maximum_path_sum;
+
+ public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+ }
