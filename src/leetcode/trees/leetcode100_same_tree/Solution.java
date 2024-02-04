d08317cb3b94c0ae548bb9b725874d830c8bc05e
JIHO CHOI <wlghsp@gmail.com>
2024-02-04 18:13:08 +0900

    leetcode100_same_tree 풀이 예정

---
+++ src/leetcode/trees/leetcode100_same_tree/Solution.java
@@ -0,0 +1,11 @@
+package leetcode.trees.leetcode100_same_tree;
+
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        else if (p == null || q == null) return false;
+
+
+        return false;
+    }
+}
