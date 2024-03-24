48965176e1c2c2abeda792884124c3843994a69c
CHOIJIHO <wlghsp@gmail.com>
2024-03-24 12:38:27 +0900

    leetcode134_gas_station

--- src/leetcode/greedy/leetcode134_gas_station/Solution.java
+++ src/leetcode/greedy/leetcode134_gas_station/Solution.java
@@ -1,9 +1,24 @@
 package leetcode.greedy.leetcode134_gas_station;
 
+import java.util.Arrays;
+
 public class Solution {
 
     public int canCompleteCircuit(int[] gas, int[] cost) {
+        if (Arrays.stream(gas).sum() < Arrays.stream(cost).sum()) return -1;
+
         int answer = 0;
+        int total = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            total += gas[i] - cost[i];
+
+            if (total < 0) {
+                total = 0;
+                answer++;
+            }
+        }
+
         return answer;
     }
 }
