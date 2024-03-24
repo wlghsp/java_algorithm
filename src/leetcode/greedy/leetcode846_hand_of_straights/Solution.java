3b3090c7cd7341d0e1df33b796d6c51be60492d7
CHOIJIHO <wlghsp@gmail.com>
2024-03-24 17:23:30 +0900

    leetcode846_hand_of_straights

--- src/leetcode/greedy/leetcode846_hand_of_straights/Solution.java
+++ src/leetcode/greedy/leetcode846_hand_of_straights/Solution.java
@@ -1,8 +1,43 @@
 package leetcode.greedy.leetcode846_hand_of_straights;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 public class Solution {
 
     public boolean isNStraightHand(int[] hand, int groupSize) {
+        if (hand.length % groupSize != 0) {
+            return false;
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        Map<Integer, Integer> countMap = new HashMap<>();
+
+        Arrays.stream(hand)
+                .forEach(h ->
+                        countMap.put(h, countMap.getOrDefault(h, 0) + 1));
+
+        countMap.keySet().forEach(pq::offer);
+
+        while (!pq.isEmpty()) {
+            int first = pq.peek();
+
+            for (int i = first; i < first + groupSize; i++) {
+                if (!countMap.containsKey(i)) {
+                    return false;
+                }
+                countMap.put(i, countMap.get(i) - 1);
+                if (countMap.get(i) == 0) { // 다 사용했는데 최솟값이 아니라면
+                    if (i != pq.peek()) {
+                        return false;
+                    }
+                    pq.poll();
+                }
+
+            }
+        }
         return true;
     }
 }
