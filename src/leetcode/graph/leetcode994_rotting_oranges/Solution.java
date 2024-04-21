ec232cf94ad007bafa09536e0c299f0e8a81f74b
JIHO CHOI <wlghsp@gmail.com>
2024-04-21 18:56:57 +0900

    leetcode994_rotting_oranges

--- src/leetcode/graph/leetcode994_rotting_oranges/Solution.java
+++ src/leetcode/graph/leetcode994_rotting_oranges/Solution.java
@@ -1,7 +1,42 @@
 package leetcode.graph.leetcode994_rotting_oranges;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution {
     public int orangesRotting(int[][] grid) {
-        return 0;
+        int m = grid.length, n = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        int fresh = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    queue.offer(new int[]{i, j});
+                } else if (grid[i][j] == 1) {
+                    fresh++;
+                }
+            }
+        }
+
+        int count = 0;
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        while (!queue.isEmpty() && fresh != 0) {
+            count++;
+            int sz = queue.size();
+            for (int i = 0; i < sz; i++) {
+                int[] rotten = queue.poll();
+                int r = rotten[0], c = rotten[1];
+                for (int[] dir : dirs) {
+                    int x = r + dir[0], y = c + dir[1];
+                    if (0 <= x && x < m && 0 <= y && y < n && grid[x][y] == 1) {
+                        grid[x][y] = 2;
+                        queue.offer(new int[]{x, y});
+                        fresh--;
+                    }
+                }
+            }
+        }
+        return fresh == 0 ? count : -1;
     }
 }
--- test/leetcode/graph/leetcode994_rotting_oranges/SolutionTest.java
+++ test/leetcode/graph/leetcode994_rotting_oranges/SolutionTest.java
@@ -11,9 +11,11 @@ class SolutionTest {
     void test() {
         Solution s = new Solution();
         int result1 = s.orangesRotting(new int[][]{{2, 1, 1}, {1, 1, 0}, {0, 1, 1}});
-        int result2 = s.orangesRotting(new int[][]{{0, 2}});
+        int result2 = s.orangesRotting(new int[][]{{2, 1, 1}, {0, 1, 1}, {1, 0, 1}});
+        int result3 = s.orangesRotting(new int[][]{{0, 2}});
 
-        assertEquals(-1, result1);
-        assertEquals(0, result2);
+        assertEquals(4, result1);
+        assertEquals(-1, result2);
+        assertEquals(0, result3);
     }
 }
