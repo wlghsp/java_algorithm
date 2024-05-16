88b40689d3bb4ded4458dd3edad620e5ecae08c1
JIHO CHOI <wlghsp@gmail.com>
2024-05-16 22:46:35 +0900

    boj11478_서로다른부분문자열의개수

---
+++ src/baekjoon/silver/silver3/boj11478_서로다른부분문자열의개수/Main.java
@@ -0,0 +1,22 @@
+package baekjoon.silver.silver3.boj11478_서로다른부분문자열의개수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                set.add(s.substring(i, j));
+            }
+        }
+        System.out.println(set.size());
+    }
+}
