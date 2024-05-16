f7cb09c201271f86035e61cbf3dc46aa4a693386
JIHO CHOI <wlghsp@gmail.com>
2024-05-16 23:51:41 +0900

    boj15439_베라의패션

---
+++ src/baekjoon/bronze/bronze4/boj15439_베라의패션/Main.java
@@ -0,0 +1,14 @@
+package baekjoon.bronze.bronze4.boj15439_베라의패션;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N * (N - 1));
+    }
+}
