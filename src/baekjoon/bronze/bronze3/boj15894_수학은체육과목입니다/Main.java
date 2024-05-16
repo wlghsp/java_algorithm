b7b1c385d9625b303989029e388f49223b6ec66f
CHOIJIHO <wlghsp@gmail.com>
2024-05-16 17:11:13 +0900

    boj15894_수학은체육과목입니다

---
+++ src/baekjoon/bronze/bronze3/boj15894_수학은체육과목입니다/Main.java
@@ -0,0 +1,14 @@
+package baekjoon.bronze.bronze3.boj15894_수학은체육과목입니다;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Long.parseLong(br.readLine());
+        System.out.println(4 * N);
+    }
+}
