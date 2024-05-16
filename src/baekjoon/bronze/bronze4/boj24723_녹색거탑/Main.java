be1c21581b2feaaee1bc6e16d5eddce93c61df63
JIHO CHOI <wlghsp@gmail.com>
2024-05-17 00:06:32 +0900

    boj24723_녹색거탑

---
+++ src/baekjoon/bronze/bronze4/boj24723_녹색거탑/Main.java
@@ -0,0 +1,14 @@
+package baekjoon.bronze.bronze4.boj24723_녹색거탑;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println((int) Math.pow(2, N));
+    }
+}
