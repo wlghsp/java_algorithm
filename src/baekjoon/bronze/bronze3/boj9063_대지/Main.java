07c49c20c10ed448e74909071c43889cd428231f
CHOIJIHO <wlghsp@gmail.com>
2024-05-16 17:21:55 +0900

    boj9063_대지

---
+++ src/baekjoon/bronze/bronze3/boj9063_대지/Main.java
@@ -0,0 +1,29 @@
+package baekjoon.bronze.bronze3.boj9063_대지;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int maxX = Integer.MIN_VALUE, maxY = Integer.MIN_VALUE;
+        int minX = Integer.MAX_VALUE, minY = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            maxX = Math.max(maxX, x);
+            maxY = Math.max(maxY, y);
+            minX = Math.min(minX, x);
+            minY = Math.min(minY, y);
+        }
+
+        System.out.println((maxX - minX) * (maxY - minY));
+    }
+}
