c28eed3f40b9901b948e3c34d6c538827f92c62d
CHOIJIHO <wlghsp@gmail.com>
2024-05-16 18:50:19 +0900

    boj2903_중앙이동알고리즘 - 풀이 ing

---
+++ src/baekjoon/bronze/bronze3/boj2903_중앙이동알고리즘/Main.java
@@ -0,0 +1,12 @@
+package baekjoon.bronze.bronze3.boj2903_중앙이동알고리즘;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
