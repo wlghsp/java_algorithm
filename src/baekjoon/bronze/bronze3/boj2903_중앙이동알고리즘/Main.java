6949d3ca15c06c061446264f046d7b8f18995ef3
JIHO CHOI <wlghsp@gmail.com>
2024-05-16 19:59:07 +0900

    boj2903_중앙이동알고리즘 - 풀이 complete

--- src/baekjoon/bronze/bronze3/boj2903_중앙이동알고리즘/Main.java
+++ src/baekjoon/bronze/bronze3/boj2903_중앙이동알고리즘/Main.java
@@ -1,12 +1,18 @@
 package baekjoon.bronze.bronze3.boj2903_중앙이동알고리즘;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        int N = Integer.parseInt(br.readLine());
+        int dot = 2;
+        while (N-- > 0) {
+            dot = dot * 2 - 1;
+        }
+        System.out.println(dot * dot);
     }
 }
