c82aeddf760cb02f937e33fa579cf1087bf73824
CHOIJIHO <wlghsp@gmail.com>
2024-05-16 16:03:41 +0900

    boj9086_문자열

---
+++ src/baekjoon/bronze/bronze5/boj9086_문자열/Main.java
@@ -0,0 +1,23 @@
+package baekjoon.bronze.bronze5.boj9086_문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < T; i++) {
+            String s = br.readLine();
+            sb.append(s.charAt(0)).append(s.charAt(s.length() - 1)).append(System.lineSeparator());
+        }
+
+        System.out.println(sb);
+    }
+}
