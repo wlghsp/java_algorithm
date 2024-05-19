c2e38c102e5dff73f3f5d41d3a1370098015dc1d
CHOIJIHO <wlghsp@gmail.com>
2024-05-19 16:00:55 +0900

    leetcode46_permutations

--- src/leetcode/backtracking/leetcode46_permutations/Solution.java
+++ src/leetcode/backtracking/leetcode46_permutations/Solution.java
@@ -1,10 +1,32 @@
 package leetcode.backtracking.leetcode46_permutations;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
 
     public List<List<Integer>> permute(int[] nums) {
-        return null;
+        List<List<Integer>> ans = new ArrayList<>();
+
+        backtracking(ans, nums, new ArrayList<>());
+
+        return ans;
     }
+
+    private void backtracking(List<List<Integer>> ans, int[] nums, List<Integer> list) {
+        if (list.size() == nums.length) {
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int num : nums) {
+            if (!list.contains(num)) {
+                list.add(num);
+                backtracking(ans, nums, list);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+
+
 }
