644989747a5f5aa98de86bb81eb9f1f0b5d6a7b2
CHOIJIHO <wlghsp@gmail.com>
2024-05-19 16:39:42 +0900

    leetcode131_palindrome_partitioning

--- src/leetcode/backtracking/leetcode131_palindrome_partitioning/Solution.java
+++ src/leetcode/backtracking/leetcode131_palindrome_partitioning/Solution.java
@@ -1,10 +1,41 @@
 package leetcode.backtracking.leetcode131_palindrome_partitioning;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
 
     public List<List<String>> partition(String s) {
-        return null;
+        List<List<String>> res = new ArrayList<>();
+        List<String> partitions = new ArrayList<>();
+        addPalindrome(s, 0, partitions, res);
+        return res;
+    }
+
+    private void addPalindrome(String s, int start, List<String> partitions, List<List<String>> res) {
+        if (start == s.length()) {
+            res.add(new ArrayList<>(partitions));
+            return;
+        }
+
+        for (int i = start + 1; i <= s.length(); i++) {
+            String str = s.substring(start, i);
+            if (isPalindrome(str)) {
+                partitions.add(str);
+                addPalindrome(s, i, partitions, res);
+                partitions.remove(partitions.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left++) != str.charAt(right--)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
