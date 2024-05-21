a9dfb549ac683fb9ede9f741ae4a6aa7ee958c44
JIHO CHOI <wlghsp@gmail.com>
2024-05-21 16:18:23 +0900

    boj1913_달팽이 풀이 예정

---
+++ src/baekjoon/silver/silver3/boj1913_달팽이/Main.java
@@ -0,0 +1,15 @@
+package baekjoon.silver.silver3.boj1913_달팽이;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+
+    }
+}
