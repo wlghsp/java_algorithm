6e37abf61e1124c92f5f6bcc1d60e19fc355f249
JIHO CHOI <wlghsp@gmail.com>
2024-05-21 16:58:05 +0900

    풀이 예정 문제

---
+++ src/programmers/level2/_124나라의숫자/Solution.java
@@ -0,0 +1,9 @@
+package programmers.level2._124나라의숫자;
+
+public class Solution {
+
+    public String solution(int n) {
+        String answer = "";
+        return answer;
+    }
+}
---
+++ test/programmers/level2/_124나라의숫자/SolutionTest.java
@@ -0,0 +1,23 @@
+package programmers.level2._124나라의숫자;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SolutionTest {
+
+
+    @Test
+    void test() {
+        Solution s = new Solution();
+        String result1 = s.solution(1);
+        String result2 = s.solution(2);
+        String result3 = s.solution(3);
+        String result4 = s.solution(4);
+
+        assertThat(result1).isEqualTo(1);
+        assertThat(result2).isEqualTo(2);
+        assertThat(result3).isEqualTo(4);
+        assertThat(result4).isEqualTo(11);
+    }
+}
