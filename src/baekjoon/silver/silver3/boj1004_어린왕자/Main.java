e4f11252c9b47c424a09ed7618a7112ffd25bc2d
jihochoi <wlghsp@gmail.com>
2024-06-04 11:29:19 +0900

    백준 1004 어린왕자 풀이 완료

--- src/baekjoon/silver/silver3/boj1004_어린왕자/Main.java
+++ src/baekjoon/silver/silver3/boj1004_어린왕자/Main.java
@@ -7,20 +7,36 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int T, x1, y1, x2, y2, n;
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("src/baekjoon/silver/silver3/boj1004_어린왕자/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        T = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        x1 = Integer.parseInt(st.nextToken());
-        y1 = Integer.parseInt(st.nextToken());
-        x2 = Integer.parseInt(st.nextToken());
-        y2 = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
 
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(br.readLine());
 
+            int answer = 0;
+            for (int j = 0; j < n; j++) {
+                st = new StringTokenizer(br.readLine());
+                int cx = Integer.parseInt(st.nextToken());
+                int cy = Integer.parseInt(st.nextToken());
+                int r = Integer.parseInt(st.nextToken());
+
+                boolean isStartPointInCircle = (Math.pow(x1 - cx, 2) + Math.pow(y1 - cy, 2)) <= Math.pow(r, 2);
+                boolean isEndPointInCircle = (Math.pow(x2 - cx, 2) + Math.pow(y2 - cy, 2)) <= Math.pow(r, 2);
+
+                if (isStartPointInCircle ^ isEndPointInCircle) {
+                    answer++;
+                }
+            }
+            System.out.println(answer);
+        }
     }
 }
