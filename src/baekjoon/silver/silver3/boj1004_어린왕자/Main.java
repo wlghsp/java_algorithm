9360271dd0681f9863ba0f71e5a784ed93cabcd6
jihochoi <wlghsp@gmail.com>
2024-06-04 12:36:07 +0900

    백준 1004 어린왕자 주석 추가

--- src/baekjoon/silver/silver3/boj1004_어린왕자/Main.java
+++ src/baekjoon/silver/silver3/boj1004_어린왕자/Main.java
@@ -29,10 +29,15 @@ public class Main {
                 int cy = Integer.parseInt(st.nextToken());
                 int r = Integer.parseInt(st.nextToken());
 
+                // 원의 중점까지의 거리가 같거나 작으면 원 안에 위치
+                // 2개 다 원의 안에 위치하면 진입/이탈 없음
+                // 1개의 점만 원의 안에 위치해야 진입 or 이탈 존재
+                //  1. 원의 중점과 출발점 거리가 반지름 보다 작고 도착점과의 거리는 반지름보다 커야 함
+                //  2. 원의 중점과 출발점 거리가 반지름 보다 크고 도착점과의 거리는 반지름보다 작아야 함
                 boolean isStartPointInCircle = (Math.pow(x1 - cx, 2) + Math.pow(y1 - cy, 2)) <= Math.pow(r, 2);
                 boolean isEndPointInCircle = (Math.pow(x2 - cx, 2) + Math.pow(y2 - cy, 2)) <= Math.pow(r, 2);
 
-                if (isStartPointInCircle ^ isEndPointInCircle) {
+                if (isStartPointInCircle ^ isEndPointInCircle) { // XOR 연산 두 변수가 다른 경우만 참
                     answer++;
                 }
             }
