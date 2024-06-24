6d57ebba32b5d905ca540ff9e8922bcf0060747a
CHOIJIHO <wlghsp@gmail.com>
2024-06-25 07:21:33 +0900

    7주차 문제 풀이 예정

---
+++ src/baekjoon/gold/gold3/boj20058_마법사상어와파이어스톰/Main.java
@@ -0,0 +1,14 @@
+package baekjoon.gold.gold3.boj20058_마법사상어와파이어스톰;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.setIn(new FileInputStream("src/baekjoon/gold/gold5/boj6593_상범빌딩/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
---
+++ src/baekjoon/gold/gold5/boj6593_상범빌딩/Main.java
@@ -0,0 +1,15 @@
+package baekjoon.gold.gold5.boj6593_상범빌딩;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.setIn(new FileInputStream("src/baekjoon/gold/gold5/boj6593_상범빌딩/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
