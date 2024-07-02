88f2bbde59bdee4a324aa0ca32113b501707ad32
CHOIJIHO <wlghsp@gmail.com>
2024-07-02 20:27:21 +0900

    풀이 예정

---
+++ src/baekjoon/silver/silver2/boj24479_알고리즘수업깊이우선탐색1/Main.java
@@ -0,0 +1,17 @@
+package baekjoon.silver.silver2.boj24479_알고리즘수업깊이우선탐색1;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int N, M, R;
+
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/baekjoon/silver/silver2/boj24479_알고리즘수업깊이우선탐색1/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
