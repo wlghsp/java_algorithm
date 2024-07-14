31b2653b8129185f9ed15a396d96b4af71bc5e0b
CHOIJIHO <wlghsp@gmail.com>
2024-07-14 19:00:20 +0900

    프로그래머스 레벨 1 바탕화면 정리 풀이 예정

---
+++ src/programmers/level1/바탕화면정리/Solution.java
@@ -0,0 +1,8 @@
+package programmers.level1.바탕화면정리;
+
+public class Solution {
+    public int[] solution(String[] wallpaper) {
+        int[] answer = {};
+        return answer;
+    }
+}
---
+++ test/programmers/level1/바탕화면정리/SolutionTest.java
@@ -0,0 +1,22 @@
+package programmers.level1.바탕화면정리;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SolutionTest {
+    @Test
+    void test() {
+        Solution s = new Solution();
+        int[] result1 = s.solution(new String[]{".#...", "..#..", "...#."});
+        int[] result2 = s.solution(new String[]{"..........", ".....#....", "......##..", "...##.....", "....#....."});
+        int[] result3 = s.solution(new String[]{".##...##.", "#..#.#..#", "#...#...#", ".#.....#.", "..#...#..", "...#.#...", "....#...."});
+        int[] result4 = s.solution(new String[]{"..", "#."});
+
+        assertThat(result1).isEqualTo(new int[]{0, 1, 4, 3});
+        assertThat(result2).isEqualTo(new int[]{1, 3, 5, 8});
+        assertThat(result3).isEqualTo(new int[]{0, 0, 7, 9});
+        assertThat(result4).isEqualTo(new int[]{1, 0, 2, 1});
+    }
+}
