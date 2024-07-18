2f194d6e76b050564d85a85d6dcc802fd160e3cd
JIHO CHOI <wlghsp@gmail.com>
2024-07-19 07:39:37 +0900

    프로그래머스 레벨 1 달리기 경주 풀이 완료

--- src/programmers/level1/달리기경주/Solution.java
+++ src/programmers/level1/달리기경주/Solution.java
@@ -1,11 +1,34 @@
 package programmers.level1.달리기경주;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
     public String[] solution(String[] players, String[] callings) {
-        List<String> answer = new ArrayList<>();
-        return answer.toArray(String[]::new);
+        Map<String, Integer> nameMap = new HashMap<>();
+        Map<Integer, String> rankMap = new HashMap<>();
+        for (int i = 0; i < players.length; i++) {
+            nameMap.put(players[i], i);
+            rankMap.put(i, players[i]);
+        }
+
+        for (String calling : callings) {
+            // 앞지르는 선수
+            Integer behindRank = nameMap.get(calling);
+            String behindName = rankMap.get(behindRank);
+
+            // 뒤로 밀리는 선수 
+            String frontName = rankMap.get(behindRank - 1);
+
+            // 서로 교환
+            nameMap.put(behindName, behindRank - 1);
+            rankMap.put(behindRank - 1, behindName);
+
+            nameMap.put(frontName, behindRank);
+            rankMap.put(behindRank, frontName);
+        }
+
+        return rankMap.values()
+                .toArray(new String[0]);
     }
 }
