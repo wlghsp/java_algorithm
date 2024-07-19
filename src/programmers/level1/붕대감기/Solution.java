64b7fe35aed92a123cd75042043f623d7b4b15b4
JIHO CHOI <wlghsp@gmail.com>
2024-07-19 20:24:54 +0900

    프로그래머스 레벨 1 붕대 감기 풀이 완료

--- src/programmers/level1/붕대감기/Solution.java
+++ src/programmers/level1/붕대감기/Solution.java
@@ -2,8 +2,45 @@ package programmers.level1.붕대감기;
 
 public class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
+        int maxSec = Integer.MIN_VALUE;
 
-        int answer = 0;
-        return answer;
+        for (int[] attack : attacks) {
+            maxSec = Math.max(attack[0], maxSec);
+        }
+        int maxHealth = health;
+
+        int consecutive = 0;
+        int attackIdx = 0;
+        for (int i = 1; i <= maxSec; i++) {
+
+            if (i == attacks[attackIdx][0]) {
+                consecutive = 0;
+                health -= attacks[attackIdx][1];
+                attackIdx++;
+            } else {
+                consecutive++;
+
+                if (maxHealth <= health) continue;
+
+                health += bandage[1];
+
+                if (consecutive == bandage[0]) {
+                    health += bandage[2];
+                    consecutive = 0;
+                }
+
+
+                if (health > maxHealth)
+                    health = maxHealth;
+            }
+
+            if (health <= 0) {
+                return -1;
+            }
+
+        }
+
+
+        return health;
     }
 }
--- test/programmers/level1/붕대감기/SolutionTest.java
+++ test/programmers/level1/붕대감기/SolutionTest.java
@@ -1,7 +1,8 @@
 package programmers.level1.붕대감기;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
 
@@ -13,9 +14,9 @@ class SolutionTest {
         int result3 = s.solution(new int[]{4, 2, 7}, 20, new int[][]{{1, 15}, {5, 16}, {8, 6}});
         int result4 = s.solution(new int[]{1, 1, 1}, 5, new int[][]{{1, 2}, {3, 2}});
 
-        Assertions.assertThat(result1).isEqualTo(5);
-        Assertions.assertThat(result2).isEqualTo(-1);
-        Assertions.assertThat(result3).isEqualTo(-1);
-        Assertions.assertThat(result4).isEqualTo(3);
+        assertThat(result1).isEqualTo(5);
+        assertThat(result2).isEqualTo(-1);
+        assertThat(result3).isEqualTo(-1);
+        assertThat(result4).isEqualTo(3);
     }
 }
