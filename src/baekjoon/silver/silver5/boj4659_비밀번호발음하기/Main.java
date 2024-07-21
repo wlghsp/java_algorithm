470f69cfb98ed6984a0cdd4558766eec01b7fb47
JIHO CHOI <wlghsp@gmail.com>
2024-07-21 21:27:27 +0900

    백준 4659 비밀번호 발음하기 다른 풀이 참조 학습

--- src/baekjoon/silver/silver5/boj4659_비밀번호발음하기/Main.java
+++ src/baekjoon/silver/silver5/boj4659_비밀번호발음하기/Main.java
@@ -10,65 +10,38 @@ public class Main {
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("src/baekjoon/silver/silver5/boj4659_비밀번호발음하기/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String password;
+        char prev;
+        int count;
+        while (!(password = br.readLine()).equals("end")) {
 
-        while (true) {
-            String str = br.readLine();
-            if ("end".equals(str)) {
-                break;
+            boolean acceptable = false;
+            prev = '.';
+            count = 0;
+            for (char c : password.toCharArray()) {
+                if (isVowel(c)) acceptable = true;
+
+                if (isVowel(c) != isVowel(prev)) count = 1;
+                else count++;
+
+                if (count > 2 || (prev == c && (c !='e' && c != 'o'))) {
+                    acceptable = false;
+                    break;
+                }
+
+                prev = c;
             }
-            boolean acceptable = true;
 
-            if (noVowelMatch(str)) acceptable = false;
-
-            if (threeConsecutiveVowelOrConsonant(str)) acceptable = false;
-
-            if (twoConsecutiveLetter(str)) acceptable = false;
-
-            System.out.println("<" + str + "> is " + (acceptable ? "" : "not ") + "acceptable.");
+            System.out.println("<" + password + "> is " + (acceptable ? "" : "not ") + "acceptable.");
 
         }
     }
 
-    private static boolean twoConsecutiveLetter(String str) {
-        for (int i = 0; i < str.length() - 1; i++) {
-            if (isNotOkay(str, i)) {
-                return true;
-            }
-        }
-        return false;
-    }
 
-    private static boolean isNotOkay(String str, int i) {
-        String substr = str.substring(i, i + 2);
-        if (substr.equals("ee") || substr.equals("oo")) return false;
-        return (str.charAt(i) == str.charAt(i + 1));
-    }
 
     private static boolean isVowel(char c) {
         return "aeiou".indexOf(c) != -1;
     }
 
-    private static boolean threeConsecutiveVowelOrConsonant(String str) {
-        int vowelCount = 0;
-        int consonantCount = 0;
 
-        for (char c : str.toCharArray()) {
-            if (isVowel(c)) {
-                vowelCount++;
-                consonantCount = 0;
-            } else if (Character.isLetter(c)) {
-                consonantCount++;
-                vowelCount = 0;
-            }
-
-            if (vowelCount >= 3 || consonantCount >= 3) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean noVowelMatch(String str) {
-        return !str.matches(".*[aeiou].*");
-    }
 }
