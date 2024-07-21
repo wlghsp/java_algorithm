1b80c7e13fac8277b98a09db978d5db839371136
JIHO CHOI <wlghsp@gmail.com>
2024-07-21 23:10:37 +0900

    백준 1251 단어 나누기 풀이 예정

---
+++ src/baekjoon/silver/silver5/boj1251_단어나누기/Main.java
@@ -0,0 +1,17 @@
+package baekjoon.silver.silver5.boj1251_단어나누기;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/baekjoon/silver/silver5/boj1251_단어나누기/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+
+
+    }
+}
