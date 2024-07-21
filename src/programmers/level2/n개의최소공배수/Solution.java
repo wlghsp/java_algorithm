060f31bd160efb1a1131a21888421a9dc2f7459f
JIHO CHOI <wlghsp@gmail.com>
2024-07-21 21:35:24 +0900

    프로그래머스 레벨 2 N개의 최소공배수 풀이 완료

--- src/programmers/level2/n개의최소공배수/Solution.java
+++ src/programmers/level2/n개의최소공배수/Solution.java
@@ -3,7 +3,29 @@ package programmers.level2.n개의최소공배수;
 public class Solution {
 
     public int solution(int[] arr) {
-        int answer = 0;
-        return answer;
+        return getLCMWithArr(arr);
+    }
+
+    private int getLCMWithArr(int[] arr) {
+        if (arr.length == 1) {
+            return arr[0];
+        }
+
+        int gcd = getGCD(arr[0], arr[1]);
+        int lcm = (arr[0] * arr[1]) / gcd;
+
+        for (int i = 2; i < arr.length; i++) {
+            gcd = getGCD(lcm, arr[i]);
+            lcm = (lcm * arr[i]) / gcd;
+        }
+
+        return lcm;
+    }
+
+    private int getGCD(int a, int b) {
+        if (a % b == 0) {
+            return b;
+        }
+        return getGCD(b, a % b);
     }
 }
