f765bf823fdfa79a672dfe52f56e57a60b864bd9
JIHO CHOI <wlghsp@gmail.com>
2024-07-21 22:35:32 +0900

    프로그래머스 레벨 2 점프와 순간 이동 학습 완료
    - 종료위치에서 거꾸로 이동
    - 순간이동으로 이동한 위치의 수는 무조건 짝수

--- src/programmers/level2/점프와순간이동/Solution.java
+++ src/programmers/level2/점프와순간이동/Solution.java
@@ -3,6 +3,17 @@ package programmers.level2.점프와순간이동;
 public class Solution {
     public int solution(int n) {
         int ans = 0;
+
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+
+            } else {
+                n -= 1;
+                ans++;
+            }
+        }
+
         return ans;
     }
 }
