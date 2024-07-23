014ac2d28be6dd26887954992b5adb47616cb36e
CHOIJIHO <wlghsp@gmail.com>
2024-07-24 08:21:41 +0900

    프로그래머스 레벨 2 n^2 배열 자르기 풀이 예정

---
+++ src/programmers/level2/n2배열자르기/Solution.java
@@ -0,0 +1,9 @@
+package programmers.level2.n2배열자르기;
+
+public class Solution {
+
+    public int[] solution(int n, long left, long right) {
+        int[] answer = {};
+        return answer;
+    }
+}
---
+++ test/programmers/level2/n2배열자르기/SolutionTest.java
@@ -0,0 +1,19 @@
+package programmers.level2.n2배열자르기;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SolutionTest {
+
+    @Test
+    void test() {
+        Solution s = new Solution();
+        int[] result1 = s.solution(3, 2, 5);
+        int[] result2 = s.solution(4, 7, 14);
+
+        assertThat(result1).isEqualTo(new int[]{3, 2, 2, 3});
+        assertThat(result2).isEqualTo(new int[]{4,3,3,3,4,4,4,4});
+    }
+}
