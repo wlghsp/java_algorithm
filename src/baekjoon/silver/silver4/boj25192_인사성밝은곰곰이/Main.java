704ee03bfdd34f15a774d5029149c4834391d4a6
JIHO CHOI <wlghsp@gmail.com>
2024-07-23 20:31:22 +0900

    백준 25192 인사성 밝은 곰곰이 풀이 예정

---
+++ src/baekjoon/silver/silver4/boj25192_인사성밝은곰곰이/Main.java
@@ -0,0 +1,16 @@
+package baekjoon.silver.silver4.boj25192_인사성밝은곰곰이;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/baekjoon/silver/silver4/boj25192_인사성밝은곰곰이/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+    }
+}
