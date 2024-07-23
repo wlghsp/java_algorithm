9041f0ba93d5a11531d31bd722452d829c14aaca
JIHO CHOI <wlghsp@gmail.com>
2024-07-23 19:56:39 +0900

    프로그래머스 레벨 2 귤 고르기 풀이 완료

--- src/programmers/level2/귤고르기/Solution.java
+++ src/programmers/level2/귤고르기/Solution.java
@@ -1,8 +1,28 @@
 package programmers.level2.귤고르기;
 
+import java.util.*;
+
 public class Solution {
+
     public int solution(int k, int[] tangerine) {
-        int answer = 0;
-        return answer;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int t : tangerine) {
+            map.put(t, map.getOrDefault(t, 0) + 1);
+        }
+        List<Integer> counts = new ArrayList<>(map.values());
+
+        counts.sort(Collections.reverseOrder());
+
+        int types = 0;
+
+        for (Integer count : counts) {
+            k -= count;
+            types++;
+            if (k <= 0) {
+                break;
+            }
+        }
+
+        return types;
     }
 }
