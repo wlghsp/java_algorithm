4c5cb9e020e1fad7014698df8ca7344bbe98b43d
CHOIJIHO <wlghsp@gmail.com>
2024-07-25 07:49:38 +0900

    백준 2828 사과 담기 게임 풀이 완료

--- src/baekjoon/silver/silver5/boj2828_사과담기게임/Main.java
+++ src/baekjoon/silver/silver5/boj2828_사과담기게임/Main.java
@@ -9,11 +9,31 @@ import java.util.StringTokenizer;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("src/baekjoon/silver/silver5/boj1251_단어나누기/input.txt"));
+        System.setIn(new FileInputStream("src/baekjoon/silver/silver5/boj2828_사과담기게임/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st   = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
+        int j = Integer.parseInt(br.readLine());
 
+        int ans = 0;
+        int start = 1;
+        int end = start + M - 1;
+        for (int i = 0; i < j; i++) {
+            int apple = Integer.parseInt(br.readLine());
+            if (end < apple) {
+                // 오른쪽 이동
+                ans += (apple - end);
+                start += apple - end;
+                end = apple;
+            } else if (start > apple) {
+                // 왼쪽 이동
+                ans += (start - apple);
+                end -= start - apple;
+                start = apple;
+            }
+        }
+
+        System.out.println(ans);
     }
 }
