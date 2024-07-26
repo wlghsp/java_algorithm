46e82346c8b52b431b4b93d050122d8a4d8eb158
JIHO CHOI <wlghsp@gmail.com>
2024-07-26 20:18:59 +0900

    프로그래머스 레벨 2 할인행사 풀이 완료

--- src/programmers/level2/할인행사/Solution.java
+++ src/programmers/level2/할인행사/Solution.java
@@ -1,20 +1,33 @@
 package programmers.level2.할인행사;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
-    /**
-     * 일정한 금액 지불  10일동안 회원
-     * 회원 대상 매일 1가지 할인
-     * 할인제품 하나에 하나 구매 가능
-     * 원하는 제품과 수량이 할인하는 날짜와 10일 연속 일치 시 회원 가입
-     *
-     * 10일 연속 want와 number 일치 시 회원가입
-     * want 원하는 제품 배열
-     * number 원하는 제품 수량
-     * 할인하는 제품
-     */
 
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
+        Map<String, Integer> wantMap = new HashMap<>();
+        for (int i = 0; i < want.length; i++) {
+            wantMap.put(want[i], number[i]);
+        }
+
+        for (int i = 0; i < discount.length - 9; i++) {
+            Map<String, Integer> discountMap = new HashMap<>();
+            for (int j = i; j < i + 10; j++) {
+                discountMap.put(discount[j], discountMap.getOrDefault(discount[j], 0) + 1);
+            }
+            boolean same = true;
+
+            for (int j = 0; j < want.length; j++) {
+                if (!wantMap.get(want[j]).equals(discountMap.get(want[j]))) {
+                    same = false;
+                    break;
+                }
+            }
+            answer += same ? 1 : 0;
+        }
+
         return answer;
     }
 }
