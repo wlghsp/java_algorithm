deab942cb2f43eb8c4e589a33c085274495333b5
CHOIJIHO <wlghsp@gmail.com>
2024-07-27 18:45:49 +0900

    마법사상어와 파이어스톰 풀이 시작

--- src/baekjoon/gold/gold3/boj20058_마법사상어와파이어스톰/Main.java
+++ src/baekjoon/gold/gold3/boj20058_마법사상어와파이어스톰/Main.java
@@ -1,14 +1,21 @@
 package baekjoon.gold.gold3.boj20058_마법사상어와파이어스톰;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        System.setIn(new FileInputStream("src/baekjoon/gold/gold5/boj6593_상범빌딩/input.txt"));
+    static int N, Q, gridSize;
+    static int[][] grid;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/baekjoon/gold/gold3/boj20058_마법사상어와파이어스톰/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        Q = Integer.parseInt(br.readLine());
+        gridSize = (int) Math.sqrt(N);
+
     }
 }
