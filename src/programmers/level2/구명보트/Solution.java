d46cd48dd3079cd1cd6edff6a20fa2fdb0f5946f
JIHO CHOI <wlghsp@gmail.com>
2024-07-28 19:43:17 +0900

    프로그래머스 레벨 2 구명보트 복습 완료

--- src/programmers/level2/구명보트/Solution.java
+++ src/programmers/level2/구명보트/Solution.java
@@ -3,18 +3,26 @@ package programmers.level2.구명보트;
 import java.util.Arrays;
 
 public class Solution {
-    public int solution(int[] people, int limit) {
-        int answer = 0;
-        Arrays.sort(people);
+    /**
+     * 1. 사람들의 몸무게를 정렬
+     * 2. left 가장 가벼운 사람, right 는 가장 무거운 사람
+     * 3. 가장 가벼운 사람과 가장 무거운 사람을 함께 태울 수 있으면 left 증가
+     * 4. 둘다 못 태우면 무거운 사람만 태우고 right 감소
+     * 5. 보트 사용한 횟수 증가
+     */
 
+    public int solution(int[] people, int limit) {
+        int boats = 0;
+
+        Arrays.sort(people);
         int left = 0;
-        for (int right = people.length - 1; right >= left; right--) {
-            if (people[right] + people[left] <= limit) {
+        for (int right = people.length - 1; left <= right; right--) {
+            if (people[left] + people[right] <= limit) {
                 left++;
             }
-            answer++;
+            boats++;
         }
 
-        return answer;
+        return boats;
     }
 }
