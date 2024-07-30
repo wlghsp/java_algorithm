2523a3163e16f1e23fded24a02961265392bab25
CHOIJIHO <wlghsp@gmail.com>
2024-07-31 08:09:51 +0900

    백준 15900 나무탈출 풀이 예정

---
+++ src/baekjoon/silver/silver1/boj15900_나무탈출/Main.java
@@ -0,0 +1,17 @@
+package baekjoon.silver.silver1.boj15900_나무탈출;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/baekjoon/silver/silver1/boj15900_나무탈출/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+    }
+}
