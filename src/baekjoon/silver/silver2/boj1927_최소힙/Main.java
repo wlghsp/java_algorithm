dc54cd8fbb53309998e6c9b2d3156a7285097bfc
CHOIJIHO <wlghsp@gmail.com>
2024-08-01 19:04:14 +0900

    백준 1927 최소 힙 풀이 예정

---
+++ src/baekjoon/silver/silver2/boj1927_최소힙/Main.java
@@ -0,0 +1,17 @@
+package baekjoon.silver.silver2.boj1927_최소힙;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/baekjoon/silver/silver2/boj1927_최소힙/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+    }
+}
