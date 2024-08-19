948ec5fafca1a72e09a8cf13f76e9bb67f18d584
JIHO CHOI <wlghsp@gmail.com>
2024-08-19 20:04:38 +0900

    백준 1446 지름길 풀이 완료

--- src/baekjoon/silver/silver1/boj1446_지름길/Main.java
+++ src/baekjoon/silver/silver1/boj1446_지름길/Main.java
@@ -4,11 +4,10 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int N, D;
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("src/baekjoon/silver/silver1/boj1446_지름길/input.txt"));
@@ -17,29 +16,39 @@ public class Main {
 
         int N = Integer.parseInt(st.nextToken());
         int D = Integer.parseInt(st.nextToken());
+        ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
+        for (int i = 0; i <= D; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int length = Integer.parseInt(st.nextToken());
+            if (end <= D) {
+                graph.get(start).add(new int[]{end, length});
+            }
+        }
 
         int[] dist = new int[D + 1];
         for (int i = 0; i <= D; i++) {
             dist[i] = i;  // 초기화: 지름길을 사용하지 않을 경우의 거리
         }
 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int length = Integer.parseInt(st.nextToken());
+        for (int i = 0; i <= D; i++) {
+            if (i > 0) {
+                dist[i] = Math.min(dist[i - 1] + 1, dist[i]);
+            }
 
-            if (end <= D && dist[end] > dist[start] + length) {
-                dist[end] = dist[start] + length;
+            for (int[] tmp : graph.get(i)) {
+                int end = tmp[0];
+                int length = tmp[1];
+                if (dist[end] > dist[i] + length) {
+                    dist[end] = dist[i] + length;
+                }
             }
         }
 
-        // 고속도로의 모든 지점을 순차적으로 업데이트
-        for (int i = 1; i <= D; i++) {
-            dist[i] = Math.min(dist[i], dist[i - 1] + 1);
-        }
-
-        // 최종 결과 출력
         System.out.println(dist[D]);
     }
 }
