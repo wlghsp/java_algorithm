521ba0a9c408996d6b1c939e2d5c627967a40675
JIHO CHOI <wlghsp@gmail.com>
2024-08-26 13:13:26 +0900

    프로그래머스 레벨 2 두 큐 합 같게 만들기

--- test/programmers/level2/두큐합같게만들기/SolutionTest.java
+++ test/programmers/level2/두큐합같게만들기/SolutionTest.java
@@ -12,7 +12,7 @@ class SolutionTest {
         Solution s = new Solution();
         int result1 = s.solution(new int[]{3, 2, 7, 2}, new int[]{4, 6, 5, 1});
         int result2 = s.solution(new int[]{1, 2, 1, 2}, new int[]{1, 10, 1, 2});
-        int result3 = s.solution(new int[]{1, 2, 1, 2}, new int[]{1, 10, 1, 2});
+        int result3 = s.solution(new int[]{1, 1}, new int[]{1, 5});
 
         assertThat(result1).isEqualTo(2);
         assertThat(result2).isEqualTo(7);
