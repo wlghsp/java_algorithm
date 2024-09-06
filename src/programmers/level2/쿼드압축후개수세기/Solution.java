6aa7ea5474641b2f248bbeaca194437117342aaf
JIHO CHOI <jh.choi2@okestro.com>
2024-09-06 15:13:15 +0900

    프로그래머스 쿼드 압축 후 개수 세기 풀이 예정

---
+++ src/programmers/level2/쿼드압축후개수세기/Solution.java
@@ -0,0 +1,10 @@
+package programmers.level2.쿼드압축후개수세기;
+
+public class Solution {
+
+    public int[] solution(int[][] arr) {
+        int[] answer = {};
+        return answer;
+    }
+
+}
---
+++ test/programmers/level2/쿼드압축후개수세기/SolutionTest.java
@@ -0,0 +1,18 @@
+package programmers.level2.쿼드압축후개수세기;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SolutionTest {
+
+    @Test
+    void test() {
+        Solution s = new Solution();
+        int[] result1 = s.solution(new int[][]{{1, 1, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 1}, {1, 1, 1, 1}});
+        int[] result2 = s.solution(new int[][]{{1, 1, 1, 1, 1, 1, 1, 1}, {0, 1, 1, 1, 1, 1, 1, 1}, {0, 0, 0, 0, 1, 1, 1, 1}, {0, 1, 0, 0, 1, 1, 1, 1}, {0, 0, 0, 0, 0, 0, 1, 1}, {0, 0, 0, 0, 0, 0, 0, 1}, {0, 0, 0, 0, 1, 0, 0, 1}, {0, 0, 0, 0, 1, 1, 1, 1}});
+
+        assertThat(result1).isEqualTo(new int[]{4, 9});
+        assertThat(result2).isEqualTo(new int[]{10, 15});
+    }
+}
