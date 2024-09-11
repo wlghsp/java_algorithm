afa6262416255bc01412b9403d4f23534d1eceeb
JIHO CHOI <jh.choi2@okestro.com>
2024-09-11 16:14:07 +0900

    백준 31403 A + B - C 풀이 완료

---
+++ src/baekjoon/bronze/bronze4/boj31403_A_B_C/Main.java
@@ -0,0 +1,18 @@
+package baekjoon.bronze.bronze4.boj31403_A_B_C;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+        String c = br.readLine();
+
+        System.out.println(Integer.parseInt(a) + Integer.parseInt(b) - Integer.parseInt(c));
+        System.out.println(Integer.parseInt(a + b) - Integer.parseInt(c));
+    }
+}
