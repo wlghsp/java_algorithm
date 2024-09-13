d233c4f72b44cbcbb6727c01e46132131f738843
JIHO CHOI <jh.choi2@okestro.com>
2024-09-13 10:19:56 +0900

    백준 1874 스택 수열 풀이 완료

--- src/baekjoon/silver/silver2/boj1874_스택_수열/Main.java
+++ src/baekjoon/silver/silver2/boj1874_스택_수열/Main.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
     static int N;
@@ -12,5 +14,34 @@ public class Main {
         System.setIn(new FileInputStream("src/baekjoon/silver/silver2/boj1874_스택_수열/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
+
+        int current = 1;
+        StringBuilder sb = new StringBuilder();
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < N; i++) {
+            int target = Integer.parseInt(br.readLine());
+
+            if (target >= current) {
+                while (target >= current) {
+                    stack.push(current);
+                    current++;
+                    sb.append("+").append(System.lineSeparator());
+                }
+                stack.pop();
+                sb.append("-").append(System.lineSeparator());
+            } else {
+                if (stack.peek() == target) {
+                    stack.pop();
+                    sb.append("-").append(System.lineSeparator());
+                } else {
+                    System.out.println("NO");
+                    return;
+                }
+            }
+        }
+
+        System.out.println(sb);
+
     }
 }
