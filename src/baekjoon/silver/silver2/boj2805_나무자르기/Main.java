4a576b3c1c8ec2a655550d0180022c6c628e3b73
JIHO CHOI <jh.choi2@okestro.com>
2024-09-19 10:51:04 +0900

    백준 15686 나무자르기 학습 완료

--- src/baekjoon/silver/silver2/boj2805_나무자르기/Main.java
+++ src/baekjoon/silver/silver2/boj2805_나무자르기/Main.java
@@ -8,6 +8,7 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("src/baekjoon/silver/silver2/boj2805_나무자르기/input.txt"));
@@ -15,7 +16,41 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-
+        process();
     }
+
+    private static void process() {
+        long L = 0, R = 2_000_000_000, answer = 0;
+
+        while (L <= R) {
+            long mid = L + (R - L) / 2;
+            if (determination((int) mid)) {
+                answer = mid;
+                L = mid + 1;
+            } else {
+                R = mid - 1;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    private static boolean determination(int H) {
+        long sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (arr[i] > H) {
+                sum += arr[i] - H;
+            }
+        }
+
+        return sum >= M;
+    }
+
 }
