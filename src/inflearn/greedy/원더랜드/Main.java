db884a2e03f7d00ca9865d92b649ef99217822b9
JIHO CHOI <jh.choi2@okestro.com>
2024-09-22 18:06:28 +0900

    인프런 원더랜드 크루스칼 알고리즘 풀이 학습 완료
    - 크루스칼 알고리즘에 유니온 파인드를 사용함

--- src/inflearn/greedy/원더랜드/Main.java
+++ src/inflearn/greedy/원더랜드/Main.java
@@ -4,28 +4,77 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 
-class Road {
-    int start;
-    int end;
+class Edge implements Comparable<Edge> {
+    int v1;
+    int v2;
     int cost;
 
-    public Road(int start, int end, int cost) {
-        this.start = start;
-        this.end = end;
+    public Edge(int v1, int v2, int cost) {
+        this.v1 = v1;
+        this.v2 = v2;
         this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        return this.cost - o.cost;
     }
 }
 
 public class Main {
-
     static int V, E;
+    static int[] unf;
+    static List<Edge> edges = new ArrayList<>();
+
+    public static int find(int v) {
+        if (v == unf[v]) {
+            return v;
+        } else {
+            return unf[v] = find(unf[v]);
+        }
+    }
+
+    public static void union(int a, int b) {
+        int fa = find(a);
+        int fb = find(b);
+        if (fa != fb) {
+            unf[fa] = b;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("src/inflearn/greedy/원더랜드/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        unf = new int[V + 1];
+        for (int i = 1; i <= V; i++) {
+            unf[i] = i;
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            edges.add(new Edge(v1, v2, cost));
+        }
+        Collections.sort(edges);
+
+        int answer = 0;
+        for (Edge edge : edges) {
+            int fv1 = find(edge.v1);
+            int fv2 = find(edge.v2);
+            if (fv1 != fv2) {
+                answer += edge.cost;
+                union(edge.v1, edge.v2);
+            }
+        }
+
+        System.out.println(answer);
     }
 }
