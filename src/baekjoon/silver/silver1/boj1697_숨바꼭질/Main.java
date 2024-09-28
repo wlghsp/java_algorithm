7e3c4643076da7246bf581d3c2044d4167d96ae4
JIHO CHOI <wlghsp@gmail.com>
2024-09-28 22:37:44 +0900

    백준 1697 숨바꼭질 코드 최적화 가독성 향상

--- src/baekjoon/silver/silver1/boj1697_숨바꼭질/Main.java
+++ src/baekjoon/silver/silver1/boj1697_숨바꼭질/Main.java
@@ -7,49 +7,43 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N, K;
-    static int[] check = new int[100001];
+    static int[] visited;
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("src/baekjoon/silver/silver1/boj1697_숨바꼭질/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken()); // 수빈이의 위치
+        K = Integer.parseInt(st.nextToken()); // 동생의 위치
 
-        if (N == K) System.out.println(0);
-        else bfs(N);
+        bfs(N);
     }
 
-    static void bfs(int n) {
+    static void bfs(int start) {
+        visited = new int[100001];
+
         Queue<Integer> q = new LinkedList<>();
-        q.add(n);
-        check[n] = 1;
+        q.add(start);
+        visited[start] = 1; // 시작점 1로 설정하고 방문처리
+
         while (!q.isEmpty()) {
-            int temp = q.poll();
+            int current = q.poll();
 
-            for (int i = 0; i < 3; i++) {
-                int next;
-
-                if (i == 0) {
-                    next = temp + 1;
-                } else if (i == 1) {
-                    next = temp - 1;
-                } else {
-                    next = temp * 2;
-                }
-
-                if (next == K) {
-                    System.out.println(check[temp]);
-                    return;
-                }
-
-                if (next >= 0 && next < check.length && check[next] == 0) {
-                    q.add(next);
-                    check[next] = check[temp] + 1;
-                }
+            if (current == K) {
+                // 시작점이 1이므로 1을 마이너스 해줌
+                System.out.println(visited[current] - 1);
+                return;
             }
 
+            int[] nextPositions = {current * 2, current + 1, current - 1};
 
+            for (int next : nextPositions) {
+                if (next < 0 || next >= visited.length) continue;
+                if (visited[next] != 0) continue;
+
+                q.add(next);
+                visited[next] = visited[current] + 1;
+            }
         }
 
     }
