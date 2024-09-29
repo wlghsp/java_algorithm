df82b8f178aeb7e4eb782dcd40368b1419bb3d3f
JIHO CHOI <wlghsp@gmail.com>
2024-09-29 11:55:57 +0900

    인프런 4.가장 높은 탑 쌓기 (LIS 응용)

--- src/inflearn/dynamic_programming/가장높은탑쌓기/Main.java
+++ src/inflearn/dynamic_programming/가장높은탑쌓기/Main.java
@@ -3,31 +3,61 @@ package inflearn.dynamic_programming.가장높은탑쌓기;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
+
+class Brick {
+    int s;
+    int h;
+    int w;
+
+    public Brick(int s, int h, int w) {
+        this.s = s;
+        this.h = h;
+        this.w = w;
+    }
+}
 
 public class Main {
 
     static int N;
-    static int[] S, H, W;
+    static int[] dy;
+    static List<Brick> bricks;
 
     public int solution() {
-        return 0;
+        dy[0] = bricks.get(0).h;
+        int answer = dy[0];
+
+        for (int i = 1; i < N; i++) {
+            int maxHeight = 0;
+            for (int j = i - 1; j >= 0; j--) {
+               if (bricks.get(j).w > bricks.get(i).w && dy[j] > maxHeight) {
+                   maxHeight = dy[j];
+               }
+            }
+            dy[i] = maxHeight + bricks.get(i).h;
+            answer = Math.max(answer, dy[i]);
+        }
+
+        return answer;
     }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        S = new int[N];
-        H = new int[N];
-        W = new int[N];
+        bricks = new ArrayList<>();
+        dy = new int[N];
         StringTokenizer st;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            S[i] = Integer.parseInt(st.nextToken());
-            H[i] = Integer.parseInt(st.nextToken());
-            W[i] = Integer.parseInt(st.nextToken());
+            int S = Integer.parseInt(st.nextToken());
+            int H = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+            bricks.add(new Brick(S, H, W));
         }
+        bricks.sort((a, b) -> b.s - a.s);
 
         System.out.println(T.solution());
     }
