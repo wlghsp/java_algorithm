753bd5ff9d56fdbf9c2343bc6a4e495cd1ba31d9
JIHO CHOI <wlghsp@gmail.com>
2024-09-30 21:30:50 +0900

    백준 2655 가장 높은 탑 쌓기 학습했으나 이해 안감

--- src/baekjoon/gold/gold3/boj2655_가장높은탑쌓기/Main.java
+++ src/baekjoon/gold/gold3/boj2655_가장높은탑쌓기/Main.java
@@ -4,72 +4,62 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 
 public class Main {
 
     static int N;
-    static int[] dy, path;
+    static int[] dy;
     static List<Brick> bricks = new ArrayList<>();
 
     public static int solution() {
-        dy[0] = bricks.get(0).h;
         int maxHeight = 0;
-        int lastIndex = 0;
-        for (int i = 1; i < N; i++) {
-            dy[i] = bricks.get(i).h;
-            path[i] = 0;
-            for (int j = i - 1; j >= 0; j--) {
-                if (bricks.get(i).w > bricks.get(j).w && dy[i] < dy[j] + bricks.get(i).h) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (bricks.get(i).w > bricks.get(j).w && dy[j] + bricks.get(i).h > dy[i]) {
                     dy[i] = dy[j] + bricks.get(i).h;
-                    path[i] = j;
                 }
             }
-            if (dy[i] > maxHeight) {
-                maxHeight = dy[i];
-                lastIndex = i;
-            }
+            maxHeight = Math.max(maxHeight, dy[i]);
         }
 
-        return lastIndex;
+        return maxHeight;
     }
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("src/baekjoon/gold/gold3/boj2655_가장높은탑쌓기/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        dy = new int[N];
-        path = new int[N];
+        dy = new int[N + 1];
+        bricks.add(new Brick(0, 0, 0, 0));
         StringTokenizer st;
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int h = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
             bricks.add(new Brick(i, s, h, w));
         }
-        bricks.sort((a, b) -> b.s - a.s);
+        bricks.sort(Comparator.comparingInt(a -> a.s));
 
-        int lastIndex = solution();
-        List<Integer> result = new ArrayList<>();
-        while (lastIndex != 0) {
-            result.add(bricks.get(lastIndex).index);
-            lastIndex = path[lastIndex];
+        int maxHeight = solution();
+        Stack<Integer> stack = new Stack<>();
+        while (N > 0) {
+            if (maxHeight == dy[N]) {
+                stack.push(bricks.get(N).index);
+                maxHeight -= bricks.get(N).h;
+            }
+            N--;
         }
 
-        Collections.reverse(result);
         StringBuilder sb = new StringBuilder();
-        sb.append(result.size()).append("\n");
-        for (Integer i : result) {
-            sb.append(i).append("\n");
-        }
+        sb.append(stack.size()).append("\n");
+        while (!stack.isEmpty())
+            sb.append(stack.pop()).append("\n");
         System.out.println(sb);
-
     }
+
 }
 
 class Brick {
